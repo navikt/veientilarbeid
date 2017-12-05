e6fb2d15062c354d5ebd3f70b17273bd1cdbb489
Christian Finstad <christian.finstad@nav.no>
2017-12-05 12:41:17 +0100

    [FO-5] Legger til dekoratørfilter

--- src/test/java/MainLocal.java
+++ src/test/java/MainLocal.java
@@ -1,9 +1,11 @@
-
 import no.nav.apiapp.ApiApp;
 import no.nav.sbl.veientilarbeid.config.ApplicationConfig;
 
-public class Main {
+public class MainLocal {
+
     public static void main(String... args) throws Exception {
+        System.setProperty("appres.cms.url","http://localhost:8080");
+
         ApiApp.startApp(ApplicationConfig.class, args);
     }
 }
