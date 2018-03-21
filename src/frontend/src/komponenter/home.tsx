d7e1bdbdd43e85ba32f2db1c7419bc2e749d8a2c
Joselito Poblete <joselito.poblete@gmail.com>
2018-03-21 13:29:43 +0100

    FO-801 Ny komigang komponent, og slette utbrukte komponenter

--- src/frontend/src/komponenter/home.tsx
+++ src/frontend/src/komponenter/home.tsx
@@ -1,16 +1,10 @@
 import * as React from 'react';
-import Tjenester from './tjenester/tjenester';
-import Komigang from './overskrift/kom-i-gang';
-import Oppgaver from './oppgaver/oppgaver';
-import Introduksjon from './introduksjon/introduksjon';
+import Komigang from './komigang/kom-i-gang';
 
 function Home() {
     return (
         <div className="body__wrapper">
-            <Introduksjon/>
             <Komigang/>
-            <Oppgaver />
-            <Tjenester/>
         </div>
     );
 }
