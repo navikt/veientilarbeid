acee53046fa2f3d0cd2a558259aa4b97afdc2961
Joselito Poblete <joselito.poblete@gmail.com>
2018-03-22 12:37:56 +0100

    FO-801 Vis ulike tekst beskrivelse avhengig om brukere kommer fra den nye registrering eller gamle registrering

--- src/frontend/src/komponenter/komigang/kom-i-gang.tsx
+++ src/frontend/src/komponenter/komigang/kom-i-gang.tsx
@@ -2,32 +2,46 @@ import * as React from 'react';
 import { Innholdstittel } from 'nav-frontend-typografi';
 import { Element } from 'nav-frontend-typografi';
 import { FormattedMessage } from 'react-intl';
+import { parse } from 'query-string';
 
 const aktivitetsplanSvg = require('./aktivitetsplan.svg');
 const AKTIVITETSPLAN_PATH = '/aktivitetsplan';
 
-function Komigang() {
-    return (
-        <div className="komigang">
-            <div className="komigang__tekst">
-                <Innholdstittel className="blokk-s">
-                    <FormattedMessage id="overskrift-komigang"/>
-                </Innholdstittel>
-                <Element className="blokk-s">
-                    <FormattedMessage id="beskrivelse-komigang"/>
-                </Element>
-                <div className="nav-frontend-lenker">
-                    <a href={AKTIVITETSPLAN_PATH} className="lenke">
-                        <FormattedMessage id="lenke-komigang"/>
-                    </a>
-                    <i className="nav-frontend-chevron chevronboks chevron--hoyre" />
+interface State {
+    nyRegistrering: boolean;
+}
+
+class Komigang extends React.PureComponent<{}, State> {
+    constructor(props: {}) {
+        super(props);
+        this.state = {
+            nyRegistrering: parse(location.search).nyRegistrering === 'true'
+        };
+    }
+    render() {
+        const beskrivelseTekstId = this.state.nyRegistrering ? 'beskrivelse-komigang-ny' : 'beskrivelse-komigang';
+        return (
+            <div className="komigang">
+                <div className="komigang__tekst">
+                    <Innholdstittel className="blokk-s">
+                        <FormattedMessage id="overskrift-komigang"/>
+                    </Innholdstittel>
+                    <Element className="blokk-s">
+                        <FormattedMessage id={beskrivelseTekstId}/>
+                    </Element>
+                    <div className="nav-frontend-lenker">
+                        <a href={AKTIVITETSPLAN_PATH} className="lenke">
+                            <FormattedMessage id="lenke-komigang"/>
+                        </a>
+                        <i className="nav-frontend-chevron chevronboks chevron--hoyre"/>
+                    </div>
+                </div>
+                <div className="komigang__illustrasjon">
+                    <img src={aktivitetsplanSvg} alt="aktivitetsplan-illustrasjon"/>
                 </div>
             </div>
-            <div className="komigang__illustrasjon">
-                <img src={aktivitetsplanSvg} alt="aktivitetsplan-illustrasjon"/>
-            </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default Komigang;
