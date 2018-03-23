8a7100012c62b33362a5b75e898fee36e202fe12
Lars Andreas van Woensel Kooy Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2018-03-23 11:37:10 +0100

    FO-776 Trekk ut tekster fra kjernekomponent

--- src/frontend/src/komponenter/informasjonsmodul-dagpenger/stort-ekspanderbartpanel/src/stort-ekspanderbartpanel-pure.tsx
+++ src/frontend/src/komponenter/informasjonsmodul-dagpenger/stort-ekspanderbartpanel/src/stort-ekspanderbartpanel-pure.tsx
@@ -1,36 +1,37 @@
 import * as React from 'react';
 import { Normaltekst, Systemtittel } from 'nav-frontend-typografi';
 import StortEkspanderbartpanelBasePure from './stort-ekspanderbartpanel-base-pure';
-const utklippstavleSvg = require('./utklippstavle.svg');
+import {Figur} from "./index";
 
 
 export interface StortEkspanderbartpanelPureProps {
     onClick: (event: React.SyntheticEvent<HTMLButtonElement>) => void;
     tittel: string;
-    tittelProps?: string;
+    undertekst: string;
+    figur?: Figur;
     apen: boolean;
 }
 
 class StortEkspanderbartpanelPure extends React.PureComponent<StortEkspanderbartpanelPureProps> {
-    static defaultProps: Partial<StortEkspanderbartpanelPureProps> = {
-        tittelProps: 'undertittel',
-    };
-
     render() {
-        const { tittel, tittelProps, ...renderProps } = this.props;
+        const { tittel, undertekst, figur, ...renderProps } = this.props;
         //const heading = <Typografi type={'element'} tag="span" className="ekspanderbartPanel__heading">{tittel}</Typografi>;
+        const figurComponent = figur ? (
+            <div className="figur__wrapper">
+                <img src={require(`./${figur}.svg`)} className="figur"/>
+            </div>
+        ) : (null);
+
         const heading = (
             <React.Fragment>
-                <div className="figur__wrapper">
-                    <img src={utklippstavleSvg} className="figur"/>
-                </div>
+                {figurComponent}
                 <div className="stortEkspanderbartPanel__heading-wrapper">
                     <Systemtittel tag="span" className="stortEkspanderbartPanel__heading">
-                        Informasjon om dagpenger
+                        {tittel}
                     </Systemtittel>
                     <div className="stortEkspanderbartPanel__underheading-wrapper">
                         <Normaltekst tag="div" className="stortEkspanderbartPanel__heading">
-                            Les mer om dagpenger og når du må søke
+                            {undertekst}
                         </Normaltekst>
                         <div>
                             <span className="stortEkspanderbartPanel__indikator" />
