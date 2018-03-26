b2ac8dc1717a7894652b3525696884fe3e0a2709
Joselito Poblete <joselito.poblete@gmail.com>
2018-03-26 11:54:05 +0200

    FO-5 Svg manglet bakgrunnsfarger

--- src/frontend/src/komponenter/home.tsx
+++ src/frontend/src/komponenter/home.tsx
@@ -2,12 +2,14 @@ import * as React from 'react';
 import Komigang from './komigang/kom-i-gang';
 import CV from './cv/cv';
 import Arbeidssoker from './arbeidssoker/arbeidssoker';
+import Informasjon from "./informasjon/informasjon";
 
 function Home() {
     return (
         <div>
             <Komigang/>
             <CV/>
+            <Informasjon/>
             <Arbeidssoker/>
         </div>
     );
---
+++ src/frontend/src/komponenter/informasjon/informasjon.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+function Informasjon() {
+    return (
+        <div className="informasjon-container"/>
+    );
+}
+
+export default Informasjon;
