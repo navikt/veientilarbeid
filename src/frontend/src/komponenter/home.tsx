c64fcf87e57ad585896122448c28846f5c846dba
Lars Andreas van Woensel Kooy Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2018-03-26 14:25:25 +0200

    FO-776 Opprydding etter merge

--- src/frontend/src/komponenter/home.tsx
+++ src/frontend/src/komponenter/home.tsx
@@ -1,16 +1,13 @@
 import * as React from 'react';
-import Tjenester from './tjenester/tjenester';
 import InformasjonsmodulDagpenger from './informasjonsmodul-dagpenger/informasjonsmodul-dagpenger';
 
 import Komigang from './komigang/kom-i-gang';
 
 function Home() {
     return (
-        <div>
+        <div className="body__wrapper">
             <Komigang/>
             <InformasjonsmodulDagpenger/>
-            <Komigang/>
-            <Tjenester/>
         </div>
     );
 }
