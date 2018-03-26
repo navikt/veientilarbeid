816711d136023385588f873a622315f8e9677941
Joselito Poblete <joselito.poblete@gmail.com>
2018-03-26 12:43:59 +0200

    FO-5 Lint fiks

--- src/frontend/src/komponenter/home.tsx
+++ src/frontend/src/komponenter/home.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import Komigang from './komigang/kom-i-gang';
 import CV from './cv/cv';
 import Arbeidssoker from './arbeidssoker/arbeidssoker';
-import Informasjon from "./informasjon/informasjon";
+import Informasjon from './informasjon/informasjon';
 
 function Home() {
     return (
