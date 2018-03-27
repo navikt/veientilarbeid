6f43de84310c3fafc088c17a2934a4a61565d5e1
Lars Andreas van Woensel Kooy Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2018-03-27 10:39:33 +0200

    FO-776 Endre knapp til lenke.

--- src/frontend/src/komponenter/informasjonsmodul-dagpenger/informasjonsmodul-dagpenger.tsx
+++ src/frontend/src/komponenter/informasjonsmodul-dagpenger/informasjonsmodul-dagpenger.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
 import StortEkspanderbartpanel from './stort-ekspanderbartpanel/src';
-import {injectIntl, InjectedIntlProps} from 'react-intl';
-import EkspanderbartpanelGruppe from "./ekspanderbartpanel-gruppe/ekspanderbartpanel-gruppe";
+import { injectIntl, InjectedIntlProps } from 'react-intl';
+import EkspanderbartpanelGruppe from './ekspanderbartpanel-gruppe/ekspanderbartpanel-gruppe';
 import {EkspanderbartpanelPure} from 'nav-frontend-ekspanderbartpanel';
-import {Ingress} from "nav-frontend-typografi";
-import KnappBase from 'nav-frontend-knapper';
+import { Ingress } from 'nav-frontend-typografi';
+import LenkeMedChevron from "../lenke-med-chevron/lenke-med-chevron";
 
+const SOKNAD_OM_DAGPENGER_PATH = '/veiledearbeidssoker/mistet-jobben/dagpenger-soknadsprosess';
 
 interface DummyProp {
     dummy?: string; //TypeScript klager hvis props kun er InjectedIntlProps
@@ -33,9 +34,11 @@ class InformasjonsmodulDagpenger extends React.Component<Props> {
                         }}
                         apen={true}
                     >
-                        <Ingress>
-                            Dagpenger er en delvis erstatning for tapt arbeidsinntekt når du mister jobben.
-                        </Ingress>
+                        <div className="informasjonsmodul-ingress__wrapper">
+                            <Ingress>
+                                Dagpenger er en delvis erstatning for tapt arbeidsinntekt når du mister jobben.
+                            </Ingress>
+                        </div>
                         <EkspanderbartpanelGruppe>
                             <EkspanderbartpanelPure
                                 tittel={intl.messages['informasjonsmodul-dagpenger-del-1-tittel']}
@@ -56,13 +59,11 @@ class InformasjonsmodulDagpenger extends React.Component<Props> {
                                 {intl.messages['informasjonsmodul-dagpenger-del-3-tekst']}
                             </EkspanderbartpanelPure>
                         </EkspanderbartpanelGruppe>
-                        <KnappBase
-                            type="standard"
-                            onClick={() => {
-                            }}
-                        >
-                            {intl.messages['informasjonsmodul-dagpenger-knapp']}
-                        </KnappBase>
+                        <div className="informasjonsmodul-lenke__wrapper">
+                            <LenkeMedChevron path={SOKNAD_OM_DAGPENGER_PATH}>
+                                {intl.messages['informasjonsmodul-dagpenger-knapp']}
+                            </LenkeMedChevron>
+                        </div>
                     </StortEkspanderbartpanel>
                 </div>
             </div>
