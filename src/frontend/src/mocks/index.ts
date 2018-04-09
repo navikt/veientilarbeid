0991c6dc875c90d5e5dcde735066d23e956b8cd0
Joselito Poblete <joselito.poblete@gmail.com>
2018-04-09 14:51:08 +0200

    FO-792 Pålogging nivå 4

--- src/frontend/src/mocks/index.ts
+++ src/frontend/src/mocks/index.ts
@@ -1,12 +1,18 @@
 /*tslint:disable*/
 import {mock, respondWith, delayed } from './utils';
 import oppfolgingResponse from './oppfolging-mock';
+import innloggingsInfo from './innloggings-info';
 import { VEILARBOPPFOLGINGPROXY_URL } from '../ducks/api';
 
 const MOCK_OPPFOLGING = true;
+const MOCK_INNLOGGINGS_INFO = true;
 
 if (MOCK_OPPFOLGING) {
     (mock as any).get(`${VEILARBOPPFOLGINGPROXY_URL}/oppfolging`, respondWith(delayed(1000, oppfolgingResponse)));
+}
+
+if (MOCK_INNLOGGINGS_INFO) {
+    (mock as any).get('glob:/innloggingslinje/auth*', respondWith(delayed(1000, innloggingsInfo)));
 }
 
 (mock as any).mock('*', respondWith((url: string, config: {}) => mock.realFetch.call(window, url, config)));
