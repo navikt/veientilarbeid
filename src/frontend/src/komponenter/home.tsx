92fab97080681374ee6f767a464e92b9219e31a0
Joselito Poblete <joselito.poblete@gmail.com>
2018-04-10 15:39:14 +0200

    FO-845 Fjern ubrukt kode

--- src/frontend/src/komponenter/home.tsx
+++ src/frontend/src/komponenter/home.tsx
@@ -1,18 +1,11 @@
 import * as React from 'react';
-import InformasjonsmodulDagpenger from './informasjonsmoduler/dagpenger/dagpenger';
-
 import Komigang from './komigang/kom-i-gang';
-import CV from './cv/cv';
-import Arbeidssoker from './arbeidssoker/arbeidssoker';
 
 function Home() {
     return (
-        <div>
+        <>
             <Komigang/>
-            <CV/>
-            <InformasjonsmodulDagpenger/>
-            <Arbeidssoker/>
-        </div>
+        </>
     );
 }
 
