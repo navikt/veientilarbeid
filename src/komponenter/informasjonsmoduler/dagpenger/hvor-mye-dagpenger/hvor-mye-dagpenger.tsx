e5eecf8db607f9787f0441ca8bc6d5d932cdbb09
Lars Andreas van Woensel Kooy Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2018-04-16 16:45:55 +0200

    FO-776 Legg til noe tabellstyling

--- src/komponenter/informasjonsmoduler/dagpenger/hvor-mye-dagpenger/hvor-mye-dagpenger.tsx
+++ src/komponenter/informasjonsmoduler/dagpenger/hvor-mye-dagpenger/hvor-mye-dagpenger.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { injectIntl, InjectedIntlProps } from 'react-intl';
 import TableBody from './table-body';
+import { Normaltekst } from 'nav-frontend-typografi';
 
 interface DummyProp {
     dummy?: string; // TypeScript klager hvis props kun er InjectedIntlProps
@@ -18,65 +19,41 @@ class HvorMyeDagpenger extends React.Component<Props> {
         const intl = this.props.intl;
 
         return (
-            <>
-                <div
-                    dangerouslySetInnerHTML={{
-                        __html: intl.messages['dagpenger.belop.innhold']
-                    }}
-                />
-                <table>
-                    <caption>{intl.messages['dagpenger.belop.tabell.caption']}</caption>
-                    <thead>
-                        <tr>
-                            <th>{intl.messages['dagpenger.belop.tabell.1.header']}</th>
-                            <th>{intl.messages['dagpenger.belop.tabell.2.header']}</th>
-                            <th>{intl.messages['dagpenger.belop.tabell.3.header']}</th>
-                        </tr>
-                    </thead>
-                    <TableBody
-                        antallRader={3}
-                        antallKolonner={5}
-                        getTabellverdi={(rad, kolonne) => this.getTabellverdi(rad, kolonne)}
-                    />
-                </table>
-                <div
-                    dangerouslySetInnerHTML={{
-                        __html: intl.messages['dagpenger.belop.ekstra']
-                    }}
-                />
-            </>
+            <div className="hvor-mye-dagpenger__wrapper">
+                <div className="hvor-mye-dagpenger">
+                    <Normaltekst>
+                        <div
+                            dangerouslySetInnerHTML={{
+                                __html: intl.messages['dagpenger.belop.innhold']
+                            }}
+                        />
+                    </Normaltekst>
+                    <table className="tabell-enkel blokk-l">
+                        <caption>{intl.messages['dagpenger.belop.tabell.caption']}</caption>
+                        <thead>
+                            <tr>
+                                <th>{intl.messages['dagpenger.belop.tabell.1.header']}</th>
+                                <th>{intl.messages['dagpenger.belop.tabell.2.header']}</th>
+                                <th>{intl.messages['dagpenger.belop.tabell.3.header']}</th>
+                            </tr>
+                        </thead>
+                        <TableBody
+                            antallRader={3}
+                            antallKolonner={5}
+                            getTabellverdi={(rad, kolonne) => this.getTabellverdi(rad, kolonne)}
+                        />
+                    </table>
+                    <Normaltekst>
+                        <div
+                            dangerouslySetInnerHTML={{
+                                __html: intl.messages['dagpenger.belop.ekstra']
+                            }}
+                        />
+                    </Normaltekst>
+                </div>
+            </div>
         );
     }
 }
-
-/*
-<tbody>
-                        <tr>
-                            <th>{intl.messages['dagpenger.belop.tabell.1.kol.1']}</th>
-                            <th>{intl.messages['dagpenger.belop.tabell.1.kol.2']}</th>
-                            <th>{intl.messages['dagpenger.belop.tabell.1.kol.3']}</th>
-                        </tr>
-                        <tr>
-                            <th>{intl.messages['dagpenger.belop.tabell.2.kol.1']}</th>
-                            <th>{intl.messages['dagpenger.belop.tabell.2.kol.2']}</th>
-                            <th>{intl.messages['dagpenger.belop.tabell.2.kol.3']}</th>
-                        </tr>
-                        <tr>
-                            <th>{intl.messages['dagpenger.belop.tabell.3.kol.1']}</th>
-                            <th>{intl.messages['dagpenger.belop.tabell.3.kol.2']}</th>
-                            <th>{intl.messages['dagpenger.belop.tabell.3.kol.3']}</th>
-                        </tr>
-                        <tr>
-                            <th>{intl.messages['dagpenger.belop.tabell.4.kol.1']}</th>
-                            <th>{intl.messages['dagpenger.belop.tabell.4.kol.2']}</th>
-                            <th>{intl.messages['dagpenger.belop.tabell.4.kol.3']}</th>
-                        </tr>
-                        <tr>
-                            <th>{intl.messages['dagpenger.belop.tabell.5.kol.1']}</th>
-                            <th>{intl.messages['dagpenger.belop.tabell.5.kol.2']}</th>
-                            <th>{intl.messages['dagpenger.belop.tabell.5.kol.3']}</th>
-                        </tr>
-                    </tbody>
- */
 
 export default injectIntl(HvorMyeDagpenger);
