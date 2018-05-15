2ddfa9d9fbae19ea928a16ebf8140670a66c6713
Lars Andreas van Woensel Kooy Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2018-05-15 16:35:20 +0200

    FO-756 Legg til test for resultatperioden

--- src/komponenter/informasjonsmoduler/dagpenger/soketidspunkt-for-dagpenger/soketidspunkt-resultat.tsx
+++ src/komponenter/informasjonsmoduler/dagpenger/soketidspunkt-for-dagpenger/soketidspunkt-resultat.tsx
@@ -4,6 +4,7 @@ import { InjectedIntlProps, injectIntl, FormattedMessage } from 'react-intl';
 import { momentIDag } from './moment-utils';
 import { Normaltekst } from 'nav-frontend-typografi';
 import Snakkeboble from 'nav-frontend-snakkeboble';
+import { formaterResultatperiode } from './soketidspunkt-resultat-utils';
 
 interface OwnProps {
     dato: Moment;
@@ -13,12 +14,6 @@ interface OwnProps {
 type Props = OwnProps & InjectedIntlProps;
 
 class SoketidspunktResultat extends React.Component<Props> {
-
-    resultatPeriode(periode: string): React.ReactNode {
-        return (
-            <FormattedMessage id="dagpenger.soketidspunkt.dato.resultat.periode" values={{periode}}/>
-        );
-    }
 
     byggResultattekst(): React.ReactNode {
         const maxDato = this.props.dato.clone().subtract(3, 'days');
@@ -33,21 +28,14 @@ class SoketidspunktResultat extends React.Component<Props> {
                     }}
                 />
             );
-        }
-        if (minDato.year() !== maxDato.year()) {
-            return this.resultatPeriode(`${minDato.format('LL')} - ${maxDato.format('LL')}.`);
-        } else if (minDato.month() !== maxDato.month()) {
-            const year = `${minDato.year()}`;
-            const minDatoStreng = minDato.format('LL').replace(year, '').trim();
-            const maxDatoStreng = maxDato.format('LL').replace(year, '').trim();
-            return this.resultatPeriode(`${minDatoStreng} - ${maxDatoStreng} ${year}.`);
         } else {
-            const year = minDato.year();
-            const month = minDato.format('MMMM');
-
-            return this.resultatPeriode(`${minDato.date()}. - ${maxDato.date()}. ${month} ${year}.`);
+            return (
+                <FormattedMessage
+                    id="dagpenger.soketidspunkt.dato.resultat.periode"
+                    values={{periode: formaterResultatperiode(minDato, maxDato)}}
+                />
+            );
         }
-
     }
 
     render() {
