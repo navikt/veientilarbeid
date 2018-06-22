526f2b3c1aaa545da9fa3f6ab6d4006acacf5561
Lars Andreas van Woensel Kooy Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2018-06-22 15:32:16 +0200

    FO-1102 Legg til mia

--- src/komponenter/ressurslenker/ressurslenker.tsx
+++ src/komponenter/ressurslenker/ressurslenker.tsx
@@ -1,13 +1,15 @@
 import * as React from 'react';
 import BliBedreJobbsoker from './bli-bedre-jobbsoker';
 import HvordanSokeJobber from './hvordan-soke-jobber';
+import MuligheterIArbeidsmarkedet from './muligheter-i-arbeidsmarkedet';
 
 export default function Ressurslenker() {
     // TODO I overskrift-blibedrejobbsoker brukes det nobreakspace implisitt. Burde bruke &nbsp; i alle
     // TODO mellomrom etter tankestreken.
     return (
-        <div className="hvordansokejobber-container">
-            <div className="hvordansokejobber">
+        <div className="ressurslenker-container">
+            <div className="ressurslenker">
+                <MuligheterIArbeidsmarkedet />
                 <HvordanSokeJobber />
                 <BliBedreJobbsoker />
             </div>
