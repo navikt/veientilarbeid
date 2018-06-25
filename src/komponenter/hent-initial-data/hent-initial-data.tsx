cae8b73c54e686abf941aa45918d222240846a01
Lars Andreas van Woensel Kooy Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2018-06-25 16:31:45 +0200

    FO-1232 Legg til feature toggling

--- src/komponenter/hent-initial-data/hent-initial-data.tsx
+++ src/komponenter/hent-initial-data/hent-initial-data.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { connect, Dispatch } from 'react-redux';
 import { hentOppfolging, selectOppfolging, State as OppfolgingState } from '../../ducks/oppfolging';
+import { hentFeatureToggles, selectFeatureToggles, State as FeatureTogglesState } from '../../ducks/feature-toggles';
 import { AppState } from '../../reducer';
 import Innholdslaster from '../innholdslaster/innholdslaster';
 import Feilmelding from '../feilmeldinger/feilmelding';
@@ -11,27 +12,31 @@ interface OwnProps {
 
 interface StateProps {
     oppfolging: OppfolgingState;
+    featureToggles: FeatureTogglesState;
 }
 
 interface DispatchProps {
     hentOppfolging: () => void;
+    hentFeatureToggles: () => Promise<void | {}>;
 }
 
 type Props = StateProps & DispatchProps & OwnProps;
 
 class HentInitialData extends React.Component<Props> {
     componentWillMount() {
-        this.props.hentOppfolging();
+        this.props.hentFeatureToggles().then(() =>
+            this.props.hentOppfolging()
+        );
     }
 
     render() {
-        const { oppfolging, children } = this.props;
+        const { oppfolging, featureToggles, children } = this.props;
 
         return (
             <Innholdslaster
                 feilmeldingKomponent={<Feilmelding tekstId="feil-i-systemene-beskrivelse"/>}
                 storrelse="XXL"
-                avhengigheter={[oppfolging]}
+                avhengigheter={[oppfolging, featureToggles]}
             >
                 {children}
             </Innholdslaster>
@@ -40,12 +45,13 @@ class HentInitialData extends React.Component<Props> {
 }
 
 const mapStateToProps = (state: AppState): StateProps => ({
-        oppfolging: selectOppfolging(state)
-    }
-);
+        oppfolging: selectOppfolging(state),
+        featureToggles: selectFeatureToggles(state),
+});
 
 const mapDispatchToProps = (dispatch: Dispatch<AppState>): DispatchProps => ({
-        hentOppfolging: () => dispatch(hentOppfolging())
+        hentOppfolging: () => dispatch(hentOppfolging()),
+        hentFeatureToggles: () => dispatch(hentFeatureToggles()),
     });
 
 export default connect(mapStateToProps, mapDispatchToProps)(HentInitialData);
