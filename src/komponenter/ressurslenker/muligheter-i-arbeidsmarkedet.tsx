2a279954c21f1b242880bfe537368488982af1b3
Lars Andreas van Woensel Kooy Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2018-06-25 16:38:37 +0200

    FO-1232 Ikke vis MiA hvis toggle er av

--- src/komponenter/ressurslenker/muligheter-i-arbeidsmarkedet.tsx
+++ src/komponenter/ressurslenker/muligheter-i-arbeidsmarkedet.tsx
@@ -1,29 +1,44 @@
 import * as React from 'react';
+import { connect } from 'react-redux';
 import { Element, Systemtittel } from 'nav-frontend-typografi';
 import { FormattedMessage } from 'react-intl';
 import Lenkepanel from 'nav-frontend-lenkepanel';
+import { AppState } from '../../reducer';
 
 const hvordansokejobber = require('./muligheter-i-arbeidsmarkedet.svg');
 
-export default function MuligheterIArbeidsmarkedet() {
-    if (!document.location.href.includes('visMia=true')) {
-        return null;
-    }
-    return (
-        <Lenkepanel className="ressurslenke" href="/mia/">
-            <img
-                src={hvordansokejobber}
-                alt="Forstørrelsesglass"
-                className="ressurslenke__illustrasjon"
-            />
-            <div className="ressurslenke__tekst">
-                <Systemtittel className="blokk-xs">
-                    <FormattedMessage id="mia-overskrift"/>
-                </Systemtittel>
-                <Element>
-                    <FormattedMessage id="mia-beskrivelse"/>
-                </Element>
-            </div>
-        </Lenkepanel>
-    );
+interface StateProps {
+    visMia: boolean;
 }
+
+class MuligheterIArbeidsmarkedet extends React.Component<StateProps> {
+    render() {
+        if (!this.props.visMia) {
+            return null;
+        }
+        return (
+            <Lenkepanel className="ressurslenke" href="/mia/">
+                <img
+                    src={hvordansokejobber}
+                    alt="Forstørrelsesglass"
+                    className="ressurslenke__illustrasjon"
+                />
+                <div className="ressurslenke__tekst">
+                    <Systemtittel className="blokk-xs">
+                        <FormattedMessage id="mia-overskrift"/>
+                    </Systemtittel>
+                    <Element>
+                        <FormattedMessage id="mia-beskrivelse"/>
+                    </Element>
+                </div>
+            </Lenkepanel>
+        );
+    }
+}
+
+
+const mapStateToProps = (state: AppState): StateProps => ({
+    visMia: state.featureToggles.data['veientilarbeid.mia'],
+});
+
+export default connect(mapStateToProps)(MuligheterIArbeidsmarkedet);
--- src/mocks/index.ts
+++ src/mocks/index.ts
@@ -1,11 +1,13 @@
 /*tslint:disable*/
 import {mock, respondWith, delayed } from './utils';
 import oppfolgingResponse from './oppfolging-mock';
-import {VEILEDERARBEIDSSOKER_URL, VEILARBOPPFOLGINGPROXY_URL} from '../ducks/api';
+import {VEILEDERARBEIDSSOKER_URL, VEILARBOPPFOLGINGPROXY_URL, FEATURE_URL} from '../ducks/api';
+import featureTogglesMock from './feature-toggles-mock';
 const teksterResponse = require('./tekster-mock.json');
 
 const MOCK_OPPFOLGING = true;
 const MOCK_TEKSTER = true;
+const MOCK_FEATURE_TOGGLES = true;
 
 if (MOCK_OPPFOLGING) {
     (mock as any).get(`${VEILARBOPPFOLGINGPROXY_URL}/oppfolging`, respondWith(delayed(1000, oppfolgingResponse)));
@@ -13,6 +15,10 @@ if (MOCK_OPPFOLGING) {
 
 if (MOCK_TEKSTER) {
     (mock as any).get(`${VEILEDERARBEIDSSOKER_URL}/tekster`, respondWith(delayed(1000, teksterResponse)));
+}
+
+if (MOCK_FEATURE_TOGGLES) {
+    (mock as any).get(`${FEATURE_URL}`, respondWith(delayed(1000, featureTogglesMock)));
 }
 
 (mock as any).mock('*', respondWith((url: string, config: {}) => mock.realFetch.call(window, url, config)));
