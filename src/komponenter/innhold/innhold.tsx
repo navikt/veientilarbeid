8d2799cf76cf7e166e5864e302bb75a14e75a358
Lars Andreas van Woensel Kooy Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2018-07-12 12:55:58 +0200

    FO-1305 Legg til meldekort

--- src/komponenter/innhold/innhold.tsx
+++ src/komponenter/innhold/innhold.tsx
@@ -2,12 +2,14 @@ import * as React from 'react';
 import Aktivitetsplan from '../aktivitetsplan/aktivitetsplan';
 import Ressurslenker from '../ressurslenker/ressurslenker';
 import Dagpenger from '../informasjonsmoduler/dagpenger/dagpenger';
+import Meldekort from '../meldekort/meldekort';
 
 function Innhold() {
     return (
         <div className="innhold__wrapper">
             <div className="innhold">
                 <Aktivitetsplan/>
+                <Meldekort/>
                 <Ressurslenker/>
                 <Dagpenger/>
             </div>
