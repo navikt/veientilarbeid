f739b5ca789e3f2586b6188add706adc78e2a4a4
Lars Andreas van Woensel Kooy Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2018-08-08 14:46:00 +0200

    FO-1360 Legg til varsel for brukere som kan reaktiveres

---
+++ src/komponenter/reaktivering-melding/reaktivering-melding.tsx
@@ -0,0 +1,38 @@
+import * as React from 'react';
+import { connect } from 'react-redux';
+import { AppState } from '../../reducer';
+import AlertStripe from 'nav-frontend-alertstriper';
+import { InjectedIntlProps, injectIntl } from 'react-intl';
+import { Normaltekst } from 'nav-frontend-typografi';
+
+interface StateProps {
+    kanReaktiveres: boolean;
+}
+
+type Props = StateProps & InjectedIntlProps;
+
+class ReaktiveringMelding extends React.Component<Props> {
+    render() {
+        const {kanReaktiveres, intl} = this.props;
+
+        if (!kanReaktiveres) {
+            return null;
+        }
+        return (
+            <AlertStripe type="stopp" className="reaktivering-melding">
+                <Normaltekst>
+                    {intl.messages['reaktivering-melding-tekst']}&ensp;
+                    <a href={intl.messages['reaktivering-melding-lenke-url']} className="lenke">
+                        {intl.messages['reaktivering-melding-lenke-tekst']}
+                    </a>
+                </Normaltekst>
+            </AlertStripe>
+        );
+    }
+}
+
+const mapStateToProps = (state: AppState): StateProps => ({
+    kanReaktiveres: !!state.oppfolging.data.kanReaktiveres,
+});
+
+export default connect(mapStateToProps)(injectIntl(ReaktiveringMelding));
