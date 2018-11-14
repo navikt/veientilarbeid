17be185212a673b5edd1c0db5fedcba05e5f811a
Joselito Poblete <joselito.poblete@gmail.com>
2018-11-14 12:08:03 +0100

    [FO-1655] La til feature toggle

--- src/komponenter/hent-initial-data/hent-initial-data.tsx
+++ src/komponenter/hent-initial-data/hent-initial-data.tsx
@@ -3,8 +3,11 @@ import { connect, Dispatch } from 'react-redux';
 import { hentOppfolging, selectOppfolging, State as OppfolgingState } from '../../ducks/oppfolging';
 import {
     hentFeatureToggles,
-    selectFeatureToggles, selectHentServicegruppekodeFeatureToggle,
-    State as FeatureTogglesState
+    selectFeatureToggles,
+    selectHentServicegruppekodeFeatureToggle,
+    selectHentSykeforloepMetadata,
+    servicekodeToggleKey,
+    State as FeatureTogglesState, sykeforloepMetadataToggleKey
 } from '../../ducks/feature-toggles';
 import { hentServicegruppe, selectServicegruppe, State as ServicegruppeState } from '../../ducks/servicegruppe';
 import { AppState } from '../../reducer';
@@ -23,6 +26,7 @@ interface StateProps {
     servicegruppe: ServicegruppeState;
     sykeforloepMetadata: SykeforloepMetadataState;
     featureToggleServicegruppe: boolean;
+    featureToggleSykeforloepMetadata: boolean;
 }
 
 interface DispatchProps {
@@ -37,21 +41,48 @@ type Props = StateProps & DispatchProps & OwnProps;
 class HentInitialData extends React.Component<Props> {
     componentWillMount() {
         this.props.hentFeatureToggles().then((response) => {
+
             this.props.hentOppfolging();
-            // this.props.hentSykeforloepMetadata();
-            const featureToggleServicegruppe = response['veientilarbeid.hentservicekode'];
+
+            const featureToggleSykeforloepMetadata = response[sykeforloepMetadataToggleKey];
+            if (featureToggleSykeforloepMetadata) {
+                this.props.hentSykeforloepMetadata();
+            }
+
+            const featureToggleServicegruppe = response[servicekodeToggleKey];
             if (featureToggleServicegruppe) {
                 this.props.hentServicegruppe();
             }
         });
     }
 
-    render() {
-        const {oppfolging, servicegruppe, featureToggleServicegruppe, children} = this.props;
+    finnAvhengigheter () {
+        const {
+            oppfolging,
+            servicegruppe,
+            sykeforloepMetadata,
+            featureToggleServicegruppe,
+            featureToggleSykeforloepMetadata,
+        } = this.props;
 
-        const avhengigheter = featureToggleServicegruppe
-            ? [oppfolging, servicegruppe]
-            : [oppfolging];
+        if (featureToggleServicegruppe && featureToggleSykeforloepMetadata) {
+            return [oppfolging, servicegruppe, sykeforloepMetadata];
+        } else if (featureToggleServicegruppe) {
+            return [oppfolging, servicegruppe];
+        } else if (featureToggleSykeforloepMetadata) {
+            return [oppfolging, sykeforloepMetadata];
+        } else {
+            return [oppfolging];
+        }
+    }
+
+    render() {
+        const {
+            children
+        } = this.props;
+
+        const avhengigheter = this.finnAvhengigheter();
+
         return (
             <Innholdslaster
                 feilmeldingKomponent={<Feilmelding tekstId="feil-i-systemene-beskrivelse"/>}
@@ -70,6 +101,7 @@ const mapStateToProps = (state: AppState): StateProps => ({
     servicegruppe: selectServicegruppe(state),
     sykeforloepMetadata: selectSykeforloepMetadata(state),
     featureToggleServicegruppe: selectHentServicegruppekodeFeatureToggle(state),
+    featureToggleSykeforloepMetadata: selectHentSykeforloepMetadata(state),
 });
 
 const mapDispatchToProps = (dispatch: Dispatch<AppState>): DispatchProps => ({
