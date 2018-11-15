cf518840705581f94638ec7de7153cf83ada8a6d
Joselito Poblete <joselito.poblete@gmail.com>
2018-11-15 10:14:51 +0100

    [FO-1614] Justering etter ffv

--- src/komponenter/start-fra-sykefravaer/overskrift/overskrift.tsx
+++ src/komponenter/start-fra-sykefravaer/overskrift/overskrift.tsx
@@ -3,13 +3,17 @@ import { FormattedMessage } from 'react-intl';
 import { Sidetittel } from 'nav-frontend-typografi';
 import Brodsmuler from '../../brodsmuler/brodsmuler';
 
-function Overskrift() {
+interface OwnProps {
+    tittelId: string;
+}
+
+function Overskrift({tittelId}: OwnProps) {
     return (
         <div className="overskrift-veientilarbeid-container">
             <div className="overskrift-veientilarbeid">
-                <Brodsmuler tittelId="overskrift-veientilarbeid"/>
+                <Brodsmuler tittelId="overskrift-oppfolging"/>
                 <Sidetittel className="overskrift-veientilarbeid__tittel">
-                    <FormattedMessage id="overskrift-frasykefravaer"/>
+                    <FormattedMessage id={tittelId}/>
                 </Sidetittel>
             </div>
         </div>
