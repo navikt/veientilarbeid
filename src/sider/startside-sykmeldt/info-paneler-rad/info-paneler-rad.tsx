3a2bca56b28d4fe741aff924ec8c37c9c4ce7f6b
Alexander Gård <alexanog@stud.ntnu.no>
2018-11-16 14:12:00 +0100

    FO-1651 Endret på lenker

--- src/sider/startside-sykmeldt/info-paneler-rad/info-paneler-rad.tsx
+++ src/sider/startside-sykmeldt/info-paneler-rad/info-paneler-rad.tsx
@@ -1,39 +1,8 @@
 import * as React from 'react';
-import { FormattedMessage, injectIntl, InjectedIntlProps } from 'react-intl';
-import { Normaltekst, Systemtittel } from 'nav-frontend-typografi';
 import './info-paneler-rad.less';
+import InfoPanel from './info-panel';
 
-interface InfoPanelProps {
-    bilde?: any; // tslint:disable-line
-    intl: any; // tslint:disable-line
-    tittelId: string;
-    tekstId: string;
-    lenkeTekstId: string;
-    lenkeUrlId: string;
-}
-
-const InfoPanel = (props: InfoPanelProps) => {
-
-    return (
-        <div className="info-panel">
-            <div className="info-panel--bilde-container"/>
-            <div className="info-panel--tekst-container">
-                <Systemtittel className="blokk-s">
-                    <FormattedMessage id={props.tittelId} />
-                </Systemtittel>
-                <Normaltekst className="blokk-m">
-                    <FormattedMessage id={props.tekstId} />
-                </Normaltekst>
-                <a href={props.intl.messages[props.lenkeUrlId]} className="lenke blokk-xl">
-                    <FormattedMessage id={props.lenkeTekstId} />
-                </a>
-            </div>
-        </div>
-    );
-
-};
-
-const InfoPanelerRad: React.SFC<InjectedIntlProps> = (props?: InjectedIntlProps) => {
+const InfoPanelerRad: React.SFC = () => {
 
     return (
         <div className="info-paneler-rad">
@@ -42,17 +11,15 @@ const InfoPanelerRad: React.SFC<InjectedIntlProps> = (props?: InjectedIntlProps)
                 tekstId="info-paneler-rad-hjelp-til-jobb-tekst"
                 lenkeTekstId="info-paneler-rad-hjelp-til-jobb-lenke-tekst"
                 lenkeUrlId="info-paneler-rad-hjelp-til-jobb-lenke-url"
-                intl={props!.intl}
             />
             <InfoPanel
                 tittelId="info-paneler-rad-ditt-sykefravaer-tittel"
                 tekstId="info-paneler-rad-ditt-sykefravaer-tekst"
                 lenkeTekstId="info-paneler-rad-ditt-sykefravaer-lenke-tekst"
                 lenkeUrlId="info-paneler-rad-ditt-sykefravaer-lenke-url"
-                intl={props!.intl}
             />
         </div>
     );
 };
 
-export default injectIntl(InfoPanelerRad);
+export default InfoPanelerRad;
