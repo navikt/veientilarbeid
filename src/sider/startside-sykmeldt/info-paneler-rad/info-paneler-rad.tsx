56db1dfe5cf5ea3431f972d56003aa0f207ea247
Alexander Gård <alexanog@stud.ntnu.no>
2018-11-19 10:15:26 +0100

    FO-1728 Endret tekster og fjernet info-panel i henhold til skisse

--- src/sider/startside-sykmeldt/info-paneler-rad/info-paneler-rad.tsx
+++ src/sider/startside-sykmeldt/info-paneler-rad/info-paneler-rad.tsx
@@ -8,13 +8,6 @@ const InfoPanelerRad: React.SFC = () => {
     return (
         <div className="info-paneler-rad">
             <InfoPanel
-                tittelId="info-paneler-rad-hjelp-til-jobb-tittel"
-                tekstId="info-paneler-rad-hjelp-til-jobb-tekst"
-                lenkeTekstId="info-paneler-rad-hjelp-til-jobb-lenke-tekst"
-                lenkeUrlId="info-paneler-rad-hjelp-til-jobb-lenke-url"
-                bilde={sykefravaer}
-            />
-            <InfoPanel
                 tittelId="info-paneler-rad-ditt-sykefravaer-tittel"
                 tekstId="info-paneler-rad-ditt-sykefravaer-tekst"
                 lenkeTekstId="info-paneler-rad-ditt-sykefravaer-lenke-tekst"
