4346c3a1b35f6e89e700010068127df6504f3573
Alexander Gård <alexander.gard@nav.no>
2018-11-28 12:22:51 +0100

    FO-1696 Fjernet kall til /veilarbregistrering/api/registrering
    Kallet til registrering skal flyttes til tiltakinfo, og skal derfor ikke lenger gjøres i veientilarbeid

--- src/ducks/actions.ts
+++ src/ducks/actions.ts
@@ -3,13 +3,11 @@ import { ActionTypes as FeatureTogglesActionTypes } from './feature-toggles';
 import { ActionTypes as ServicegruppeActionTypes } from './servicegruppe';
 import { ActionTypes as SykeforloepMetadataActionTypes } from './sykeforloep-metadata';
 import { ActionTypes as JobbsokerbesvarelseActionTypes } from './jobbsokerbesvarelse';
-import { ActionTypes as RegistreringActionTypes } from './registrering';
 
 type ActionsTypes = OppfolgingActionTypes
     | FeatureTogglesActionTypes
     | ServicegruppeActionTypes
     | SykeforloepMetadataActionTypes
-    | JobbsokerbesvarelseActionTypes
-    | RegistreringActionTypes;
+    | JobbsokerbesvarelseActionTypes;
 
 export default ActionsTypes;
--- src/sider/startside-sykmeldt/info-paneler-rad/info-paneler-rad.tsx
+++ src/sider/startside-sykmeldt/info-paneler-rad/info-paneler-rad.tsx
@@ -3,73 +3,28 @@ import './info-paneler-rad.less';
 import InfoPanel from './info-panel';
 import sykefravaerBilde from './sykefravaer.svg';
 import hjelpTilJobbBilde from './hjelp-til-jobb.svg';
-import { AppState } from '../../../reducer';
-import {
-    FremtidigSituasjonSvar,
-    hentRegistrering,
-    selectRegistrering,
-    State as RegistreringState
-} from '../../../ducks/registrering';
-import { connect, Dispatch } from 'react-redux';
-import { InjectedIntlProps, injectIntl } from 'react-intl';
 
-interface StateProps {
-    registrering: RegistreringState;
-}
+const InfoPanelerRad: React.SFC = () => {
 
-interface DispatchProps {
-    hentRegistrering: () => void;
-}
+    return (
+        <div className="info-paneler-rad">
+            <InfoPanel
+                tittelId="info-paneler-rad-hjelp-til-jobb-tittel"
+                tekstId="info-paneler-rad-hjelp-til-jobb-tekst"
+                lenkeTekstId="info-paneler-rad-hjelp-til-jobb-lenke-tekst"
+                lenkeUrlId="info-paneler-rad-hjelp-til-jobb-lenke-url"
+                bilde={hjelpTilJobbBilde}
+            />
+            <InfoPanel
+                tittelId="info-paneler-rad-ditt-sykefravaer-tittel"
+                tekstId="info-paneler-rad-ditt-sykefravaer-tekst"
+                lenkeTekstId="info-paneler-rad-ditt-sykefravaer-lenke-tekst"
+                lenkeUrlId="info-paneler-rad-ditt-sykefravaer-lenke-url"
+                bilde={sykefravaerBilde}
+            />
+        </div>
+    );
 
-type Props = StateProps & DispatchProps & InjectedIntlProps;
+};
 
-function lagTiltaksInfoLenke(tiltaksInfoLenkeBase: string, fremtidigSituasjon?: FremtidigSituasjonSvar): string {
-    return tiltaksInfoLenkeBase + (fremtidigSituasjon ? `?fremdtidigSituasjon=${fremtidigSituasjon}` : '');
-}
-
-class InfoPanelerRad extends React.Component<Props> {
-
-    componentDidMount() {
-        this.props.hentRegistrering();
-    }
-
-    render() {
-
-        const {data} = this.props.registrering;
-
-        const fremdtidigSituasjon = data ? data.registrering.besvarelse.fremtidigSituasjon : undefined;
-        const tiltaksInfoLenkeBase = this.props.intl.messages['info-paneler-rad-hjelp-til-jobb-lenke-url'];
-
-        const tiltaksInfoLenke = lagTiltaksInfoLenke(tiltaksInfoLenkeBase, fremdtidigSituasjon);
-
-        return (
-            <div className="info-paneler-rad">
-                <InfoPanel
-                    tittelId="info-paneler-rad-hjelp-til-jobb-tittel"
-                    tekstId="info-paneler-rad-hjelp-til-jobb-tekst"
-                    lenkeTekstId="info-paneler-rad-hjelp-til-jobb-lenke-tekst"
-                    lenkeUrl={tiltaksInfoLenke}
-                    bilde={hjelpTilJobbBilde}
-                />
-                <InfoPanel
-                    tittelId="info-paneler-rad-ditt-sykefravaer-tittel"
-                    tekstId="info-paneler-rad-ditt-sykefravaer-tekst"
-                    lenkeTekstId="info-paneler-rad-ditt-sykefravaer-lenke-tekst"
-                    lenkeUrlId="info-paneler-rad-ditt-sykefravaer-lenke-url"
-                    bilde={sykefravaerBilde}
-                />
-            </div>
-        );
-    }
-
-}
-
-const mapStateToProps = (state: AppState): StateProps => ({
-    registrering: selectRegistrering(state),
-});
-
-const mapDispatchToProps = (dispatch: Dispatch<AppState>): DispatchProps => ({
-    hentRegistrering: () => dispatch(hentRegistrering()),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(injectIntl(InfoPanelerRad));
+export default InfoPanelerRad;
