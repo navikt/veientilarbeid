99dd11c2abc9054174eb36bb5976d0346622fea9
Joselito Poblete <joselito.poblete@gmail.com>
2018-11-28 16:31:27 +0100

    FO-1756 Hent sykmeldtinfo fra veilarbregistrering

--- src/ducks/api.ts
+++ src/ducks/api.ts
@@ -4,7 +4,7 @@ import { alleFeatureToggles } from './feature-toggles';
 export const VEILARBOPPFOLGINGPROXY_URL = '/veilarboppfolging/api';
 export const FEATURE_URL = '/veientilarbeid/api/feature';
 export const SERVICEGRUPPE_URL = '/veilarbtiltakinfo/api/servicegruppekode';
-export const SYKEFORLOEP_METADATA_URL = '/syforest/sykeforloep/metadata';
+export const SYKEFORLOEP_METADATA_URL = '/veilarbregistrering/api/sykmeldtinfodata';
 export const JOBBSOKERBESVARELSE_URL = '/veilarbjobbsokerkompetanse/api/hent';
 
 export const getCookie = (name: string) => {
