a1bf861ab4b6df0c8b3ab5a73ba2954922d4836d
Alexander Gård <alexander.gard@nav.no>
2018-12-03 13:52:59 +0100

    FO-1788 Gjorde klart oppsett for deploy til heroku

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import App from './app';
 
 import './index.less';
 
-if (process.env.NODE_ENV === 'development') {
+if (process.env.REACT_APP_MOCK) {
     console.log('=========================='); /*tslint:disable-line:no-console*/
     console.log('======= DEVELOPMENT ======'); /*tslint:disable-line:no-console*/
     console.log('=========================='); /*tslint:disable-line:no-console*/
