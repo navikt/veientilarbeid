4780d8b8c81461c12c241d90dda037893e116702
Terese Haug <tereshh@gmail.com>
2019-02-04 12:15:22 +0100

    FO-1880 Stillingsøk. Tilrettelegger for skjermleser

--- src/komponenter/stillingsok/stillingsok.tsx
+++ src/komponenter/stillingsok/stillingsok.tsx
@@ -18,57 +18,57 @@ interface InputState {
 type Props = DummyProp & InjectedIntlProps;
 
 class StillingSok extends React.Component<Props, InputState> {
+
     constructor(props: Props) {
         super(props);
         this.state = {
             inputValue: ''
         };
         this.handleChange = this.handleChange.bind(this);
-        this.handlePressEnter = this.handlePressEnter.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleChange(e: React.FormEvent<HTMLInputElement>) {
-        e.preventDefault();
         this.setState({
             inputValue: e.currentTarget.value
         });
     }
 
-    handlePressEnter(e: React.KeyboardEvent, url: string) {
-        if (e.key === 'Enter') {
-           window.location.href = url;
-        }
+    handleSubmit(e: React.FormEvent<HTMLFormElement>, url: string) {
+        e.preventDefault();
+        location.href = url;
     }
 
     render() {
-        const { inputValue } = this.state;
         const intl = this.props.intl;
+        const { inputValue } = this.state;
         const URL = `${STILLINGSOK_URL}/?q=${inputValue}`;
 
         return (
             <div className="stillingsok">
-
                 <Innholdstittel className="stillingsok__tittel blokk-s">
                     <FormattedMessage id="stillingsok-overskrift" />
                 </Innholdstittel>
-
-                <div className="input-container">
+                <form
+                    className="input-container"
+                    role="search"
+                    onSubmit={event => this.handleSubmit(event, URL)}
+                >
                     <Input
                         className="sok-stillinger-input"
-                        label=""
+                        type="search"
+                        label="Søk:"
+                        aria-label={intl.messages['stillingsok-placeholder']}
                         placeholder={intl.messages['stillingsok-placeholder']}
                         onChange={event => this.handleChange(event)}
-                        onKeyPress={(event) => this.handlePressEnter(event, URL)}
                     />
-                    <div className="sok-stillinger-knapp">
-                        <a className="knapp knapp--hoved" href={URL}>
-                            {inputValue === ''
-                                ? <FormattedMessage id="stillingsok-knapp-sok-alle" />
-                                : <FormattedMessage id="stillingsok-knapp-sok" />
-                            }
-                        </a>
-                    </div>
-                </div>
+                    <button className="sok-stillinger-knapp knapp knapp--hoved" type="submit">
+                        {inputValue === ''
+                            ? <FormattedMessage id="stillingsok-knapp-sok-alle" />
+                            : <FormattedMessage id="stillingsok-knapp-sok" />
+                        }
+                    </button>
+                </form>
             </div>
         );
     }
