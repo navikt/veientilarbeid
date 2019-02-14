ce859294e10bad01c989bc4c781c70fb73c74a7c
Kjell Arne Brødreskift <kjarbr@gmail.com>
2019-02-14 11:45:57 +0100

    FO-1900. Laget ny komponent for dagpenger og fjernet gammel kode.

---
+++ src/komponenter/dagpenger/dagpenger.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import { injectIntl, FormattedMessage } from 'react-intl';
+import { Innholdstittel, Normaltekst } from 'nav-frontend-typografi';
+
+import './dagpenger.less';
+import { Hovedknapp } from 'nav-frontend-knapper';
+
+const Dagpenger = injectIntl(({intl}) => {
+
+    const handleButtonClick = () => {
+        window.location.href = intl.formatMessage({id: 'dagpenger-lenke-url'});
+    };
+
+    return (
+        <section className="dagpenger">
+            <Innholdstittel tag="h1" className="blokk-xs">
+                <FormattedMessage id="dagpenger-tittel"/>
+            </Innholdstittel>
+            <Normaltekst className="blokk-m dagpenger__tekst">
+                <FormattedMessage id="dagpenger-tekst"/>
+            </Normaltekst>
+            <Hovedknapp onClick={handleButtonClick}>
+                <FormattedMessage id="dagpenger-lenke-tekst"/>
+            </Hovedknapp>
+        </section>
+    );
+});
+
+export default Dagpenger;
