845a87240b547a76366ea961ede0ebdd0c57c719
Johan Rusvik <johan.rusvik@gmail.com>
2019-02-15 09:47:15 +0100

    KAIZEN-0 Fjener utdatert TODO

--- src/routes.tsx
+++ src/routes.tsx
@@ -15,7 +15,6 @@ type AllProps = StateProps & RouteComponentProps<any>; // tslint:disable-line
 
 class Routes extends React.Component<AllProps> {
     render() {
-        // TODO: Bytt verdi når beregning av gjenstående sykedager er på plass
         const erSykmeldtMedArbeidsgiver = this.props.sykmeldtInfo.data.erSykmeldtMedArbeidsgiver;
         const { location } = this.props;
         const search = location.search;
