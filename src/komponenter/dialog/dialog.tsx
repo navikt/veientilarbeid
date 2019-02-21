662edf2c2538cd470e7d3f30a4a622cb1ac164e9
Stian Surén <stian.suren@bekk.no>
2019-02-21 13:30:06 +0100

    FO-1935 Logging på inngangen til dialogen

--- src/komponenter/dialog/dialog.tsx
+++ src/komponenter/dialog/dialog.tsx
@@ -4,12 +4,23 @@ import LenkepanelBase from 'nav-frontend-lenkepanel';
 import { Systemtittel } from 'nav-frontend-typografi';
 import './dialog.less';
 import dialogIkon from './dialog.svg';
+import { gaTilDialog } from '../../metrics';
 
 class Dialog extends React.Component  {
     render() {
+
+        const linkCreator = (props: {}) => {
+          return <a onClick={gaTilDialog} {...props}/>;
+        };
+
         return (
             <section className="dialog">
-                <LenkepanelBase href="/aktivitetsplan/dialog" tittelProps="undertittel" border={true}>
+                <LenkepanelBase
+                    href="/aktivitetsplan/dialog"
+                    tittelProps="undertittel"
+                    linkCreator={linkCreator}
+                    border={true}
+                >
                     <div className="dialog__innhold">
                         <img
                             src={dialogIkon}
