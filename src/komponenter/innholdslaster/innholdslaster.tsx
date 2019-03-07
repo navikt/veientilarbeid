0feda40aed4722dcac3791af88742115350a4480
Terese Haug <tereshh@gmail.com>
2019-03-07 09:34:03 +0100

    FO-1078 Tar i bruk type DataElement i Innholdslaster og forbedrer ventPa

--- src/komponenter/innholdslaster/innholdslaster.tsx
+++ src/komponenter/innholdslaster/innholdslaster.tsx
@@ -1,24 +1,24 @@
 import * as React from 'react';
 import './innholdslaster.less';
 import Laster from './innholdslaster-laster';
-import { STATUS } from '../../ducks/api';
+import { DataElement, STATUS } from '../../ducks/api';
 
 const array = (value: {}) => (Array.isArray(value) ? value : [value]);
 
 const harStatus = (...status: string[]) =>
     (element: {status: string}) => array(status).toString().includes(element.status);
 
-const noenHarFeil = (avhengigheter: any[]) => avhengigheter && avhengigheter.some(harStatus(STATUS.ERROR));
-const alleLastet = (avhengigheter: any[]) => avhengigheter && avhengigheter.every(harStatus(STATUS.OK));
-const alleVentetPa = (ventPa: any[]) => ventPa && ventPa.every(harStatus(STATUS.OK, STATUS.ERROR));
+const noenHarFeil = (avhengigheter: DataElement[]) => avhengigheter && avhengigheter.some(harStatus(STATUS.ERROR));
+const alleLastet = (avhengigheter: DataElement[]) => avhengigheter && avhengigheter.every(harStatus(STATUS.OK));
+const alleVentetPa = (ventPa?: DataElement[]) => ventPa ? ventPa.every(harStatus(STATUS.OK, STATUS.ERROR)) : true;
 
 const alleLastetEllerReloading = (avhengigheter: any[]) => (
     avhengigheter && avhengigheter.every(harStatus(STATUS.OK, STATUS.RELOADING))
 );
 
 interface InnholdslasterProps {
-    avhengigheter: {status: string}[];
-    ventPa?: {status: string}[];
+    avhengigheter: DataElement[];
+    ventPa?: DataElement[];
     feilmeldingKomponent?: React.ReactNode | React.ReactChild;
     storrelse?: 'XXS' | 'XS' | 'S' | 'M' | 'L' | 'XL' | 'XXL' | 'XXXL';
 }
@@ -71,7 +71,7 @@ class Innholdslaster extends React.Component<InnholdslasterProps, Innholdslaster
     render() {
         const { avhengigheter, ventPa, feilmeldingKomponent, storrelse } = this.props;
 
-        if (alleLastet(avhengigheter) && (!ventPa || alleVentetPa(ventPa))) {
+        if (alleLastet(avhengigheter) && alleVentetPa(ventPa)) {
             // Alle avhengigheter lastet inn uten problemer og ventPa er ferdig (enten OK eller FEILET){
             return this.renderChildren();
 
