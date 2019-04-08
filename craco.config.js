1510e9828edc429c34d045bf0bd879119b77565a
Johan Rusvik <johan.rusvik@gmail.com>
2019-04-08 14:10:17 +0200

    FO-2003 Disabler chunking

--- craco.config.js
+++ craco.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const CracoLessPlugin = require('craco-less');
-const NpmImportPlugin = require('less-plugin-npm-import');
 const BUILD_PATH = path.resolve(__dirname, './build');
 
 const removeCssHashPlugin = {
@@ -17,31 +16,33 @@ const removeCssHashPlugin = {
 
             if (options.filename && options.filename.endsWith('.css')) {
                 options.filename = "static/css/[name].css";
-                options.chunkFilename = "static/css/[name].chunk.css";
             }
 
         });
+
         return webpackConfig;
     }
 };
 
 module.exports = {
     plugins: [
-        { plugin: CracoLessPlugin,
-            options: {
-                lessLoaderOptions: {
-                    loader: new NpmImportPlugin({ prefix: '~' })
-                }
-            }
-        },
+        { plugin: CracoLessPlugin },
         { plugin: removeCssHashPlugin },
     ],
     webpack: {
         configure: {
+            optimization: {
+                splitChunks: {
+                    cacheGroups: {
+                        default: false,
+                        vendors: false
+                    },
+                },
+                runtimeChunk: false
+            },
             output: {
                 path: BUILD_PATH,
                 filename: 'static/js/[name].js',
-                chunkFilename: 'static/js/[name].chunk.js',
             },
         }
     }
