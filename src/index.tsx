84ff89d7e068d84c0511407edfc91aa34bcbf84f
Johan Rusvik <johan.rusvik@gmail.com>
2019-04-09 09:51:24 +0200

    FO-2003 Skrur på mock alltid for å enkelt kunne kjøre opp lokalt

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,11 @@ import 'react-app-polyfill/ie11';
 import App from './app';
 
 import './index.less';
-import { erDemo } from './demo/demo-state';
 import NAVSPA from './NAVSPA';
 
-if (process.env.REACT_APP_MOCK && !erDemo()) {
-    console.log('=========================='); /*tslint:disable-line:no-console*/
-    console.log('======= DEVELOPMENT ======'); /*tslint:disable-line:no-console*/
-    console.log('=========================='); /*tslint:disable-line:no-console*/
-    require('./mocks');
-}
+console.log('=========================='); /*tslint:disable-line:no-console*/
+console.log('======= DEVELOPMENT ======'); /*tslint:disable-line:no-console*/
+console.log('=========================='); /*tslint:disable-line:no-console*/
+require('./mocks');
 
 NAVSPA.eksporter('vta', App);
-
