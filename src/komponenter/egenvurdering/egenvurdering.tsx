229c131b45de0dc37825ffd9fcc3c85c098a749e
Johan Rusvik <johan.rusvik@gmail.com>
2019-04-17 10:23:27 +0200

    FO-2150 Konverterer til functional component

--- src/komponenter/egenvurdering/egenvurdering.tsx
+++ src/komponenter/egenvurdering/egenvurdering.tsx
@@ -5,33 +5,27 @@ import { injectIntl, FormattedMessage, InjectedIntlProps } from 'react-intl';
 
 import './egenvurdering.less';
 
-class Egenvurdering extends React.Component<InjectedIntlProps> {
+const Egenvurdering = ({intl}: InjectedIntlProps) => {
 
-    constructor(props: InjectedIntlProps) {
-        super(props);
-    }
-
-    handleButtonClick = () => {
-        window.location.href = this.props.intl.formatMessage({id: 'egenvurdering-lenke-url'});
+    const handleButtonClick = () => {
+        window.location.href = intl.formatMessage({id: 'egenvurdering-lenke-url'});
     };
 
-    render() {
-        return (
-            <section className="egenvurdering blokk-m">
-                <div className="innhold">
-                    <Systemtittel tag="h1" className="blokk-xs">
-                        <FormattedMessage id="egenvurdering-tittel"/>
-                    </Systemtittel>
-                    <Normaltekst className="blokk-m egenvurdering__tekst">
-                        <FormattedMessage id="egenvurdering-tekst"/>
-                    </Normaltekst>
-                    <Hovedknapp onClick={this.handleButtonClick}>
-                        <FormattedMessage id="egenvurdering-lenke-tekst"/>
-                    </Hovedknapp>
-                </div>
-            </section>
-        );
-    }
-}
+    return (
+        <section className="egenvurdering blokk-m">
+            <div className="innhold">
+                <Systemtittel tag="h1" className="blokk-xs">
+                    <FormattedMessage id="egenvurdering-tittel"/>
+                </Systemtittel>
+                <Normaltekst className="blokk-m egenvurdering__tekst">
+                    <FormattedMessage id="egenvurdering-tekst"/>
+                </Normaltekst>
+                <Hovedknapp onClick={handleButtonClick}>
+                    <FormattedMessage id="egenvurdering-lenke-tekst"/>
+                </Hovedknapp>
+            </div>
+        </section>
+    );
+};
 
 export default injectIntl(Egenvurdering);
