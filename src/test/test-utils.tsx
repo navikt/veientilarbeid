6895623854db324e2963613fc51b65889e5f9d70
Johan Rusvik <johan.rusvik@gmail.com>
2019-04-23 16:02:16 +0200

    FO-2187 Fjerner ubrukt kode

--- src/test/test-utils.tsx
+++ src/test/test-utils.tsx
@@ -1,33 +1,12 @@
 import * as React from 'react';
-import * as sinon from 'sinon';
 import { mount } from 'enzyme';
-import { shallowWithIntl } from 'enzyme-react-intl';
 import getStore from '../store';
 import { AppState } from '../reducer';
 import IntlProvider from '../Intl-provider';
-import { Provider, Store } from 'react-redux';
-
-interface ElementWithStore {
-    store: Store<AppState>;
-}
+import { Provider } from 'react-redux';
+import { Store } from 'redux';
 
 export const store = getStore();
-
-export function shallowwithStoreAndIntl(children: React.ReactNode) {
-    return shallowWithIntl(React.cloneElement(children, {
-        store
-    })).dive().dive();
-}
-
-export function mountWithStoreAndIntl(children: React.ReactElement<ElementWithStore>, withStore?: Store<AppState>) {
-    return mount(
-        <Provider store={withStore || store}>
-            <IntlProvider>
-                {children}
-            </IntlProvider>
-        </Provider>
-    );
-}
 
 export function mountWithStore(children: React.ReactNode, withStore?: Store<AppState>) {
     return mount(
@@ -37,25 +16,4 @@ export function mountWithStore(children: React.ReactNode, withStore?: Store<AppS
             </IntlProvider>
         </Provider>
     );
-}
-
-export function stubFetchWithResponse(response: {}): Promise {
-    return sinon.stub(global, 'fetch').callsFake(() =>
-        Promise.resolve({status: 200, ok: true, json: () => (response)}));
-}
-
-export function stubFetchWithErrorResponse(status?: number) {
-    return sinon.stub(global, 'fetch').callsFake(() =>
-        Promise.resolve({status: status || 500, text: () => Promise.resolve('Skal kaste feil')}));
-}
-
-export function promiseWithSetTimeout() {
-    return new Promise(resolve => setTimeout(resolve, 0));
-}
-
-export function resetAndMakeHrefWritable() {
-    return Object.defineProperty(document.location, 'href', {
-        writable: true,
-        value: ''
-    });
 }
