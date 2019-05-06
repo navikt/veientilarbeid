db99f57c9898f52256a733bf941e1d99c7b4a6d0
Alexander Svendsen <alexandersvendsen1990@gmail.com>
2019-05-06 15:07:55 +0200

    KAIZEN-0: Quickfix alltid vis bv inngangen

--- src/innhold/innhold-logikk.tsx
+++ src/innhold/innhold-logikk.tsx
@@ -53,7 +53,6 @@ class InnholdLogikk extends React.Component<StateProps> {
 
         const skalViseEgenvurderingLenke = (
             !harEgenvurderingbesvarelse &&
-            opprettetRegistreringDato >= LANSERINGSDATO &&
             !reservasjonKRR &&
             (foreslattInnsatsgruppe === ForeslattInnsatsgruppe.STANDARD_INNSATS ||
                 foreslattInnsatsgruppe === ForeslattInnsatsgruppe.SITUASJONSBESTEMT_INNSATS)
