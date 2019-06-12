3492a983e6ee022704307525a074476c17a862ca
Geir Gåsodden <geir.gasodden@pythonia.no>
2019-06-12 12:34:22 +0200

    [FO-2323] - mer lintfix

--- src/innhold/innhold-logikk-niva4.tsx
+++ src/innhold/innhold-logikk-niva4.tsx
@@ -37,8 +37,9 @@ const InnholdLogikkNiva4 = ({
     const innsatsgruppe = innsatsgruppeData ? innsatsgruppeData.servicegruppe : null;
 
     const hotjarTrigger = () => {
-        if (window && window.hj) {
-            window.hj('trigger', 'vta-dittnav-ny');
+        if (typeof window !== 'undefined' && window.hasOwnProperty('hj')) {
+            const hotjar = 'hj';
+            window[hotjar]('trigger', 'vta-dittnav-ny');
         }
     };
 
