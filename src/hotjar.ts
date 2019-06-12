b720f36ace8e31070c814a4ac1d0122c59e0e462
Geir Gåsodden <geir.gasodden@pythonia.no>
2019-06-12 14:22:38 +0200

    [FO-2323] - endrer trigger til vta-dittnav-generell

--- src/hotjar.ts
+++ src/hotjar.ts
@@ -1,6 +1,6 @@
 export const hotjarTrigger = () => {
   if (typeof window !== 'undefined' && window.hasOwnProperty('hj')) {
       const hotjar = 'hj';
-      window[hotjar]('trigger', 'vta-dittnav-ny');
+      window[hotjar]('trigger', 'vta-dittnav-generell');
   }
 };
