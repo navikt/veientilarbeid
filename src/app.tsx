fb320b152f9a58d2a7f1d5c096e841854ea238c3
Johan Rusvik <johan.rusvik@gmail.com>
2019-06-19 09:32:10 +0200

    KAIZEN-0 Oppdaterer Error Boundary

--- src/app.tsx
+++ src/app.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Provider } from 'react-redux';
 import getStore from './store';
 import AutentiseringsInfoFetcher from './komponenter/hent-initial-data/autentiseringsInfoFetcher';
+import ErrorBoundary from './error-boundary';
 
 const store = getStore();
 
@@ -10,7 +11,9 @@ class App extends React.Component {
     render() {
         return (
             <Provider store={store}>
-                <AutentiseringsInfoFetcher/>
+                <ErrorBoundary>
+                    <AutentiseringsInfoFetcher/>
+                </ErrorBoundary>
             </Provider>
         );
     }
