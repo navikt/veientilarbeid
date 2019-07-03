8621297abb7e3f46c0db11e93142aecdeda377c5
Geir Gåsodden <geir.gasodden@pythonia.no>
2019-07-03 13:26:56 +0200

    [FO-2345] - rydder opp litt

--- src/innhold/innhold-view.tsx
+++ src/innhold/innhold-view.tsx
@@ -1,13 +1,10 @@
 import * as React from 'react';
-import { Systemtittel } from 'nav-frontend-typografi';
 import Rad from './rad';
 import AapRad from '../komponenter/aap/aap';
 import Dialog from '../komponenter/dialog/dialog';
 import Banner from '../komponenter/banner/banner';
 import Meldekort from '../komponenter/meldekort/meldekort';
 import DittSykefravaer from '../komponenter/ditt-sykefravaer/ditt-sykefravaer';
-import Dagpenger from '../komponenter/dagpenger/dagpenger';
-import AlleSkjema from '../komponenter/alleskjema/alleskjema';
 import Tiltakinfo from '../komponenter/tiltakinfo/tiltakinfo';
 import OkonomiRad from '../komponenter/okonomi/okonomi-rad';
 import ReaktiveringMelding from '../komponenter/reaktivering-melding';
@@ -15,10 +12,10 @@ import Aktivitetsplan from '../komponenter/aktivitetsplan/aktivitetsplan';
 import RessurslenkerJobbsok from '../komponenter/ressurslenker-jobbsok/ressurslenker-jobbsok';
 import Egenvurdering from '../komponenter/egenvurdering/egenvurdering';
 import Motestotte from '../komponenter/motestotte/motestotte';
+import OkonomiRadDagpenger from '../komponenter/okonomi/okonomi-rad-dagpenger';
 import './innhold.less';
 import KrrMelding from '../komponenter/krr-melding/krr-melding';
 import { erMikrofrontend } from '../utils/app-state-utils';
-import tekster from '../tekster/tekster';
 
 interface OwnProps {
     erSykmeldtMedArbeidsgiver: boolean;
@@ -62,10 +59,7 @@ export default ({erSykmeldtMedArbeidsgiver,
                 {skalViseTiltaksinfoLenke ? <Tiltakinfo/> : null}
             </Rad>
 
-            { erSykmeldtMedArbeidsgiver ? <Rad><OkonomiRad/></Rad> : <Rad><Systemtittel tag="h2" className="dagpenger__heading blokk-s">
-                {tekster['dagpenger-heading-tekst']}
-            </Systemtittel><div className="tokol"><Dagpenger/><AlleSkjema/></div></Rad>}
-            
+            { erSykmeldtMedArbeidsgiver ? <Rad><OkonomiRad/></Rad> : <OkonomiRadDagpenger />}
         </>
     );
 };
