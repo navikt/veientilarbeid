ac12be78b6bfbceb4af8ff9b80e62ebd8ecead59
Johan Rusvik <johan.rusvik@gmail.com>
2019-08-01 09:58:41 +0200

    KAIZEN-0 Refaktorerer

--- src/metrics/metrics.ts
+++ src/metrics/metrics.ts
@@ -10,6 +10,10 @@ const w = (window as any); // tslint:disable-line:no-any
 const domene = 'veientilarbeid';
 
 const logEvent = w.frontendlogger ? (navn: string, fields: object, tags: object) => {
+    if (erDemo()) {
+        return;
+    }
+
     const metrikkId = `${domene}.${navn}`;
 
     const metrikkAlleredeOpprettet = createdMetrics.alreadyCreated(metrikkId);
@@ -25,123 +29,83 @@ const logEvent = w.frontendlogger ? (navn: string, fields: object, tags: object)
 } : () => { return; };
 
 export const seVeientilarbeid = (erSykmeldtMedArbeidsgiver: boolean, servicegruppe: ServicegruppeOrNull, microfrontend: boolean) => {
-    if (!erDemo()) {
-        logEvent('seveientilarbeid',
-            {erSykmeldtMedArbeidsgiverField: erSykmeldtMedArbeidsgiver, servicegruppeField: servicegruppe, microfrontendField: microfrontend},
-            {erSykmeldtMedArbeidsgiver, servicegruppeTag: servicegruppe, microfrontendTag: microfrontend});
-    }
+    logEvent('seveientilarbeid',
+        {erSykmeldtMedArbeidsgiverField: erSykmeldtMedArbeidsgiver, servicegruppeField: servicegruppe, microfrontendField: microfrontend},
+        {erSykmeldtMedArbeidsgiver, servicegruppeTag: servicegruppe, microfrontendTag: microfrontend});
 };
 
 export const seVeientilarbeidNiva3 = () => {
-    if (!erDemo()) {
-        logEvent('seveientilarbeidniva3', {}, {});
-    }
+    logEvent('seveientilarbeidniva3', {}, {});
 };
 
 export const klikkPaSokLedigeStillinger = (servicegruppe: String | null) => {
-    if (!erDemo()) {
-        logEvent('sokledigestillinger', {innsatsgruppeField: servicegruppe}, {innsatsgruppeTag: servicegruppe});
-    }
+    logEvent('sokledigestillinger', {innsatsgruppeField: servicegruppe}, {innsatsgruppeTag: servicegruppe});
 };
 
 export const gaTilAktivitetsplan = (servicegruppe: String | null) => {
-    if (!erDemo()) {
-        logEvent('gatilaktivitetsplan', {innsatsgruppeField: servicegruppe}, {innsatsgruppeTag: servicegruppe});
-    }
+    logEvent('gatilaktivitetsplan', {innsatsgruppeField: servicegruppe}, {innsatsgruppeTag: servicegruppe});
 };
 
 export const gaTilDialog = (antall: number, servicegruppe: string | null) => {
-    if (!erDemo()) {
-        logEvent('gatildialog', {antallField: antall, innsatsgruppeField: servicegruppe}, {antallTag: antall, innsatsgruppeTag: servicegruppe});
-    }
+    logEvent('gatildialog', {antallField: antall, innsatsgruppeField: servicegruppe}, {antallTag: antall, innsatsgruppeTag: servicegruppe});
 };
 
 export const antallUlesteDialoger = (antall: number) => {
-    if (!erDemo()) {
-        logEvent('antallulestedialoger', {antallField: antall}, {antallTag: antall});
-    }
+    logEvent('antallulestedialoger', {antallField: antall}, {antallTag: antall});
 };
 
 export const gaTilMeldekort = (servicegruppe: String | null) => {
-    if (!erDemo()) {
-        logEvent('gatilmeldekort', {innsatsgruppeField: servicegruppe}, {innsatsgruppeTag: servicegruppe});
-    }
+    logEvent('gatilmeldekort', {innsatsgruppeField: servicegruppe}, {innsatsgruppeTag: servicegruppe});
 };
 
 export const seEgenvurdering = (foreslaattinnsatsgruppe: ForeslattInnsatsgruppe) => {
-    if (!erDemo()) {
-        logEvent('seegenvurdering', {}, {foreslaattInnsatsgruppe: foreslaattinnsatsgruppe});
-    }
+    logEvent('seegenvurdering', {}, {foreslaattInnsatsgruppe: foreslaattinnsatsgruppe});
 };
 
 export const seMotestotte = (foreslaattinnsatsgruppe: ForeslattInnsatsgruppe) => {
-    if (!erDemo()) {
-        logEvent('semotestotte', {}, {foreslaattInnsatsgruppe: foreslaattinnsatsgruppe});
-    }
+    logEvent('semotestotte', {}, {foreslaattInnsatsgruppe: foreslaattinnsatsgruppe});
 };
 
 export const gaTilEgenvurdering = (antallTimer: number, foreslaattinnsatsgruppe: ForeslattInnsatsgruppe) => {
-    if (!erDemo()) {
-        logEvent('gatilegenvurdering', {antallTimer: antallTimer}, {foreslaattInnsatsgruppe: foreslaattinnsatsgruppe});
-    }
+    logEvent('gatilegenvurdering', {antallTimer: antallTimer}, {foreslaattInnsatsgruppe: foreslaattinnsatsgruppe});
 };
 
 export const gaTilMotestotte = (antallTimer: number, foreslaattinnsatsgruppe: ForeslattInnsatsgruppe) => {
-    if (!erDemo()) {
-        logEvent('gatilmotestotte', {antallTimer: antallTimer}, {foreslaattInnsatsgruppe: foreslaattinnsatsgruppe});
-    }
+    logEvent('gatilmotestotte', {antallTimer: antallTimer}, {foreslaattInnsatsgruppe: foreslaattinnsatsgruppe});
 };
 
 export const gaTilDittSykefravaer = (servicegruppe: String | null) => {
-    if (!erDemo()) {
-        logEvent('gatildittsykefravaer', {innsatsgruppeField: servicegruppe}, {innsatsgruppeTag: servicegruppe});
-    }
+    logEvent('gatildittsykefravaer', {innsatsgruppeField: servicegruppe}, {innsatsgruppeTag: servicegruppe});
 };
 
 export const gaTilMIA = (servicegruppe: String | null) => {
-    if (!erDemo()) {
-        logEvent('gatilmia', {innsatsgruppeField: servicegruppe}, {innsatsgruppeTag: servicegruppe});
-    }
+    logEvent('gatilmia', {innsatsgruppeField: servicegruppe}, {innsatsgruppeTag: servicegruppe});
 };
 
 export const gaTilCV = (servicegruppe: String | null) => {
-    if (!erDemo()) {
-        logEvent('gatilcv', {innsatsgruppeField: servicegruppe}, {innsatsgruppeTag: servicegruppe});
-    }
+    logEvent('gatilcv', {innsatsgruppeField: servicegruppe}, {innsatsgruppeTag: servicegruppe});
 };
 
 export const gaTilJobbsokerkompetanse = (servicegruppe: String | null) => {
-    if (!erDemo()) {
-        logEvent('gatiljobbsokerkompetanseresultat', {innsatsgruppeField: servicegruppe}, {innsatsgruppeTag: servicegruppe});
-    }
+    logEvent('gatiljobbsokerkompetanseresultat', {innsatsgruppeField: servicegruppe}, {innsatsgruppeTag: servicegruppe});
 };
 
 export const gaTilVeiviserarbeidssoker = (servicegruppe: String | null) => {
-    if (!erDemo()) {
-        logEvent('gatilveiviserarbeidssoker', {innsatsgruppeField: servicegruppe}, {innsatsgruppeTag: servicegruppe});
-    }
+    logEvent('gatilveiviserarbeidssoker', {innsatsgruppeField: servicegruppe}, {innsatsgruppeTag: servicegruppe});
 };
 
 export const gaTilTiltaksinfo = (servicegruppe: String | null) => {
-    if (!erDemo()) {
-        logEvent('gatiltiltaksinfo', {innsatsgruppeField: servicegruppe}, {innsatsgruppeTag: servicegruppe});
-    }
+    logEvent('gatiltiltaksinfo', {innsatsgruppeField: servicegruppe}, {innsatsgruppeTag: servicegruppe});
 };
 
 export const seTiltaksinfo = (servicegruppe: String | null) => {
-    if (!erDemo()) {
-        logEvent('setiltaksinfo', {innsatsgruppeField: servicegruppe}, {innsatsgruppeTag: servicegruppe});
-    }
+    logEvent('setiltaksinfo', {innsatsgruppeField: servicegruppe}, {innsatsgruppeTag: servicegruppe});
 };
 
 export const klikkPaSoknadDagpenger = (servicegruppe: string | null) => {
-    if (!erDemo()) {
-        logEvent('soknaddagpenger', {innsatsgruppeField: servicegruppe}, {innsatsgruppeTag: servicegruppe});
-    }
+    logEvent('soknaddagpenger', {innsatsgruppeField: servicegruppe}, {innsatsgruppeTag: servicegruppe});
 };
 
 export const lesOmOkonomi = (stonad: string, servicegruppe: string | null) => {
-    if (!erDemo()) {
-        logEvent('lesomokonomi', {stonadField: stonad, innsatsgruppeField: servicegruppe}, {stonadTag: stonad, innsatsgruppeTag: servicegruppe});
-    }
+    logEvent('lesomokonomi', {stonadField: stonad, innsatsgruppeField: servicegruppe}, {stonadTag: stonad, innsatsgruppeTag: servicegruppe});
 };
