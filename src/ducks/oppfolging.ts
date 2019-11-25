011618255df4d8b3f949c95624cc8e6cf31a9611
Geir Gåsodden <geir.gasodden@pythonia.no>
2019-11-25 15:39:47 +0100

    [KAIZEN-0] - legger til RegistreringType

--- src/ducks/oppfolging.ts
+++ src/ducks/oppfolging.ts
@@ -1,6 +1,14 @@
 import { DataElement, STATUS } from './api';
 import * as React from 'react';
 
+export enum RegistreringType {
+    REAKTIVERING = 'REAKTIVERING',
+    SPERRET = 'SPERRET',
+    ALLEREDE_REGISTRERT = 'ALLEREDE_REGISTRERT',
+    SYKMELDT_REGISTRERING = 'SYKMELDT_REGISTRERING',
+    ORDINAER_REGISTRERING = 'ORDINAER_REGISTRERING'
+}
+
 export enum Servicegruppe {
     BATT = 'BATT',
     BFORM = 'BFORM',
