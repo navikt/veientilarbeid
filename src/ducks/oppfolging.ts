a014874e7543777519b5f4762bd82b68d29c859b
Geir Gåsodden <geir.gasodden@pythonia.no>
2019-11-25 15:38:31 +0100

    [KAIZEN-0] - oppdaterer Servicegruppe type

--- src/ducks/oppfolging.ts
+++ src/ducks/oppfolging.ts
@@ -2,11 +2,15 @@ import { DataElement, STATUS } from './api';
 import * as React from 'react';
 
 export enum Servicegruppe {
-    IKVAL = 'IKVAL',
     BATT = 'BATT',
     BFORM = 'BFORM',
-    VARIG = 'VARIG',
+    BKART = 'BKART',
+    IKVAL = 'IKVAL',
     IVURD = 'IVURD',
+    OPPFI = 'OPPFI',
+    VARIG = 'VARIG',
+    VURDI = 'VURDI',
+    VURDU = 'VURDU'
 }
 
 export enum Formidlingsgruppe {
