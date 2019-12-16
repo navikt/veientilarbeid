91602f70c54dbe64c7436c6b5df29a100a4e34f7
Johan Rusvik <johan.rusvik@gmail.com>
2019-12-16 15:17:00 +0100

    KAIZEN-0 Skriver om til functional component

--- src/komponenter/krr-melding/krr-melding.tsx
+++ src/komponenter/krr-melding/krr-melding.tsx
@@ -11,53 +11,51 @@ import { BrukerregistreringContext } from '../../ducks/brukerregistrering';
 import { BrukerInfoContext } from '../../ducks/bruker-info';
 import { OppfolgingContext } from '../../ducks/oppfolging';
 
-class KrrMelding extends React.Component<{}> {
-    render() {
-        const brukerregistreringData = useContext(BrukerregistreringContext).data;
-        const brukerinfoData = React.useContext(BrukerInfoContext).data;
-        const oppfolgingData = React.useContext(OppfolgingContext).data;
-        const { registrering } = brukerregistreringData;
-        const { besvarelse } = registrering;
-        const { dinSituasjon } = besvarelse;
-        const dinSituasjonOrIngenVerdi = dinSituasjon ? dinSituasjon : 'INGEN_VERDI';
-        const { registreringType, rettighetsgruppe } = brukerinfoData;
-        const { formidlingsgruppe, servicegruppe, underOppfolging, reservasjonKRR } = oppfolgingData;
-        const underOppfolgingJaNei = underOppfolging ? 'ja' : 'nei';
-        const registreringTypeOrIngenVerdi = registreringType ? registreringType : 'INGEN_VERDI';
+const KrrMelding = () => {
+    const brukerregistreringData = useContext(BrukerregistreringContext).data;
+    const brukerinfoData = React.useContext(BrukerInfoContext).data;
+    const oppfolgingData = React.useContext(OppfolgingContext).data;
+    const {registrering} = brukerregistreringData;
+    const {besvarelse} = registrering;
+    const {dinSituasjon} = besvarelse;
+    const dinSituasjonOrIngenVerdi = dinSituasjon ? dinSituasjon : 'INGEN_VERDI';
+    const {registreringType, rettighetsgruppe} = brukerinfoData;
+    const {formidlingsgruppe, servicegruppe, underOppfolging, reservasjonKRR} = oppfolgingData;
+    const underOppfolgingJaNei = underOppfolging ? 'ja' : 'nei';
+    const registreringTypeOrIngenVerdi = registreringType ? registreringType : 'INGEN_VERDI';
 
-        const metrikkData = {
-            servicegruppe,
-            formidlingsgruppe,
-            rettighetsgruppe,
-            dinSituasjon: dinSituasjonOrIngenVerdi,
-            underOppfolging: underOppfolgingJaNei,
-            registreringType: registreringTypeOrIngenVerdi,
-            reservasjonKRR,
-        };
+    const metrikkData = {
+        servicegruppe,
+        formidlingsgruppe,
+        rettighetsgruppe,
+        dinSituasjon: dinSituasjonOrIngenVerdi,
+        underOppfolging: underOppfolgingJaNei,
+        registreringType: registreringTypeOrIngenVerdi,
+        reservasjonKRR,
+    };
 
-        const klikkLenkeMetrikk = () => klikkPaDifiLenke(metrikkData);
+    const klikkLenkeMetrikk = () => klikkPaDifiLenke(metrikkData);
 
-        return (
-            <AlertStripeAdvarsel className="krr-melding blokk-xs">
-                <Normaltekst className="blokk-xs">
-                    {tekster['krr-melding-ingress']}
-                </Normaltekst>
+    return (
+        <AlertStripeAdvarsel className="krr-melding blokk-xs">
+            <Normaltekst className="blokk-xs">
+                {tekster['krr-melding-ingress']}
+            </Normaltekst>
+            <Normaltekst>
+                {tekster['krr-melding-kulepunkt-ingress']}
+            </Normaltekst>
+            <ul>
+                <li><Normaltekst>{tekster['krr-melding-kulepunkt1']}</Normaltekst></li>
+                <li><Normaltekst>{tekster['krr-melding-kulepunkt2']}</Normaltekst></li>
+                <li><Normaltekst>{tekster['krr-melding-kulepunkt3']}</Normaltekst></li>
+            </ul>
+            <Lenke href={difiLenke} onClick={klikkLenkeMetrikk}>
                 <Normaltekst>
-                    {tekster['krr-melding-kulepunkt-ingress']}
+                    {tekster['krr-melding-lenketekst']}
                 </Normaltekst>
-                <ul>
-                    <li><Normaltekst>{tekster['krr-melding-kulepunkt1']}</Normaltekst></li>
-                    <li><Normaltekst>{tekster['krr-melding-kulepunkt2']}</Normaltekst></li>
-                    <li><Normaltekst>{tekster['krr-melding-kulepunkt3']}</Normaltekst></li>
-                </ul>
-                <Lenke href={difiLenke} onClick={klikkLenkeMetrikk}>
-                    <Normaltekst>
-                        {tekster['krr-melding-lenketekst']}
-                    </Normaltekst>
-                </Lenke>
-            </AlertStripeAdvarsel>
-        );
-    }
+            </Lenke>
+        </AlertStripeAdvarsel>
+    );
 }
 
 export default KrrMelding;
