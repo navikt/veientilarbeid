5060987e2021a401ae0dcfd30774e1ba27cd0bca
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-04-14 10:26:56 +0200

    Legger til ny featuretoggle (patch)

--- src/ducks/feature-toggles.ts
+++ src/ducks/feature-toggles.ts
@@ -4,13 +4,15 @@ import { DataElement, STATUS } from './api';
 export const alleFeatureToggles = [
     'veientilarbeid.motestotte.lansert',
     'veientilarbeid.meldekort.ny-tekst',
-    'veientilarbeid.permittert.ny-dialog'
+    'veientilarbeid.permittert.ny-dialog',
+    'veientilarbeid.permittert.situasjon.endre'
 ];
 
 export interface Data {
     'veientilarbeid.motestotte.lansert': boolean;
     'veientilarbeid.meldekort.ny-tekst': boolean;
     'veientilarbeid.permittert.ny-dialog': boolean;
+    'veientilarbeid.permittert.situasjon.endre': boolean;
 }
 
 export interface State extends DataElement {
@@ -21,7 +23,8 @@ export const initialState: State = {
     data: {
         'veientilarbeid.motestotte.lansert': false,
         'veientilarbeid.meldekort.ny-tekst': false,
-        'veientilarbeid.permittert.ny-dialog': false
+        'veientilarbeid.permittert.ny-dialog': false,
+        'veientilarbeid.permittert.situasjon.endre': false
     },
     status: STATUS.NOT_STARTED
 };
