ede14d8c7f3938e1e24b9b07f1cc67389334fe83
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-04-17 11:07:48 +0200

    Endrer boksen til lenkepanel (patch)

--- src/komponenter/din-situasjon/situasjon.tsx
+++ src/komponenter/din-situasjon/situasjon.tsx
@@ -1,13 +1,16 @@
 import React, { useContext } from 'react';
+// import { Normaltekst, Undertittel } from 'nav-frontend-typografi';
 import { Normaltekst, Undertittel } from 'nav-frontend-typografi';
-import Panel from 'nav-frontend-paneler';
-import Lenke from 'nav-frontend-lenker';
+import LenkepanelBase from 'nav-frontend-lenkepanel';
+// import Panel from 'nav-frontend-paneler';
+// import Lenke from 'nav-frontend-lenker';
 import { BrukerregistreringContext } from '../../ducks/brukerregistrering';
 import { SituasjonContext } from '../../ducks/situasjon';
 import getSituasjon from './get-situasjon'
 import prettyPrintDato from '../../utils/pretty-print-dato'
 import { endresituasjonLenke } from '../../innhold/lenker';
 import { uniLogger } from '../../metrics/uni-logger';
+import Ikon from './email-text';
 import './situasjon.less'
 
 const Situasjon = () => {
@@ -20,9 +23,9 @@ const Situasjon = () => {
   const situasjonsbeskrivelse = situasjonData !== null ? situasjonData.svarTekst : getSituasjon(dinSituasjonOrIngenVerdi)
   const situasjonsId = situasjonData !== null ? situasjonData.svarId : dinSituasjonOrIngenVerdi
   const endretDato = situasjonData !== null ? situasjonData.opprettet : opprettetDato
-
-  const handleClick = (event:any) => {
-    event.preventDefault();
+  
+  const handleClick = () => {
+    //event.preventDefault();
     uniLogger('veientilarbeid.endresituasjon.gatil', { situasjonsId });
     window.location.href = endresituasjonLenke;
   };
@@ -32,6 +35,43 @@ const Situasjon = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  
+        return (
+          <LenkepanelBase
+    href={endresituasjonLenke}
+    onClick={handleClick}
+    tittelProps="undertittel"
+    border={true}
+    >
+                <div className="lenkepanel__innhold">
+                    <div className="lenkepanel__ikon">
+                        <Ikon />
+                    </div>
+                    <div>
+                        <Undertittel>
+                          Her kan du oppdatere situasjonen din
+                        </Undertittel>
+                        <Normaltekst>
+                          Sist endret { prettyPrintDato(endretDato) }: { situasjonsbeskrivelse }
+                        </Normaltekst>
+                    </div>
+                </div>
+            </LenkepanelBase>
+        );
+  /*
+  return (
+    <LenkepanelMedIkon
+        href={endresituasjonLenke}
+        alt=""
+        onClick={handleClick}
+        overskrift="Endre din situasjon"
+        ingress={ingress}
+    >
+        <Ikon />
+    </LenkepanelMedIkon>
+);
+*/
+  /*
   return (
     <Panel border className="ramme blokk-s">
       <Undertittel>
@@ -45,6 +85,7 @@ const Situasjon = () => {
       </Normaltekst>
     </Panel>
   );
+  */
 };
 
 export default Situasjon;
