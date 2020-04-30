8205c54ad0531a9c6a9a64805c500da7b5fce5db
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-04-30 09:38:31 +0200

    Legger til poagruppe som kriterie på hotjar-trigger (patch)

--- src/hotjar.ts
+++ src/hotjar.ts
@@ -1,7 +1,9 @@
-export const hotjarTrigger = (stateMicroFrontend: boolean) => {
+import { POAGruppe } from './utils/get-poa-group'
+
+export const hotjarTrigger = (stateMicroFrontend: boolean, PoaGruppe: POAGruppe) => {
   if (typeof window !== 'undefined' && window.hasOwnProperty('hj')) {
       const hotjar = 'hj';
-      const triggerType = stateMicroFrontend ? 'vta-dittnav-generell' : 'vta-generell';
+      const triggerType = stateMicroFrontend ? `vta-dittnav-${PoaGruppe}` : `vta-${PoaGruppe}`;
       window[hotjar]('trigger', triggerType);
   }
 };
--- src/utils/get-poa-group.ts
+++ src/utils/get-poa-group.ts
@@ -1,10 +1,12 @@
+export type POAGruppe = 'kss' | 'boo'
+
 interface Data {
   dinSituasjon: string;
   innsatsgruppe: string;
   formidlingsgruppe: string;
 }
 
-const getPoaGroup = (data: Data): string => {
+const getPoaGroup = (data: Data): POAGruppe => {
   const { dinSituasjon, innsatsgruppe, formidlingsgruppe } = data;
   const kriterier = [];
   kriterier.push(dinSituasjon === 'MISTET_JOBBEN' ? 'kss' : 'boo');
