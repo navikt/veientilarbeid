fe123c7d841d74e15bf62bb6d44f9e28579d5481
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-07-03 11:02:49 +0200

    Tar ut createTriggerType til egen funksjon og lager tester

--- src/hotjar.ts
+++ src/hotjar.ts
@@ -1,10 +1,14 @@
 import { POAGruppe } from './utils/get-poa-group'
 
+export const createTriggerType = (stateMicroFrontend: boolean, PoaGruppe: POAGruppe, isExperiment: boolean) => {
+  const underGruppe = isExperiment ? 'eksperiment' : PoaGruppe;
+  return stateMicroFrontend ? `vta-dittnav-${underGruppe}` : `vta-${underGruppe}`;
+};
+
 export const hotjarTrigger = (stateMicroFrontend: boolean, PoaGruppe: POAGruppe, isExperiment: boolean) => {
   if (typeof window !== 'undefined' && window.hasOwnProperty('hj')) {
       const hotjar = 'hj';
-      const underGruppe = isExperiment ? 'eksperiment' : PoaGruppe;
-      const triggerType = stateMicroFrontend ? `vta-dittnav-${underGruppe}` : `vta-${underGruppe}`;
+      const triggerType = createTriggerType(stateMicroFrontend, PoaGruppe, isExperiment);
       window[hotjar]('trigger', triggerType);
   }
 };
