8bb8778deedf2715e2bb007e83978521790dc4ac
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-07-03 10:15:07 +0200

    Klargjør for å sette opp eksperimenter som skal kunne trigge egne HotJars

--- src/hotjar.ts
+++ src/hotjar.ts
@@ -1,9 +1,10 @@
 import { POAGruppe } from './utils/get-poa-group'
 
-export const hotjarTrigger = (stateMicroFrontend: boolean, PoaGruppe: POAGruppe) => {
+export const hotjarTrigger = (stateMicroFrontend: boolean, PoaGruppe: POAGruppe, isExperiment: boolean) => {
   if (typeof window !== 'undefined' && window.hasOwnProperty('hj')) {
       const hotjar = 'hj';
-      const triggerType = stateMicroFrontend ? `vta-dittnav-${PoaGruppe}` : `vta-${PoaGruppe}`;
+      const underGruppe = isExperiment ? 'eksperiment' : PoaGruppe;
+      const triggerType = stateMicroFrontend ? `vta-dittnav-${underGruppe}` : `vta-${underGruppe}`;
       window[hotjar]('trigger', triggerType);
   }
 };
