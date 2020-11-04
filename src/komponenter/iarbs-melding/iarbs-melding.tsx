dc767df449984f06f5951fcfcb5eb62c559c667c
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-11-04 07:25:44 +0100

    Refaktorerer til funksjonell komponent

--- src/komponenter/iarbs-melding/iarbs-melding.tsx
+++ src/komponenter/iarbs-melding/iarbs-melding.tsx
@@ -4,25 +4,21 @@ import { Normaltekst } from 'nav-frontend-typografi';
 import './iarbs-melding.less';
 import tekster from '../../tekster/tekster';
 
-class iarbsMelding extends React.Component<{}> {
-    render() {
-        return (
-                <AlertStripeAdvarsel className="iarbs-melding blokk-xs">
-                    <Normaltekst className="blokk-xs">
-                        <strong>{tekster['iarbs-melding-tittel']}</strong>
-                    </Normaltekst>
-                    <Normaltekst className="blokk-xs">
-                        {tekster['iarbs-melding-innhold1']}
-                    </Normaltekst>
-                    <Normaltekst className="blokk-xs">
-                        <strong>{tekster['iarbs-melding-innhold2']}</strong>
-                    </Normaltekst>
-                    <Normaltekst className="blokk-xs">{tekster['iarbs-melding-tillegg']}</Normaltekst>
-                </AlertStripeAdvarsel>
-        );
-    }
+const iarbsMelding = () => {
+    return (
+        <AlertStripeAdvarsel className="iarbs-melding blokk-xs">
+            <Normaltekst className="blokk-xs">
+                <strong>{tekster['iarbs-melding-tittel']}</strong>
+            </Normaltekst>
+            <Normaltekst className="blokk-xs">
+                {tekster['iarbs-melding-innhold1']}
+            </Normaltekst>
+            <Normaltekst className="blokk-xs">
+                <strong>{tekster['iarbs-melding-innhold2']}</strong>
+            </Normaltekst>
+            <Normaltekst className="blokk-xs">{tekster['iarbs-melding-tillegg']}</Normaltekst>
+        </AlertStripeAdvarsel>
+    );
 }
 
 export default iarbsMelding;
-
-
