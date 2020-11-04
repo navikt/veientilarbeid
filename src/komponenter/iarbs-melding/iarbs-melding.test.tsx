75fde17e033172794c15a19479b73bac2ce70d45
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-11-04 09:43:05 +0100

    Legger inn test på tekst som ikke skal være tilstede

--- src/komponenter/iarbs-melding/iarbs-melding.test.tsx
+++ src/komponenter/iarbs-melding/iarbs-melding.test.tsx
@@ -4,8 +4,9 @@ import { render, screen } from '@testing-library/react';
 import IARBSMelding from './iarbs-melding';
 
 describe('Test av komponent', () => {
-  test('Rendrer komponent hvis skal vises', () => {
+  test('Rendrer komponent hvis skal vises', async () => {
     render(<IARBSMelding visPlaster={true} />)
+    expect(screen.queryAllByAltText(/denne teksten skal ikke være her/i).length).toBe(0)
     expect(screen.getByText(/ønsker du å søke dagpenger?/i)).toBeTruthy()
     expect(screen.getByText(/hvis du har søkt eller ønsker å søke dagpenger må du ta kontakt med oss./i)).toBeTruthy()
     expect(screen.getByText(/ring 55 55 33 33 med tastevalg 2./i)).toBeTruthy()
