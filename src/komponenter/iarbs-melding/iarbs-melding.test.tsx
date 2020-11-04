25aed894c3f575ab20b6063ff3d000eee824b332
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-11-04 07:23:06 +0100

    Legger til test av komponent

---
+++ src/komponenter/iarbs-melding/iarbs-melding.test.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { render, screen } from '@testing-library/react';
+import IARBSMelding from './iarbs-melding';
+
+describe('Test av komponent', () => {
+  test('Rendrer komponent hvis skal vises', () => {
+    render(<IARBSMelding />)
+    expect(screen.getByText(/ønsker du å søke dagpenger?/i)).toBeTruthy()
+    expect(screen.getByText(/hvis du har søkt eller ønsker å søke dagpenger må du ta kontakt med oss./i)).toBeTruthy()
+    expect(screen.getByText(/ring 55 55 33 33 med tastevalg 2./i)).toBeTruthy()
+    expect(screen.getByText(/hvis det ikke er aktuelt å søke dagpenger nå, eller du har aap kan du se bort fra denne meldingen./i)).toBeTruthy()
+  })
+})
