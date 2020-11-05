49f8a730fce44c4d0c8cf31dfde117263335ed92
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-11-05 11:18:52 +0100

    Slenger inn semikolon

--- src/komponenter/alleskjema/alleskjema.test.tsx
+++ src/komponenter/alleskjema/alleskjema.test.tsx
@@ -1,24 +1,24 @@
 import * as React from 'react';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import AlleSkjema from './alleskjema'
+import AlleSkjema from './alleskjema';
 import tekster from '../../tekster/tekster';
 
 describe('Tester alle skjema komponenten', () => {
   test('Komponenten rendres når den skal vises', () => {
-    render(<AlleSkjema />)
-    expect(screen.getByText(tekster['alleskjema-tittel'])).toBeTruthy()
-    expect(screen.getByText(tekster['alleskjema-tekst'])).toBeTruthy()
-    expect(screen.getByText(tekster['alleskjema-lenke-tekst'])).toBeTruthy()
+    render(<AlleSkjema />);
+    expect(screen.getByText(tekster['alleskjema-tittel'])).toBeTruthy();
+    expect(screen.getByText(tekster['alleskjema-tekst'])).toBeTruthy();
+    expect(screen.getByText(tekster['alleskjema-lenke-tekst'])).toBeTruthy();
   })
 
   test('Klikk på knappen trigger funksjonen', () => {
-    const mockHandleButtonClick = jest.fn()
-    render(<AlleSkjema />)
-    const button = screen.getByText(tekster['alleskjema-lenke-tekst'])
-    button.onclick = mockHandleButtonClick
-    button.onsubmit = mockHandleButtonClick
-    userEvent.click(button)
-    expect(mockHandleButtonClick).toHaveBeenCalledTimes(1)
+    const mockHandleButtonClick = jest.fn();
+    render(<AlleSkjema />);
+    const button = screen.getByText(tekster['alleskjema-lenke-tekst']);
+    button.onclick = mockHandleButtonClick;
+    button.onsubmit = mockHandleButtonClick;
+    userEvent.click(button);
+    expect(mockHandleButtonClick).toHaveBeenCalledTimes(1);
   })
 })
