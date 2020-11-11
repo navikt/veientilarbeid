eb70ec7e3b58e77e5a963c639a4947e15355d529
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-11-11 12:02:47 +0100

    Legger til test av komponenten

---
+++ src/komponenter/meldinger/trekk-dp-soknad.test.tsx
@@ -0,0 +1,54 @@
+import * as React from 'react';
+import '@testing-library/jest-dom/extend-expect';
+import { render, screen } from '@testing-library/react';
+import TrekkDPSoknad from './trekk-dp-soknad';
+import { InnloggingsNiva } from '../../ducks/autentisering';
+import { contextProviders } from '../../test/test-context-providers';
+
+describe('Tester at komponenten rendrer som forventet', () => {
+    test('Rendrer IKKE komponenten dersom ikke oppfølging og ikke nivå 4', () => {
+        const providerProps = {};
+        const { container } = render(<TrekkDPSoknad />, { wrapper: contextProviders(providerProps) });
+        expect(container).toBeEmptyDOMElement();
+    });
+
+    test('Komponentet vises IKKE dersom under oppfølging og nivå 3', () => {
+        const providerProps = {
+            autentisering: {
+                securityLevel: InnloggingsNiva.LEVEL_3,
+            },
+            oppfolging: {
+                underOppfolging: true,
+            },
+        };
+        const { container } = render(<TrekkDPSoknad />, { wrapper: contextProviders(providerProps) });
+        expect(container).toBeEmptyDOMElement();
+    });
+
+    test('Komponentet vises IKKE dersom ikke under oppfølging og nivå 4', () => {
+        const providerProps = {
+            autentisering: {
+                securityLevel: InnloggingsNiva.LEVEL_4,
+            },
+            oppfolging: {
+                underOppfolging: false,
+            },
+        };
+        const { container } = render(<TrekkDPSoknad />, { wrapper: contextProviders(providerProps) });
+        expect(container).toBeEmptyDOMElement();
+    });
+
+    test('Komponentet VISES hvis under oppfølging og nivå 4', async () => {
+        const providerProps = {
+            autentisering: {
+                securityLevel: InnloggingsNiva.LEVEL_4,
+            },
+            oppfolging: {
+                underOppfolging: true,
+            },
+        };
+        render(<TrekkDPSoknad />, { wrapper: contextProviders(providerProps) });
+        expect(screen.getByText(/trekk dagpengesøknaden/i)).toBeInTheDocument();
+        expect(await screen.queryByText(/denne teksten finnes ikke/i)).toBeFalsy();
+    });
+});
