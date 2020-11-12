5e7463bd3ece07179af851959997012d317f1c78
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-11-12 09:47:23 +0100

    Legger til typer på providerProps

--- src/komponenter/ditt-sykefravaer/ditt-sykefravaer.test.tsx
+++ src/komponenter/ditt-sykefravaer/ditt-sykefravaer.test.tsx
@@ -1,19 +1,19 @@
 import * as React from 'react';
 import '@testing-library/jest-dom/extend-expect';
 import { render, screen } from '@testing-library/react';
-import { contextProviders } from '../../test/test-context-providers';
+import { contextProviders, ProviderProps } from '../../test/test-context-providers';
 import tekster from '../../tekster/tekster';
 import DittSykefravaer from './ditt-sykefravaer';
 
 describe('Tester at komponenten rendres som den skal', () => {
     test('Komponenten vises IKKE dersom ikke sykefravær', () => {
-        const providerProps = {};
+        const providerProps: ProviderProps = {};
         const { container } = render(<DittSykefravaer />, { wrapper: contextProviders(providerProps) });
         expect(container).toBeEmptyDOMElement();
     });
 
     test('Komponenten VISES dersom ikke erSykmeldtMedArbeidsgiver', async () => {
-        const providerProps = {
+        const providerProps: ProviderProps = {
             brukerInfo: {
                 erSykmeldtMedArbeidsgiver: true,
             },
