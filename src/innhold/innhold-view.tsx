5f5d9b0394b5efe8c6f24ed43c9da8e0628f7f07
Julian Ravn Thrap-Meyer <jrtm@users.noreply.github.com>
2020-11-18 15:34:16 +0100

    Revert "Forenkling av innholdslogikk (#529)"
    This reverts commit 99e031228a4082696628c639791cd5316ab42a91.

--- src/innhold/innhold-view.tsx
+++ src/innhold/innhold-view.tsx
@@ -1,10 +1,13 @@
 import * as React from 'react';
-import './innhold.less';
 import Rad from './rad';
 import AapRad from '../komponenter/aap/aap';
+import Dialog from '../komponenter/dialog/dialog';
 import Banner from '../komponenter/banner/banner';
+import Meldekort from '../komponenter/meldekort/meldekort';
+import DittSykefravaer from '../komponenter/ditt-sykefravaer/ditt-sykefravaer';
 import OkonomiRad from '../komponenter/okonomi/okonomi-rad';
 import ReaktiveringMelding from '../komponenter/reaktivering-melding';
+import Aktivitetsplan from '../komponenter/aktivitetsplan/aktivitetsplan';
 import RessurslenkerJobbsok from '../komponenter/ressurslenker-jobbsok/ressurslenker-jobbsok';
 import Egenvurdering from '../komponenter/egenvurdering/egenvurdering';
 import Motestotte from '../komponenter/motestotte/motestotte';
@@ -12,28 +15,42 @@ import './innhold.less';
 import KrrMelding from '../komponenter/krr-melding/krr-melding';
 import IARBSMelding from '../komponenter/iarbs-melding/iarbs-melding';
 import Registrert from '../komponenter/registrert/registrert';
-import AktivitetDialogMeldekort from './aktivitet-dialog-meldekort';
-import InnholdMetrics from './innhold-metrics';
+import { BrukerInfoContext } from '../ducks/bruker-info';
 
-const InnholdView = () => {
+interface OwnProps {
+    visRessurslenker: boolean;
+    skalViseIARBSPlaster: boolean;
+}
+
+const AktivitetDialogMeldekort = () => {
+    const { erSykmeldtMedArbeidsgiver } = React.useContext(BrukerInfoContext).data;
     return (
         <>
-            <InnholdMetrics />
+            <Aktivitetsplan />
+            <div className="tokol">
+                <Dialog />
+                {erSykmeldtMedArbeidsgiver ? <DittSykefravaer /> : <Meldekort />}
+            </div>
+        </>
+    );
+};
+
+const InnholdView = ({ visRessurslenker, skalViseIARBSPlaster }: OwnProps) => {
+    return (
+        <>
             <Banner />
             <Rad>
                 <ReaktiveringMelding />
                 <KrrMelding />
                 <Registrert />
-                <IARBSMelding />
+                <IARBSMelding visPlaster={skalViseIARBSPlaster} />
                 <Egenvurdering />
                 <Motestotte />
                 <AktivitetDialogMeldekort />
             </Rad>
 
             <AapRad />
-            <Rad>
-                <RessurslenkerJobbsok />
-            </Rad>
+            <Rad>{visRessurslenker ? <RessurslenkerJobbsok /> : null}</Rad>
             <OkonomiRad />
         </>
     );
--- src/komponenter/hent-initial-data/autentiseringsInfoFetcher.tsx
+++ src/komponenter/hent-initial-data/autentiseringsInfoFetcher.tsx
@@ -4,9 +4,11 @@ import Feilmelding from '../feilmeldinger/feilmelding';
 import { fetchData } from '../../ducks/api-utils';
 import { contextpathDittNav, erMikrofrontend } from '../../utils/app-state-utils';
 import DataProvider from './data-provider';
+import InnholdLogikkNiva4 from '../../innhold/innhold-logikk-niva4';
+import InnholdLogikkNiva3 from '../../innhold/innhold-logikk-niva3';
 import OppfolgingBrukerregistreringProvider from './oppfolging-brukerregistrering-provider';
 import * as Autentisering from '../../ducks/autentisering';
-import InnholdView from '../../innhold/innhold-view';
+import { InnloggingsNiva } from '../../ducks/autentisering';
 
 export const AUTH_API = '/api/auth';
 
@@ -27,11 +29,15 @@ const AutentiseringsInfoFetcher = () => {
             avhengigheter={[state]}
         >
             <Autentisering.AutentiseringContext.Provider value={state}>
-                <OppfolgingBrukerregistreringProvider>
-                    <DataProvider>
-                        <InnholdView />
-                    </DataProvider>
-                </OppfolgingBrukerregistreringProvider>
+                {state.data.securityLevel === InnloggingsNiva.LEVEL_3 ? (
+                    <InnholdLogikkNiva3 />
+                ) : (
+                    <OppfolgingBrukerregistreringProvider>
+                        <DataProvider>
+                            <InnholdLogikkNiva4 />
+                        </DataProvider>
+                    </OppfolgingBrukerregistreringProvider>
+                )}
             </Autentisering.AutentiseringContext.Provider>
         </Innholdslaster>
     );
