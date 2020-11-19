1a0da0eb8edcaf35f22a6cf91365138e8e2ca720
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-11-19 15:21:05 +0100

    Oppdaterer lenkegeneratoren

--- src/komponenter/dittnav/utils/lenker.js
+++ src/komponenter/dittnav/utils/lenker.js
@@ -1,36 +1,33 @@
-const generateUrl = service => {
-  let url = 'https://www.nav.no'
-  switch (service) {
-    case 'NAVNO_URL':
-      url = 'https://www.nav.no'
-      break
-    case 'TJENESTER_URL':
-      url = 'https://www.nav.no'
-      break
-    case 'VEILEDERARBEIDSSOKER_URL':
-      url = 'https://www.nav.no'
-      break
-    default:
-      url = 'https://www.nav.no'
+function getEnvironment () {
+  let environment = 'test'
+  if (typeof window === 'true' && /localhost/.test(window.location)) {
+    environment = 'dev'
+  } else if (typeof window === 'true' && !/q/.test(window.location)) {
+    environment = 'prod'
   }
-  return url
+  return environment
 }
+
+const env = getEnvironment()
+const NAVNO_URL = env === 'prod' ? 'https://www.nav.no' : env === 'test' ? 'https://www-q.nav.no' : 'http://localhost:3002'
+const TJENESTER_URL = env === 'prod' ? 'https://tjenester.nav.no' : env === 'test' ? 'https://tjenester-q.nav.no' : 'http://localhost:3002'
+const VEILEDERARBEIDSSOKER_URL = env === 'prod' ? 'https://arbeidssokerregistrering.nav.no/start' : env === 'test' ? 'https://arbeidssokerregistrering-q.nav.no/start' : 'http://localhost:3002'
 
 export const lenker = {
   skjemaer: {
     tittel: 'Skjemaer',
-    url: `${generateUrl('NAVNO_URL')}/soknader`,
+    url: `${NAVNO_URL}/soknader`,
   },
   dinPensjon: {
     tittel: 'Din pensjon',
-    url: `${generateUrl('TJENESTER_URL')}/pselv/publisering/dinpensjon.jsf`,
+    url: `${TJENESTER_URL}/pselv/publisering/dinpensjon.jsf`,
   },
   veilederArbeidssoker: {
     tittel: 'Veileder for arbeidssøker',
-    url: `${generateUrl('VEILEDERARBEIDSSOKER_URL')}`,
+    url: `${VEILEDERARBEIDSSOKER_URL}`,
   },
   dittSykefravaer: {
     tittel: 'Ditt sykefravær',
-    url: `${generateUrl('TJENESTER_URL')}/sykefravaer`,
+    url: `${TJENESTER_URL}/sykefravaer`,
   },
 };
