3a3a2feb88aef34feaae0f1a6031fdf1172e1135
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-11-23 08:43:10 +0100

    Oppdaterer alle skjema til å sjekk oppfølgingsstatus

--- src/komponenter/alleskjema/alleskjema.test.tsx
+++ src/komponenter/alleskjema/alleskjema.test.tsx
@@ -1,21 +1,37 @@
 import * as React from 'react';
+import '@testing-library/jest-dom/extend-expect';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import { contextProviders, ProviderProps } from '../../test/test-context-providers';
 import AlleSkjema from './alleskjema';
 import tekster from '../../tekster/tekster';
 
 describe('Tester alle skjema komponenten', () => {
-    test('Komponenten rendres når den skal vises', () => {
-        render(<AlleSkjema />);
+    test('Komponenten rendres når bruker er under oppfølging', () => {
+        const props: ProviderProps = {
+            underOppfolging: { erBrukerUnderOppfolging: true },
+        };
+        render(<AlleSkjema />, { wrapper: contextProviders(props) });
         expect(screen.getByText(tekster['alleskjema-tittel'])).toBeTruthy();
         expect(screen.getByText(tekster['alleskjema-tekst'])).toBeTruthy();
         expect(screen.getByText(tekster['alleskjema-lenke-tekst'])).toBeTruthy();
     });
 
+    test('Komponenten rendres IKKE når bruker IKKE er under oppfølging', () => {
+        const props: ProviderProps = {
+            underOppfolging: { erBrukerUnderOppfolging: false },
+        };
+        const { container } = render(<AlleSkjema />, { wrapper: contextProviders(props) });
+        expect(container).toBeEmptyDOMElement();
+    });
+
     test('Klikk på knappen trigger funksjonen', () => {
         const mockHandleButtonClick = jest.fn();
         const mockWindowNavigate = jest.fn();
-        render(<AlleSkjema />);
+        const props: ProviderProps = {
+            underOppfolging: { erBrukerUnderOppfolging: true },
+        };
+        render(<AlleSkjema />, { wrapper: contextProviders(props) });
         const button = screen.getByText(tekster['alleskjema-lenke-tekst']);
         button.onclick = mockHandleButtonClick;
         button.onsubmit = mockHandleButtonClick;
