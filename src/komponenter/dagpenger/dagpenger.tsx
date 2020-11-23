bd6f1aff8aa3ab5c1c88a018683908a37e0e0f06
Michael Gundersen <michael.gundersen@nav.no>
2020-11-23 15:01:20 +0100

    Oppdaterer dagpenger

--- src/komponenter/dagpenger/dagpenger.tsx
+++ src/komponenter/dagpenger/dagpenger.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
-import { Knapp } from 'nav-frontend-knapper';
+import {Knapp} from 'nav-frontend-knapper';
 import Panel from 'nav-frontend-paneler';
-import { Systemtittel, Normaltekst } from 'nav-frontend-typografi';
-import { loggAktivitet } from '../../metrics/metrics';
+import {Normaltekst, Systemtittel} from 'nav-frontend-typografi';
+import {loggAktivitet} from '../../metrics/metrics';
 import './dagpenger.less';
-import { dagpengerSoknadLenke } from '../../innhold/lenker';
+import {dagpengerSoknadLenke} from '../../innhold/lenker';
 import tekster from '../../tekster/tekster';
-import { AmplitudeAktivitetContext } from '../../ducks/amplitude-aktivitet-context';
+import {AmplitudeAktivitetContext} from '../../ducks/amplitude-aktivitet-context';
+import {UnderOppfolgingContext} from "../../ducks/under-oppfolging";
 
 const Dagpenger = () => {
     const amplitudeAktivitetsData = React.useContext(AmplitudeAktivitetContext);
+    const { erBrukerUnderOppfolging } = React.useContext(UnderOppfolgingContext).data;
+    const kanViseKomponent = erBrukerUnderOppfolging;
 
     React.useEffect(() => {
         loggAktivitet({ aktivitet: 'Viser dagpengesøknad', ...amplitudeAktivitetsData });
@@ -20,7 +23,7 @@ const Dagpenger = () => {
         window.location.assign(dagpengerSoknadLenke);
     };
 
-    return (
+    return !kanViseKomponent ? null : (
         <div className="dagpenger">
             <Panel border className="dagpenger-ramme blokk-l">
                 <div className="innhold">
