27bcd7db94cafad801c05f5956f69925942306d7
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-11-23 12:49:15 +0100

    Legger til test og oppdaterer økonomirad

--- src/komponenter/okonomi/okonomi-rad-dagpenger.tsx
+++ src/komponenter/okonomi/okonomi-rad-dagpenger.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Systemtittel } from 'nav-frontend-typografi';
+import { UnderOppfolgingContext } from '../../ducks/under-oppfolging';
 import Rad from '../../innhold/rad';
 import Dagpenger from '../dagpenger/dagpenger';
 import AlleSkjema from '../alleskjema/alleskjema';
@@ -7,20 +8,25 @@ import SjekkKontonummer from '../paminnelser/sjekk-kontonummer';
 import TrekkDagpengeSoknad from '../meldinger/trekk-dp-soknad';
 import tekster from '../../tekster/tekster';
 
-const OkonomiRadDagpenger = () => (
-    <Rad>
-        <Systemtittel tag="h2" className="dagpenger__heading blokk-s">
-            {tekster['dagpenger-heading-tekst']}
-        </Systemtittel>
-        <div className="tokol">
-            <Dagpenger />
-            <AlleSkjema />
-        </div>
-        <div className="tokol">
-            <SjekkKontonummer />
-            <TrekkDagpengeSoknad />
-        </div>
-    </Rad>
-);
+const OkonomiRadDagpenger = () => {
+    const { erBrukerUnderOppfolging } = React.useContext(UnderOppfolgingContext).data;
+    const kanViseKomponent = erBrukerUnderOppfolging;
+
+    return !kanViseKomponent ? null : (
+        <Rad>
+            <Systemtittel tag="h2" className="dagpenger__heading blokk-s">
+                {tekster['dagpenger-heading-tekst']}
+            </Systemtittel>
+            <div className="tokol">
+                <Dagpenger />
+                <AlleSkjema />
+            </div>
+            <div className="tokol">
+                <SjekkKontonummer />
+                <TrekkDagpengeSoknad />
+            </div>
+        </Rad>
+    );
+};
 
 export default OkonomiRadDagpenger;
