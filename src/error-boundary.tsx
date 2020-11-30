a403d6611285e384e59a98817625b37469b57c04
Michael Gundersen <21148282+theagilepadawan@users.noreply.github.com>
2020-11-30 16:57:31 +0100

    Rendre generelle fliser når VTA feiler

--- src/error-boundary.tsx
+++ src/error-boundary.tsx
@@ -1,10 +1,24 @@
 import * as React from 'react';
+import GenerelleFliser from './komponenter/dittnav/generelle-fliser';
 
 interface OwnProps {
     children: React.ReactNode;
 }
 
-export default class ErrorBoundary extends React.Component<OwnProps> {
+interface OwnState {
+    hasError: boolean;
+}
+
+export default class ErrorBoundary extends React.Component<OwnProps, OwnState> {
+    constructor(props: OwnProps) {
+        super(props);
+        this.state = { hasError: false };
+    }
+
+    static getDerivedStateFromError(error: any) {
+        return { hasError: true };
+    }
+
     componentDidCatch(error: any, info: any) {
         const w = window as any;
         const logError = w.frontendlogger
@@ -20,6 +34,9 @@ export default class ErrorBoundary extends React.Component<OwnProps> {
         });
     }
     render() {
+        if (this.state.hasError) {
+            return <GenerelleFliser />;
+        }
         return this.props.children;
     }
 }
