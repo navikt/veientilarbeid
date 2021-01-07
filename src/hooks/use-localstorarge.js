764d53d22255e18cf42ffc4b352ce7e6f0653814
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-01-07 13:27:10 +0100

    Legger til hook for les/skriv til localstorage

---
+++ src/hooks/use-localstorarge.js
@@ -0,0 +1,26 @@
+import { useEffect, useRef, useState } from 'react'
+
+function useLocalStorage (key, defaultValue, { serialize = JSON.stringify, deserialize = JSON.parse } = {}) {
+  const [state, setState] = useState(() => {
+    const itemInStorage = window.localStorage.getItem(key)
+    if (itemInStorage) {
+      return deserialize(itemInStorage)
+    }
+    return typeof defaultValue === 'function' ? defaultValue() : (defaultValue || '')
+  })
+
+  const prevKeyRef = useRef(key)
+
+  useEffect(() => {
+    const prevKey = prevKeyRef.current
+    if (prevKey !== key) {
+      window.localStorage.removeItem(prevKey)
+    }
+    prevKeyRef.current = key
+    window.localStorage.setItem(key, serialize(state))
+  }, [key, state, serialize])
+
+  return [state, setState]
+}
+
+export { useSessionStorage }
