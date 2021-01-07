bb9f09723c5e856c26b6d964d8a4603d8bb2ba3f
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-01-07 14:50:02 +0100

    Legger til reaktivering ikke aktuelt melding

---
+++ src/komponenter/reaktivering/reaktivering-ikke-aktuelt-melding.tsx
@@ -0,0 +1,67 @@
+import * as React from 'react';
+import { AlertStripeInfo } from 'nav-frontend-alertstriper';
+import { Knapp } from 'nav-frontend-knapper';
+import Lenke from 'nav-frontend-lenker';
+import { Normaltekst } from 'nav-frontend-typografi';
+import { reaktiveringLenke, dialogLenke } from '../../innhold/lenker';
+import { loggAktivitet } from '../../metrics/metrics';
+import { AmplitudeAktivitetContext } from '../../ducks/amplitude-aktivitet-context';
+import { AutentiseringContext, InnloggingsNiva } from '../../ducks/autentisering';
+import { OppfolgingContext } from '../../ducks/oppfolging';
+import './reaktivering-melding.less'
+
+const ReaktiveringIkkeAktueltMelding = () => {
+    const amplitudeAktivitetsData = React.useContext(AmplitudeAktivitetContext);
+    const { kanReaktiveres } = React.useContext(OppfolgingContext).data;
+    const { securityLevel } = React.useContext(AutentiseringContext).data;
+    const isLevel4 = securityLevel === InnloggingsNiva.LEVEL_4;
+    const kanViseKomponent = isLevel4 && kanReaktiveres;
+
+    React.useEffect(() => {
+        if (kanViseKomponent) {
+            loggAktivitet({ aktivitet: 'Viser reaktivering ikke aktuelt', ...amplitudeAktivitetsData });
+        }
+    }, [kanViseKomponent, amplitudeAktivitetsData]);
+
+    const handleReaktivering = (event: React.SyntheticEvent) => {
+        event.preventDefault();
+        loggAktivitet({ aktivitet: 'Går til registrering fra reaktivering ikke aktuelt', ...amplitudeAktivitetsData });
+        window.location.assign(reaktiveringLenke);
+    };
+
+    const handleDialog = (event: React.SyntheticEvent) => {
+        event.preventDefault();
+        loggAktivitet({ aktivitet: 'Går til dialog fra reaktivering ikke aktuelt', ...amplitudeAktivitetsData });
+        window.location.assign(dialogLenke);
+    };
+
+    if (!kanViseKomponent) {
+        return null;
+    }
+    return (
+        <section className="reaktivering-melding blokk-m">
+            <AlertStripeInfo>
+                <Normaltekst className="blokk-xs">
+                    Du er ikke registrert som arbeidssøker hos NAV.
+                </Normaltekst>
+                <Normaltekst className="blokk-s">
+                    <Knapp onClick={handleReaktivering}>
+                        Registrer deg som arbeidssøker
+                    </Knapp>
+                </Normaltekst>
+                <Normaltekst>
+                    Er du usikker på om din situasjon betyr at du bør være registrert som arbeidssøker?
+                </Normaltekst>
+                <Normaltekst className="blokk-xs">
+                    <Lenke
+                        href={dialogLenke}
+                        onClick={handleDialog}>
+                        Ta kontakt med veilederen din i dialogtjenesten
+                    </Lenke>
+                </Normaltekst>
+            </AlertStripeInfo>
+        </section>
+    );
+};
+
+export default ReaktiveringIkkeAktueltMelding;
