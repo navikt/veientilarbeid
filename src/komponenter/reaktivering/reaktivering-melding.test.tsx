78dcd338e5615ee965b5750f1fced61705caccc3
Julian Ravn Thrap-Meyer <Julian.Ravn.Thrap-Meyer@nav.no>
2021-01-22 10:41:40 +0100

    Teller kun antall visninger av reaktivering-melding som har blitt vist til bruker

--- src/komponenter/reaktivering/reaktivering-melding.test.tsx
+++ src/komponenter/reaktivering/reaktivering-melding.test.tsx
@@ -4,19 +4,26 @@ import { render, screen } from '@testing-library/react';
 import { InnloggingsNiva } from '../../ducks/autentisering';
 import { contextProviders, ProviderProps } from '../../test/test-context-providers';
 import Reaktivering from './reaktivering-melding';
+import { mockIntersectionObserver } from '../../mocks/intersection-observer-mock';
 
 describe('Tester at komponenten rendres slik den skal', () => {
+    beforeEach(() => {
+        mockIntersectionObserver();
+    });
+
     test('Komponenten rendres IKKE som default', () => {
-        const mockSetReaktivering = jest.fn()
-        const mockSetApen = jest.fn()
+        const mockSetReaktivering = jest.fn();
+        const mockSetApen = jest.fn();
         const providerProps: ProviderProps = {};
-        const { container } = render(<Reaktivering setReaktivering={mockSetReaktivering} setApen={mockSetApen} />, { wrapper: contextProviders(providerProps) });
+        const { container } = render(<Reaktivering setReaktivering={mockSetReaktivering} setApen={mockSetApen} />, {
+            wrapper: contextProviders(providerProps),
+        });
         expect(container).toBeEmptyDOMElement();
     });
 
     test('Komponenten rendres dersom brukeren KAN reaktiveres og er nivå 4', async () => {
-        const mockSetReaktivering = jest.fn()
-        const mockSetApen = jest.fn()
+        const mockSetReaktivering = jest.fn();
+        const mockSetApen = jest.fn();
         const providerProps: ProviderProps = {
             autentisering: {
                 securityLevel: InnloggingsNiva.LEVEL_4,
@@ -25,14 +32,32 @@ describe('Tester at komponenten rendres slik den skal', () => {
                 kanReaktiveres: true,
             },
         };
-        render(<Reaktivering setReaktivering={mockSetReaktivering} setApen={mockSetApen} />, { wrapper: contextProviders(providerProps) });
-        expect(screen.getByText(/har du mottatt dagpenger vil utbetalingene nå være stoppet\. du må registrere deg på nytt og sende inn ny søknad om dagpenger\./i)).toBeInTheDocument();
-        expect(screen.getByText(/dersom du ønsker arbeidsrettet oppfølging fra NAV, må du være registrert som arbeidssøker\./i)).toBeInTheDocument();
-        expect(screen.getByText(/dersom du har søkt eller ønsker å søke om dagpenger må du være registrert som arbeidssøker\./i)).toBeInTheDocument();
+        render(<Reaktivering setReaktivering={mockSetReaktivering} setApen={mockSetApen} />, {
+            wrapper: contextProviders(providerProps),
+        });
+        expect(
+            screen.getByText(
+                /har du mottatt dagpenger vil utbetalingene nå være stoppet\. du må registrere deg på nytt og sende inn ny søknad om dagpenger\./i
+            )
+        ).toBeInTheDocument();
+        expect(
+            screen.getByText(
+                /dersom du ønsker arbeidsrettet oppfølging fra NAV, må du være registrert som arbeidssøker\./i
+            )
+        ).toBeInTheDocument();
+        expect(
+            screen.getByText(
+                /dersom du har søkt eller ønsker å søke om dagpenger må du være registrert som arbeidssøker\./i
+            )
+        ).toBeInTheDocument();
         expect(screen.getByText(/registrer deg som arbeidssøker/i)).toBeInTheDocument();
-        expect(screen.getByText(/Er du usikker på om din situasjon betyr at du bør være registrert som arbeidssøker/i)).toBeInTheDocument();
+        expect(
+            screen.getByText(/Er du usikker på om din situasjon betyr at du bør være registrert som arbeidssøker/i)
+        ).toBeInTheDocument();
         expect(screen.getByText(/ta kontakt med veilederen din i dialogtjenesten/i)).toBeInTheDocument();
-        expect(screen.getByText(/jeg har ikke lenger behov for å være registrert som arbeidssøker hos nav/i)).toBeInTheDocument();
+        expect(
+            screen.getByText(/jeg har ikke lenger behov for å være registrert som arbeidssøker hos nav/i)
+        ).toBeInTheDocument();
         expect(await screen.queryByText(/denne teksten finnes ikke/i)).not.toBeInTheDocument();
     });
 });
