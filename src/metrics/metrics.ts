f222ac0bdf6cc13a8520140274178a5f4e4f9982
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-02-10 14:45:41 +0100

    Fjerner flere ubrukte loggere

--- src/metrics/metrics.ts
+++ src/metrics/metrics.ts
@@ -1,38 +1,9 @@
-import { FormidlingsgruppeOrNull, ServicegruppeOrNull } from '../ducks/oppfolging';
-import { RegistreringTypeOrIngenVerdi } from '../ducks/bruker-info';
-import { AmplitudeData, amplitudeLogger, setIdentifyProperty } from './amplitude-utils';
-import { uniLogger } from './uni-logger';
-import { POAGruppe } from '../utils/get-poa-group';
+import { AmplitudeData, amplitudeLogger } from './amplitude-utils';
 
 const domene = 'veientilarbeid';
 
 export const lesOmOkonomi = (stonad: string, servicegruppe: string | null) => {
     amplitudeLogger(`${domene}.okonomi.click`, { stonad, servicegruppe });
-};
-
-type StandardMetrikkData = {
-    servicegruppe: ServicegruppeOrNull;
-    formidlingsgruppe: FormidlingsgruppeOrNull;
-    rettighetsgruppe: string;
-    dinSituasjon: string;
-    underOppfolging: string;
-    registreringType: RegistreringTypeOrIngenVerdi;
-};
-
-interface KrrMetrikkData extends StandardMetrikkData {
-    reservasjonKRR: string;
-}
-
-export const klikkPaDifiLenke = (metrikker: KrrMetrikkData) => {
-    uniLogger('krr.difi.click', metrikker);
-};
-
-export const tellPoaGruppe = (amplitudeData: AmplitudeData) => {
-    uniLogger('poagruppe', amplitudeData);
-};
-
-export const setIdentifyPoaGruppe = (gruppe: POAGruppe) => {
-    setIdentifyProperty('poagruppe', gruppe);
 };
 
 export type AktivitetsMetrikkData = {
