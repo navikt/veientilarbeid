c09ea33928f15602dc464d96d746f649206fe216
Michael Gundersen <21148282+theagilepadawan@users.noreply.github.com>
2021-02-17 10:59:00 +0100

    Indre Julian™️ og endstate-innhold

--- src/komponenter/onboarding/meldekort.js
+++ src/komponenter/onboarding/meldekort.js
@@ -1,10 +1,11 @@
-import React, { useEffect, useRef, useState } from 'react'
-import {Normaltekst, Systemtittel} from 'nav-frontend-typografi';
+import React, {useEffect, useRef, useState} from 'react'
+import {Normaltekst, Systemtittel, Undertekst} from 'nav-frontend-typografi';
 import Panel from 'nav-frontend-paneler';
 import {Nesteknapp, Tilbakeknapp} from 'nav-frontend-ikonknapper';
-import { AmplitudeContext } from '../../ducks/amplitude-context';
-import { amplitudeLogger } from '../../metrics/amplitude-utils'
+import {AmplitudeContext} from '../../ducks/amplitude-context';
+import {amplitudeLogger} from '../../metrics/amplitude-utils'
 import './meldekort.less'
+import Lenke from "nav-frontend-lenker";
 
 function Kort1() {
     return (
@@ -59,15 +60,41 @@ function Kort3() {
     )
 }
 
-function EndState() {
+function EndState(fra, til) {
     return (
         <div>
-            <Systemtittel className={"blokk-xs"}>Hvordan fungerer meldekort i NAV?</Systemtittel>
+            <Systemtittel className={"blokk-xs"}>Innsending av meldekort</Systemtittel>
+            <Normaltekst className={"blokk-xs"}>
+                Du kan nå sende inn meldekortet.
+            </Normaltekst>
+            <Normaltekst className={"blokk-xs"}>
+                Sender du inn for sent vil du ikke lenger stå registrert som arbeidssøker og dagpengene vil stoppes.
+            </Normaltekst>
+            <Normaltekst>
+                Gjeldende meldekort
+            </Normaltekst>
+            <div className={"meldekortinfo"}>
+            <Undertekst>
+                Periode: {"01-01-1970"} - {"01-01-1970"}
+            </Undertekst>
+                <Lenke
+                    href="https://www.nav.no/no/person/arbeid/dagpenger-ved-arbeidsloshet-og-permittering/meldekort-hvordan-gjor-du-det"
+                    target="_blank"
+                >
+                    Gå til innsending
+                </Lenke>
+            </div>
+            <Lenke
+                href="https://www.nav.no/no/person/arbeid/dagpenger-ved-arbeidsloshet-og-permittering/meldekort-hvordan-gjor-du-det"
+                target="_blank"
+            >
+                Les mer om meldekort
+            </Lenke>
         </div>
     )
 }
 
-const cards = [
+const onboardingKort = [
     <Kort1/>,
     <Kort2/>,
     <Kort3/>,
@@ -76,43 +103,53 @@ const cards = [
 
 function Meldekort() {
     const amplitudeData = React.useContext(AmplitudeContext);
-    const sisteKortiListen = cards.length - 1
+    const sisteKortiListen = onboardingKort.length - 1
     const startKort = parseInt(amplitudeData.ukerRegistrert, 10) > 0 ? sisteKortiListen : 0
-    const [cardNumber, setCardNumber] = useState(startKort)
+    const [gjeldendeKort, setGjeldendeKort] = useState(startKort)
     const forrigeKortRef = useRef(startKort)
     const nesteKort = () => {
-        if (cardNumber < cards.length - 1) {
-            setCardNumber(cardNumber + 1)
+        if (gjeldendeKort < onboardingKort.length - 1) {
+            setGjeldendeKort(gjeldendeKort + 1)
         }
     }
     const forrigeKort = () => {
-        if (cardNumber > 0) {
-            setCardNumber(cardNumber - 1)
+        if (gjeldendeKort > 0) {
+            setGjeldendeKort(gjeldendeKort - 1)
         }
     }
 
     useEffect(() => {
-        if (forrigeKortRef.current !== cardNumber) {
-            const handling = `Går fra ${forrigeKortRef.current + 1} til kort ${cardNumber + 1}`
+        if (forrigeKortRef.current !== gjeldendeKort) {
+            const handling = `Går fra ${forrigeKortRef.current + 1} til kort ${gjeldendeKort + 1}`
             amplitudeLogger('veientilarbeid.onboarding', {
                 onboarding: 'meldekort',
                 handling,
                 ...amplitudeData
             })
-            forrigeKortRef.current = cardNumber
+            forrigeKortRef.current = gjeldendeKort
         }
-    }, [cardNumber, amplitudeData])
+    }, [gjeldendeKort, amplitudeData])
 
     return (
         <Panel className="blokk-s" border>
             <div className={"kortwrapper"}>
-                {cards[cardNumber]}
-                <Normaltekst>{cardNumber + 1} av {cards.length}</Normaltekst>
+                <div className={"kortinnhold"}>
+                {onboardingKort[gjeldendeKort]}
+                </div>
+                {gjeldendeKort !== sisteKortiListen
+                    ?
+                    <Normaltekst>{gjeldendeKort + 1} av {onboardingKort.length}</Normaltekst>
+                    : null
+                }
             </div>
-            <div className={"knapper"}>
-                <Tilbakeknapp onClick={forrigeKort}/>
-                <Nesteknapp onClick={nesteKort}/>
-            </div>
+            { gjeldendeKort !== sisteKortiListen
+                ?
+                <div className={"knapper"}>
+                    <Tilbakeknapp onClick={forrigeKort}/>
+                    <Nesteknapp onClick={nesteKort}/>
+                </div>
+                : null
+            }
         </Panel>
     )
 }
