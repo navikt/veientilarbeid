cbfa52d244cf19c61bfd9538c38402e3e6a87c6f
Julian Ravn Thrap-Meyer <Julian.Ravn.Thrap-Meyer@nav.no>
2021-02-17 13:58:43 +0100

    Viser alltid tilbake/neste for nyregistrerte brukere

--- src/komponenter/onboarding/meldekort.test.js
+++ src/komponenter/onboarding/meldekort.test.js
@@ -1,58 +1,59 @@
 import * as React from 'react';
 import '@testing-library/jest-dom/extend-expect';
 import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event'
+import userEvent from '@testing-library/user-event';
 import { contextProviders, ProviderProps } from '../../test/test-context-providers';
-import Meldekort from './meldekort'
+import Meldekort from './meldekort';
 
 describe('tester onboarding komponenten for meldekort', () => {
-  test('komponenten rendres', () => {
-    const props = {}
-    const { container } = render(<Meldekort />, { wrapper: contextProviders(props) });
-    expect(container).not.toBeEmptyDOMElement();
-    expect(screen.getByText(/Hvordan fungerer meldekort i NAV/i)).toBeInTheDocument();
-  })
+    test('komponenten rendres', () => {
+        const props = {};
+        const { container } = render(<Meldekort />, { wrapper: contextProviders(props) });
+        expect(container).not.toBeEmptyDOMElement();
+        expect(screen.getByText(/Hvordan fungerer meldekort i NAV/i)).toBeInTheDocument();
+    });
 
-  test('funksjonen for neste og forrige kort fungerer', () => {
-    const props = {}
-    const { container } = render(<Meldekort />, { wrapper: contextProviders(props) });
-    const nesteknapp = screen.getByText(/neste/i)
-    const tilbakeknapp = screen.getByText(/tilbake/i)
-    expect(screen.getByText(/1 av 4/i)).toBeInTheDocument();
-    
-    // Navigerer seg gjennom kortene - fremover
-    userEvent.click(nesteknapp)
-    expect(screen.getByText(/2 av 4/i)).toBeInTheDocument();
-    userEvent.click(nesteknapp)
-    expect(screen.getByText(/3 av 4/i)).toBeInTheDocument();
-    userEvent.click(nesteknapp)
-    expect(screen.getByText(/4 av 4/i)).toBeInTheDocument();
+    test('funksjonen for neste og forrige kort fungerer for nyregistrerte', () => {
+        const props = { amplitude: { ukerRegistrert: 0 } };
+        const { container } = render(<Meldekort />, { wrapper: contextProviders(props) });
+        const nesteknapp = screen.getByText(/neste/i).parentElement;
+        const tilbakeknapp = screen.getByText(/tilbake/i).parentElement;
 
-    // Sjekker at man ikke kan gå lenger frem enn siste kort
-    userEvent.click(nesteknapp)
-    expect(screen.getByText(/4 av 4/i)).toBeInTheDocument();
+        expect(screen.getByText(/1 av 4/i)).toBeInTheDocument();
+        expect(nesteknapp).toBeEnabled();
+        expect(tilbakeknapp).toBeDisabled();
 
-    // Navigerer seg gjennom kortene - bakover
-    userEvent.click(tilbakeknapp)
-    expect(screen.getByText(/3 av 4/i)).toBeInTheDocument();
-    userEvent.click(tilbakeknapp)
-    expect(screen.getByText(/2 av 4/i)).toBeInTheDocument();
-    userEvent.click(tilbakeknapp)
-    expect(screen.getByText(/1 av 4/i)).toBeInTheDocument();
+        // Navigerer seg gjennom kortene - fremover
+        userEvent.click(nesteknapp);
+        expect(screen.getByText(/2 av 4/i)).toBeInTheDocument();
+        userEvent.click(nesteknapp);
+        expect(screen.getByText(/3 av 4/i)).toBeInTheDocument();
+        expect(nesteknapp).toBeEnabled();
 
-    // Sjekker at man ikke kan gå lenger tilbake enn første kort
-    userEvent.click(tilbakeknapp)
-    expect(screen.getByText(/1 av 4/i)).toBeInTheDocument();
-  })
+        // Kan gå tilbake til side 1
+        userEvent.click(tilbakeknapp);
+        expect(screen.getByText(/2 av 4/i)).toBeInTheDocument();
+        userEvent.click(tilbakeknapp);
+        expect(screen.getByText(/1 av 4/i)).toBeInTheDocument();
+        expect(tilbakeknapp).toBeDisabled();
 
-  test('man starter på endstate om man har vært registrert over 0 uker', () => {
-    const props = {
-      amplitude: {
-        ukerRegistrert: 1
-      }
-    }
-    const { container } = render(<Meldekort />, { wrapper: contextProviders(props) });
-    expect(container).not.toBeEmptyDOMElement();
-    expect(screen.getByText(/4 av 4/i)).toBeInTheDocument();
-  })
-})
+        // Gå helt til siste side
+        userEvent.click(nesteknapp);
+        userEvent.click(nesteknapp);
+        userEvent.click(nesteknapp);
+        expect(screen.getByText(/Innsending av meldekort/i)).toBeInTheDocument();
+        expect(nesteknapp).toBeDisabled();
+        expect(tilbakeknapp).toBeEnabled();
+    });
+
+    test('man starter på endstate om man har vært registrert over 0 uker', () => {
+        const props = {
+            amplitude: {
+                ukerRegistrert: 1,
+            },
+        };
+        const { container } = render(<Meldekort />, { wrapper: contextProviders(props) });
+        expect(container).not.toBeEmptyDOMElement();
+        expect(screen.queryByText(/Neste/)).not.toBeInTheDocument();
+    });
+});
