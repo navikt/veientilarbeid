effb60abfd1231aef70bf292eb58c2d9f7c2587e
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-02-18 15:07:54 +0100

    Legger til test for er-standard-innsatsgruppe

---
+++ src/lib/er-standard-innsatsgruppe.test.ts
@@ -0,0 +1,116 @@
+import { ForeslattInnsatsgruppe, FremtidigSituasjonSvar } from '../ducks/brukerregistrering'
+import { Servicegruppe, Formidlingsgruppe } from '../ducks/oppfolging'
+import erStandardInnsatsgruppe from './er-standard-innsatsgruppe'
+
+const grunnData = {
+  registrering: {
+    opprettetDato: new Date().toISOString(),
+    manueltRegistrertAv: null,
+    besvarelse: {
+      dinSituasjon: null,
+      fremtidigSituasjon: FremtidigSituasjonSvar.USIKKER, 
+      sisteStilling: null,
+      tilbakeIArbeid: null,
+      andreForhold: null,
+      helseHinder: null,
+      utdanning: null,
+      utdanningBestatt: null,
+      utdanningGodkjent: null,
+      teksterForBesvarelse: null
+    },
+    teksterForBesvarelse: null
+  },
+  oppfolging: {
+    kanReaktiveres: false,
+    reservasjonKRR: false,
+    servicegruppe: Servicegruppe.IKVAL,
+    formidlingsgruppe: Formidlingsgruppe.ARBS,
+
+  }
+}
+
+describe('tester funksjonaliteten for erStandardInnsatsgruppe', () => {
+  test('returnerer true for ARBS + IKVAL', () => {
+    const testData = {...grunnData}  
+    expect(erStandardInnsatsgruppe(testData)).toBe(true)
+  })
+
+  test('returnerer true for ARBS + IVURD og profilering lik STANDARD_INNSATS', () => {
+    const lokaleData = {
+      registrering: {
+        opprettetDato: new Date().toISOString(),
+        manueltRegistrertAv: null,
+        besvarelse: {
+          dinSituasjon: null,
+          fremtidigSituasjon: FremtidigSituasjonSvar.USIKKER, 
+          sisteStilling: null,
+          tilbakeIArbeid: null,
+          andreForhold: null,
+          helseHinder: null,
+          utdanning: null,
+          utdanningBestatt: null,
+          utdanningGodkjent: null,
+          teksterForBesvarelse: null
+        },
+        teksterForBesvarelse: null,
+        profilering: {
+          innsatsgruppe: ForeslattInnsatsgruppe.STANDARD_INNSATS
+        }
+      },
+      oppfolging: {
+        kanReaktiveres: false,
+        reservasjonKRR: false,
+        servicegruppe: Servicegruppe.IVURD,
+        formidlingsgruppe: Formidlingsgruppe.ARBS,
+      }
+    }
+    const testData = {...grunnData, ...lokaleData}
+    expect(erStandardInnsatsgruppe(testData)).toBe(true)
+  })
+
+  test('returnerer false for ARBS + IVURD og profilering lik BEHOV_FOR_ARBEIDSEVNEVURDERING', () => {
+    const lokaleData = {
+      registrering: {
+        opprettetDato: new Date().toISOString(),
+        manueltRegistrertAv: null,
+        besvarelse: {
+          dinSituasjon: null,
+          fremtidigSituasjon: FremtidigSituasjonSvar.USIKKER, 
+          sisteStilling: null,
+          tilbakeIArbeid: null,
+          andreForhold: null,
+          helseHinder: null,
+          utdanning: null,
+          utdanningBestatt: null,
+          utdanningGodkjent: null,
+          teksterForBesvarelse: null
+        },
+        teksterForBesvarelse: null,
+        profilering: {
+          innsatsgruppe: ForeslattInnsatsgruppe.BEHOV_FOR_ARBEIDSEVNEVURDERING
+        }
+      },
+      oppfolging: {
+        kanReaktiveres: false,
+        reservasjonKRR: false,
+        servicegruppe: Servicegruppe.IVURD,
+        formidlingsgruppe: Formidlingsgruppe.ARBS,
+      }
+    }
+    const testData = {...grunnData, ...lokaleData}
+    expect(erStandardInnsatsgruppe(testData)).toBe(false)
+  })
+
+  test('returnerer false for ARBS + IVURD og ingen profilering', () => {
+    const lokaleData = {
+      oppfolging: {
+        kanReaktiveres: false,
+        reservasjonKRR: false,
+        servicegruppe: Servicegruppe.IVURD,
+        formidlingsgruppe: Formidlingsgruppe.ARBS,
+      }
+    }
+    const testData = {...grunnData, ...lokaleData}
+    expect(erStandardInnsatsgruppe(testData)).toBe(false)
+  })
+})
