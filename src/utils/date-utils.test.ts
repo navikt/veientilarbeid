e34b16fd5958e58c2dd2891a9dca5346cb004b3d
Michael Gundersen <21148282+theagilepadawan@users.noreply.github.com>
2021-02-22 10:12:08 +0100

    Util-funksjon for å finne første mandag + testing

--- src/utils/date-utils.test.ts
+++ src/utils/date-utils.test.ts
@@ -1,6 +1,6 @@
-import { hentISOUke } from './date-utils';
+import { foerstkommendeMandag, hentISOUke } from './date-utils';
 
-describe('Utretning av ISO-uke', () => {
+describe('Utregning av ISO-uke', () => {
     it('returnerer uke 53 som siste uke av desember 2020', () => {
         expect(hentISOUke('2020-12-31')).toBe(53);
         expect(hentISOUke('2021-01-1')).toBe(53);
@@ -16,5 +16,27 @@ describe('Utretning av ISO-uke', () => {
         expect(hentISOUke('2022-01-01')).toBe(52);
         expect(hentISOUke('2022-01-02')).toBe(52);
         expect(hentISOUke('2022-01-03')).toBe(1);
+    });
+});
+
+describe('Utregning av førstkommende mandag', () => {
+    it('overgang fra vintertid til sommertid', () => {
+        expect(foerstkommendeMandag(new Date('2021-03-28'))).toEqual(new Date('2021-03-29'));
+        expect(foerstkommendeMandag(new Date('2021-03-29'))).toEqual(new Date('2021-04-05'));
+    });
+
+    it('overgang fra sommertid til vintertid', () => {
+        expect(foerstkommendeMandag(new Date('2021-10-31'))).toStrictEqual(new Date('2021-11-01'));
+        expect(foerstkommendeMandag(new Date('2021-11-01'))).toStrictEqual(new Date('2021-11-08'));
+    });
+
+    it('uke 7 i 2021', () => {
+        expect(foerstkommendeMandag(new Date('2021-02-15'))).toStrictEqual(new Date('2021-02-22'));
+        expect(foerstkommendeMandag(new Date('2021-02-16'))).toStrictEqual(new Date('2021-02-22'));
+        expect(foerstkommendeMandag(new Date('2021-02-17'))).toStrictEqual(new Date('2021-02-22'));
+        expect(foerstkommendeMandag(new Date('2021-02-18'))).toStrictEqual(new Date('2021-02-22'));
+        expect(foerstkommendeMandag(new Date('2021-02-19'))).toStrictEqual(new Date('2021-02-22'));
+        expect(foerstkommendeMandag(new Date('2021-02-20'))).toStrictEqual(new Date('2021-02-22'));
+        expect(foerstkommendeMandag(new Date('2021-02-21'))).toStrictEqual(new Date('2021-02-22'));
     });
 });
--- src/utils/date-utils.ts
+++ src/utils/date-utils.ts
@@ -1,3 +1,5 @@
+const msPerDoegn = 1000 * 60 * 60 * 24;
+
 export function datoUtenTid(dato: string) {
     return new Date(dato.substr(0, 10));
 }
@@ -8,11 +10,19 @@ export function plussDager(dato: Date, antallDager: number) {
     return kopi;
 }
 
+export function foerstkommendeMandag(iDag: Date) {
+    let foerstkommendeMandag = datoUtenTid(iDag.toISOString());
+
+    do {
+        foerstkommendeMandag.setUTCMilliseconds(foerstkommendeMandag.getUTCMilliseconds() + msPerDoegn);
+    } while (foerstkommendeMandag.getDay() !== 1);
+    return foerstkommendeMandag;
+}
+
 export function hentISOUke(datoMedTid: string) {
     const dato = datoUtenTid(datoMedTid);
     const dagIUken = dato.getUTCDay() || 7;
     dato.setUTCDate(dato.getUTCDate() + 4 - dagIUken);
     const foersteDatoIAaret = new Date(Date.UTC(dato.getUTCFullYear(), 0, 1));
-    const msPerDoegn = 1000 * 60 * 60 * 24;
     return Math.ceil(((dato.getTime() - foersteDatoIAaret.getTime()) / msPerDoegn + 1) / 7);
 }
