aae8964a086d7a4fd13f7a5b7590e7e398cb11a8
Julian Ravn Thrap-Meyer <Julian.Ravn.Thrap-Meyer@nav.no>
2021-02-23 08:34:25 +0100

    Konverter generelle-fliser-komponenter til tsx for å rette opp type-advarsler

---
+++ src/komponenter/dittnav/components/LenkepanelMedIkon.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { LenkepanelBase } from 'nav-frontend-lenkepanel';
+import { Normaltekst, Undertekst } from 'nav-frontend-typografi';
+import { trackEvent } from '../utils/googleAnalytics';
+
+type PropTypes = {
+    overskrift: JSX.Element | string;
+    ingress: JSX.Element | string | null;
+    etikett?: string;
+    gaCategory?: string;
+    gaAction?: string;
+    gaUrl?: string;
+};
+
+export const LenkepanelMedIkon: React.FC<PropTypes & React.HTMLProps<HTMLAnchorElement>> = ({
+    href,
+    onClick,
+    className,
+    overskrift,
+    ingress,
+    etikett,
+    gaCategory,
+    gaAction,
+    gaUrl,
+    children,
+}) => {
+    const linkCreator = (
+        props: React.AnchorHTMLAttributes<HTMLAnchorElement> // eslint-disable-next-line
+    ) => <a onClick={onClick} {...props} />;
+
+    return (
+        <LenkepanelBase
+            className={className}
+            href={href}
+            onClick={() => trackEvent(gaCategory, gaAction, gaUrl || href)}
+            linkCreator={linkCreator}
+            border
+        >
+            <div className="lenkepanel__innhold">
+                <div className="lenkepanel__ikon">{children}</div>
+                <div>
+                    {overskrift}
+                    {ingress ? <Normaltekst>{ingress}</Normaltekst> : ''}
+                    {etikett ? <Undertekst className="lenkepanel__etikett">{etikett}</Undertekst> : ''}
+                </div>
+            </div>
+        </LenkepanelBase>
+    );
+};
+
+export default LenkepanelMedIkon;
---
+++ src/komponenter/dittnav/components/PanelOverskrift.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Element, Normaltekst, Undertittel } from 'nav-frontend-typografi';
+
+const PanelOverskrift = ({ overskrift, type }: { overskrift: string; type: string }) => {
+    if (type === 'Undertittel') {
+        return <Undertittel className="lenkepanel__heading">{overskrift}</Undertittel>;
+    }
+
+    if (type === 'Element') {
+        return <Element className="lenkepanel__heading">{overskrift}</Element>;
+    }
+
+    if (type === 'Normaltekst') {
+        return <Normaltekst className="lenkepanel__heading">{overskrift}</Normaltekst>;
+    }
+    return overskrift;
+};
+
+export default PanelOverskrift;
