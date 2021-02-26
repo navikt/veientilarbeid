6bffd00138118c00d7c155266c33de6bdc1cceda
Julian Ravn Thrap-Meyer <Julian.Ravn.Thrap-Meyer@nav.no>
2021-02-26 13:28:39 +0100

    Legger til safeguarding rundt localStorage-kall for private-mode og lignende

--- src/utils/localStorage-utils.ts
+++ src/utils/localStorage-utils.ts
@@ -1,11 +1,23 @@
 export const hentFraLocalStorage = (key: string): string | null => {
-    return window.localStorage.getItem(key);
+    try {
+        return window.localStorage.getItem(key);
+    } catch (e) {
+        return null;
+    }
 };
 
 export const settILocalStorage = (key: string, value: string): void => {
-    window.localStorage.setItem(key, value);
+    try {
+        window.localStorage.setItem(key, value);
+    } catch (e) {
+        // Ignorer manglende localStorage. Enten full disk, privat nettlesermodus, eller annet lignende.
+    }
 };
 
 export const slettFraLocalStorage = (key: string): void => {
-    window.localStorage.removeItem(key);
+    try {
+        window.localStorage.removeItem(key);
+    } catch (e) {
+        // Uproblematisk å ikke få slettet dersom localStorage mangler
+    }
 };
