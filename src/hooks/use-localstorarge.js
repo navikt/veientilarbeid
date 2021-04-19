31bfc8f7898c29ccfeb425e4c6cf7a9385d70035
Julian Ravn Thrap-Meyer <Julian.Ravn.Thrap-Meyer@nav.no>
2021-04-19 14:30:56 +0200

    e Kjør prettier på use-localstorage.js

--- src/hooks/use-localstorarge.js
+++ src/hooks/use-localstorarge.js
@@ -1,26 +1,25 @@
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react';
 
-function useLocalStorage (key, defaultValue, { serialize = JSON.stringify, deserialize = JSON.parse } = {}) {
-  const [state, setState] = useState(() => {
-    const itemInStorage = window.localStorage.getItem(key)
-    if (itemInStorage) {
-      return deserialize(itemInStorage)
-    }
-    return typeof defaultValue === 'function' ? defaultValue() : (defaultValue || '')
-  })
+function useLocalStorage(key, defaultValue, { serialize = JSON.stringify, deserialize = JSON.parse } = {}) {
+    const [state, setState] = useState(() => {
+        const itemInStorage = window.localStorage.getItem(key);
+        if (itemInStorage) {
+            return deserialize(itemInStorage);
+        }
+        return typeof defaultValue === 'function' ? defaultValue() : defaultValue || '';
+    });
+    const prevKeyRef = useRef(key);
 
-  const prevKeyRef = useRef(key)
+    useEffect(() => {
+        const prevKey = prevKeyRef.current;
+        if (prevKey !== key) {
+            window.localStorage.removeItem(prevKey);
+        }
+        prevKeyRef.current = key;
+        window.localStorage.setItem(key, serialize(state));
+    }, [key, state, serialize]);
 
-  useEffect(() => {
-    const prevKey = prevKeyRef.current
-    if (prevKey !== key) {
-      window.localStorage.removeItem(prevKey)
-    }
-    prevKeyRef.current = key
-    window.localStorage.setItem(key, serialize(state))
-  }, [key, state, serialize])
-
-  return [state, setState]
+    return [state, setState];
 }
 
-export { useLocalStorage }
+export { useLocalStorage };
