e954aeb81fa3146b9e9f63d9f17f5cc23084ebc5
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-04-30 07:33:47 +0200

    Endrer anonym eksport til navngitt - brukerinfoMock

--- src/mocks/bruker-info-mock.ts
+++ src/mocks/bruker-info-mock.ts
@@ -1,7 +1,9 @@
-export default {
+const brukerinfoMock = {
     erSykmeldtMedArbeidsgiver: false,
     registreringType: 'INGEN_VERDI',
     geografiskTilknytning: '3808',
     rettighetsgruppe: 'DAGP',
     alder: 32,
 };
+
+export default brukerinfoMock;
