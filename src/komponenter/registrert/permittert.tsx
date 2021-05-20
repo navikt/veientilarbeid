0cbe91822bf44292625cce0d8cf4ac592afd5eaa
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-05-20 08:09:40 +0200

    F legger til lenker for informasjon om permittering og dagpenger

--- src/komponenter/registrert/permittert.tsx
+++ src/komponenter/registrert/permittert.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Normaltekst } from 'nav-frontend-typografi';
+import Lenke from 'nav-frontend-lenker';
 import './registrert.less';
 
 interface Props {
@@ -15,9 +16,19 @@ function Permittert(props: Props) {
                 Ha tett kontakt med arbeidsgiveren din om situasjonen fremover, nå når du er permittert.
             </Normaltekst>
             <Normaltekst>
-                Når du har begynt i jobben din igjen, eller mister jobben, så gir du beskjed til NAV slik.
+                Når du har begynt i jobben din igjen, eller mister jobben, så{' '}
+                <Lenke href="https://www.nav.no/arbeid/no/dagpenger#gi-beskjed-hvis-situasjonen-din-endrer-seg">
+                    gir du beskjed til NAV slik
+                </Lenke>
+                .
             </Normaltekst>
-            <Normaltekst>Du finner informasjon om dagpenger og permittering her.</Normaltekst>
+            <Normaltekst>
+                Du finner{' '}
+                <Lenke href="https://www.nav.no/arbeid/no/permittert">
+                    informasjon om dagpenger og permittering her
+                </Lenke>
+                .
+            </Normaltekst>
         </div>
     );
 }
