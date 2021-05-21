dd87f0e4a74fb7eb68364e2f1ddf0019da209a6e
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-05-21 07:11:18 +0200

    F legger til test av permittert komponenten

---
+++ src/komponenter/registrert/permittert.test.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import '@testing-library/jest-dom/extend-expect';
+import { render, screen } from '@testing-library/react';
+import Permittert from './permittert';
+
+describe('Test av permittert komponenen', () => {
+    test('Komponenten vises dersom visRegistrertSomPermittert er true', () => {
+        const { container } = render(<Permittert visRegistrertSomPermittert={true} />);
+        expect(container).not.toBeEmptyDOMElement();
+        expect(screen.getByText(/ha tett kontakt med arbeidsgiveren din om situasjonen fremover/i)).toBeInTheDocument();
+    });
+
+    test('Komponenten vises IKKE dersom visRegistrertSomPermittert er false', () => {
+        const { container } = render(<Permittert visRegistrertSomPermittert={false} />);
+        expect(container).toBeEmptyDOMElement();
+    });
+});
