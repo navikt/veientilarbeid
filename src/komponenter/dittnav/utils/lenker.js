c408695ee02826f3b67ab8be03ebe3f4c8287d87
Michael Gundersen <21148282+theagilepadawan@users.noreply.github.com>
2021-05-27 09:00:37 +0200

    F Fjerner q0-miljøet

--- src/komponenter/dittnav/utils/lenker.js
+++ src/komponenter/dittnav/utils/lenker.js
@@ -1,39 +1,46 @@
-function getEnvironment () {
-  let environment = 'test'
-  if (typeof window !== undefined && /localhost/.test(window.location)) {
-    environment = 'dev'
-  } else if (typeof window !== undefined && /q1/.test(window.location)) {
-    environment = 'q1'
-  } else if (typeof window !== undefined && /q0/.test(window.location)) {
-    environment = 'q0'
-  } else if (typeof window !== undefined && !/q/.test(window.location)) {
-    environment = 'prod'
-  }
-  return environment
+function getEnvironment() {
+    let environment = 'test';
+    if (typeof window !== undefined && /localhost/.test(window.location)) {
+        environment = 'dev';
+    } else if (typeof window !== undefined && /q1/.test(window.location)) {
+        environment = 'q1';
+    } else if (typeof window !== undefined && !/q/.test(window.location)) {
+        environment = 'prod';
+    }
+    return environment;
 }
 
-
-
-const env = getEnvironment()
-const NAVNO_URL = env === 'prod' ? 'https://www.nav.no' : /q/.test(env) ? `https://www-${env}.nav.no` : 'http://localhost:3002'
-const TJENESTER_URL = env === 'prod' ? 'https://tjenester.nav.no' : /q/.test(env) ? `https://tjenester-${env}.nav.no` : 'http://localhost:3002'
-const VEILEDERARBEIDSSOKER_URL = env === 'prod' ? 'https://veiledearbeidssoker.nav.no' : /q/.test(env) ? 'https://veiledearbeidssoker-q.nav.no' : 'http://localhost:3002'
+const env = getEnvironment();
+const NAVNO_URL =
+    env === 'prod' ? 'https://www.nav.no' : /q/.test(env) ? `https://www-${env}.nav.no` : 'http://localhost:3002';
+const TJENESTER_URL =
+    env === 'prod'
+        ? 'https://tjenester.nav.no'
+        : /q/.test(env)
+        ? `https://tjenester-${env}.nav.no`
+        : 'http://localhost:3002';
+const VEILEDERARBEIDSSOKER_URL =
+    env === 'prod'
+        ? 'https://veiledearbeidssoker.nav.no'
+        : /q/.test(env)
+        ? 'https://veiledearbeidssoker-q.nav.no'
+        : 'http://localhost:3002';
 
 export const lenker = {
-  skjemaer: {
-    tittel: 'Skjemaer',
-    url: `${NAVNO_URL}/soknader`,
-  },
-  dinPensjon: {
-    tittel: 'Din pensjon',
-    url: `${TJENESTER_URL}/pselv/publisering/dinpensjon.jsf`,
-  },
-  veilederArbeidssoker: {
-    tittel: 'Veileder for arbeidssøker',
-    url: `${VEILEDERARBEIDSSOKER_URL}`,
-  },
-  dittSykefravaer: {
-    tittel: 'Ditt sykefravær',
-    url: `${TJENESTER_URL}/sykefravaer`,
-  },
+    skjemaer: {
+        tittel: 'Skjemaer',
+        url: `${NAVNO_URL}/soknader`,
+    },
+    dinPensjon: {
+        tittel: 'Din pensjon',
+        url: `${TJENESTER_URL}/pselv/publisering/dinpensjon.jsf`,
+    },
+    veilederArbeidssoker: {
+        tittel: 'Veileder for arbeidssøker',
+        url: `${VEILEDERARBEIDSSOKER_URL}`,
+    },
+    dittSykefravaer: {
+        tittel: 'Ditt sykefravær',
+        url: `${TJENESTER_URL}/sykefravaer`,
+    },
 };
