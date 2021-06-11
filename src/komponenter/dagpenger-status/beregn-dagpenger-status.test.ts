6e2cf4fd0a6ac13897a9a411143299a90cf9d8f2
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-06-11 10:31:45 +0200

    F innfører harPaabegynteSoknader i beregnDagpengerStatus

--- src/komponenter/dagpenger-status/beregn-dagpenger-status.test.ts
+++ src/komponenter/dagpenger-status/beregn-dagpenger-status.test.ts
@@ -12,4 +12,58 @@ describe('tester funksjonen beregnDagpengerStatus', () => {
         });
         expect(status).toBe('mottarDagpenger');
     });
+
+    test('returnerer harPaabegynteSoknader når det er påbegynte søknader etter registreringsdato', () => {
+        const opprettetRegistreringDato = new Date('2021-06-01');
+        const paabegynteSoknader = [
+            {
+                tittel: 'Søknad om dagpenger (ikke permittert)',
+                lenke: 'https://tjenester-q1.nav.no/soknaddagpenger-innsending/soknad/10010WQX9',
+                dato: '2021-06-02T15:48:19.375+02:00',
+                kilde: 'HENVENDELSE',
+            },
+        ];
+        const status = beregnDagpengerStatus({
+            rettighetsgruppe: 'ITY',
+            opprettetRegistreringDato,
+            paabegynteSoknader,
+        });
+        expect(status).toBe('harPaabegynteSoknader');
+    });
+
+    test('returnerer ukjentStatus når det er påbegynte søknader etter registreringsdato men registreringsdato er ukjent', () => {
+        const opprettetRegistreringDato = null;
+        const paabegynteSoknader = [
+            {
+                tittel: 'Søknad om dagpenger (ikke permittert)',
+                lenke: 'https://tjenester-q1.nav.no/soknaddagpenger-innsending/soknad/10010WQX9',
+                dato: '2021-06-02T15:48:19.375+02:00',
+                kilde: 'HENVENDELSE',
+            },
+        ];
+        const status = beregnDagpengerStatus({
+            rettighetsgruppe: 'ITY',
+            opprettetRegistreringDato,
+            paabegynteSoknader,
+        });
+        expect(status).toBe('ukjentStatus');
+    });
+
+    test('returnerer ukjentStatus når det er påbegynte søknader før registreringsdato', () => {
+        const opprettetRegistreringDato = new Date('2021-06-01');
+        const paabegynteSoknader = [
+            {
+                tittel: 'Søknad om dagpenger (ikke permittert)',
+                lenke: 'https://tjenester-q1.nav.no/soknaddagpenger-innsending/soknad/10010WQX9',
+                dato: '2021-05-31T15:48:19.375+02:00',
+                kilde: 'HENVENDELSE',
+            },
+        ];
+        const status = beregnDagpengerStatus({
+            rettighetsgruppe: 'ITY',
+            opprettetRegistreringDato,
+            paabegynteSoknader,
+        });
+        expect(status).toBe('ukjentStatus');
+    });
 });
--- src/komponenter/dagpenger-status/beregn-dagpenger-status.ts
+++ src/komponenter/dagpenger-status/beregn-dagpenger-status.ts
@@ -1,10 +1,23 @@
+import { Soknad } from '../../ducks/paabegynte-soknader';
+
 export interface Beregningsdata {
     rettighetsgruppe?: string;
+    opprettetRegistreringDato?: Date | null;
+    paabegynteSoknader?: Soknad[];
 }
 
 function beregnDagpengerStatus(data: Beregningsdata): string {
-    const { rettighetsgruppe } = data;
+    const { rettighetsgruppe, opprettetRegistreringDato, paabegynteSoknader } = data;
     let status = 'ukjentStatus';
+    // Sjekker om det er påbegynte søknader etter registreringsdato
+    if (opprettetRegistreringDato && paabegynteSoknader && paabegynteSoknader.length > 0) {
+        const paabegynteSoknaderEtterRegistrering = paabegynteSoknader.filter(
+            (soknad) => new Date(soknad.dato) > opprettetRegistreringDato
+        );
+        if (paabegynteSoknaderEtterRegistrering.length > 0) {
+            status = 'harPaabegynteSoknader';
+        }
+    }
     if (rettighetsgruppe === 'DAGP') {
         status = 'mottarDagpenger';
     }
