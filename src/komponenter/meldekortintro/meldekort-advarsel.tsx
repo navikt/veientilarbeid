ff574e65ea3889470989265c7e2c5d0c5836d39c
Jørgen Tu Sveli <jorgen.tu.sveli@nav.no>
2021-06-18 15:55:04 +0200

    F!! Gjør meldekort-advarsel klar over om man har dagpengesøknad i spill eller ikke

--- src/komponenter/meldekortintro/meldekort-advarsel.tsx
+++ src/komponenter/meldekortintro/meldekort-advarsel.tsx
@@ -4,14 +4,40 @@ import { BrukerInfoContext } from '../../ducks/bruker-info';
 import { beregnDagerTilInaktivering } from '../../utils/meldekort-utils';
 import { datoMedUkedag, plussDager } from '../../utils/date-utils';
 import { hentIDag } from '../../utils/chrono';
+import * as PaabegynteSoknader from '../../ducks/paabegynte-soknader';
+import beregnDagpengerStatus, { DagpengerSokestatuser } from '../dagpenger-status/beregn-dagpenger-status';
+import * as Sakstema from '../../ducks/sakstema';
+import * as Brukerregistrering from '../../ducks/brukerregistrering';
 
 function MeldekortAdvarsel({ dagerEtterFastsattMeldedag }: { dagerEtterFastsattMeldedag: number | null }) {
     const { rettighetsgruppe } = React.useContext(BrukerInfoContext).data;
+    const { data: registreringData } = React.useContext(Brukerregistrering.BrukerregistreringContext);
+    const { data: paabegynteSoknaderData } = React.useContext(PaabegynteSoknader.PaabegynteSoknaderContext);
+    const { data: sakstemaData } = React.useContext(Sakstema.SakstemaContext);
+
+    const opprettetRegistreringDatoString = registreringData?.registrering?.opprettetDato;
+    const opprettetRegistreringDato = opprettetRegistreringDatoString
+        ? new Date(opprettetRegistreringDatoString)
+        : null;
+
+    const dagpengerSaksTema = sakstemaData.sakstema.find((tema) => tema.temakode === 'DAG');
+    const behandlingskjeder = dagpengerSaksTema ? dagpengerSaksTema.behandlingskjeder : null;
+
+    const dagpengerStatus = beregnDagpengerStatus({
+        behandlingskjeder,
+        opprettetRegistreringDato,
+        paabegynteSoknader: paabegynteSoknaderData.soknader,
+        rettighetsgruppe,
+    });
+
     if (dagerEtterFastsattMeldedag === null) return null;
 
     const dagerTilInaktivering = beregnDagerTilInaktivering(dagerEtterFastsattMeldedag);
     // Viser strenger melding fra dag 3 (torsdag)
-    const tillegg = dagerEtterFastsattMeldedag > 2 ? <LittStrengereVarsel rettighetsgruppe={rettighetsgruppe} /> : null;
+    const tillegg =
+        dagerEtterFastsattMeldedag > 2 ? (
+            <LittStrengereVarsel rettighetsgruppe={rettighetsgruppe} dagpengerStatus={dagpengerStatus} />
+        ) : null;
     const iDag = hentIDag();
     const inaktiveringsDato = plussDager(iDag, dagerTilInaktivering);
 
@@ -37,23 +63,35 @@ function MeldekortAdvarsel({ dagerEtterFastsattMeldedag }: { dagerEtterFastsattM
     );
 }
 
-const LittStrengereVarsel = ({ rettighetsgruppe }: { rettighetsgruppe: string }) => {
-    const dagpengerKonsekvensMelding = {
-        DAGP: 'utbetaling av dagpenger stoppes',
-        IYT: 'en eventuell søknad om dagpenger kan bli avslått',
-    };
+const LittStrengereVarsel = ({
+    rettighetsgruppe,
+    dagpengerStatus,
+}: {
+    rettighetsgruppe: string;
+    dagpengerStatus: DagpengerSokestatuser;
+}) => {
+    const dagpengerKonsekvensMelding = (() => {
+        switch (dagpengerStatus) {
+            case DagpengerSokestatuser.mottarDagpenger:
+                return 'utbetaling av dagpenger stoppes';
+            case DagpengerSokestatuser.soknadUnderBehandling:
+            case DagpengerSokestatuser.harPaabegynteSoknader:
+                return 'din søknad om dagpenger kan bli avslått';
+            default:
+                return 'en eventuell søknad om dagpenger kan bli avslått';
+        }
+    })();
 
     return (
         <div className={'strenger-varsel'}>
             <Element>Dersom du ikke sender inn meldekort vil</Element>
-
             <ul className={'konsekvenser'}>
                 <li>
                     <Normaltekst>du ikke lenger være registrert som arbeidssøker</Normaltekst>
                 </li>
                 {['DAGP', 'IYT'].includes(rettighetsgruppe) && (
                     <li>
-                        <Normaltekst>{dagpengerKonsekvensMelding[rettighetsgruppe]}</Normaltekst>
+                        <Normaltekst>{dagpengerKonsekvensMelding}</Normaltekst>
                     </li>
                 )}
             </ul>
