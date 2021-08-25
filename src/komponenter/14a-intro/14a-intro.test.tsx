fd0f5542cb8c660c2aebf408e53d3c5e25d8b346
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-08-25 06:58:42 +0200

    r oppdaterte test for visning etter uke 12

--- src/komponenter/14a-intro/14a-intro.test.tsx
+++ src/komponenter/14a-intro/14a-intro.test.tsx
@@ -51,15 +51,15 @@ describe('tester onboarding komponenten for 14a-intro', () => {
         expect(container).toBeEmptyDOMElement();
     });
 
-    test('komponenten vises IKKE når bruker har vært registrert i 13 uker', () => {
+    test('komponenten VISES også når bruker har vært registrert i 13 uker', () => {
         const { container } = render(<Intro14AWrapper />, {
             wrapper: contextProviders({
                 ...providerProps,
                 featureToggle: { 'veientilarbeid.14a-intro': true },
-                amplitude: { ukerRegistrert: 13 },
+                amplitude: { ...providerProps.amplitude, ukerRegistrert: 13 },
             }),
         });
-        expect(container).toBeEmptyDOMElement();
+        expect(container).not.toBeEmptyDOMElement();
     });
 
     test('komponenten vises IKKE når eksperimentet onboarding14a ikke er med', () => {
