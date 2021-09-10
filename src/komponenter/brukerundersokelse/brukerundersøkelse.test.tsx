4f150415de9d7f2522cf1303a33c1a0ce22c27be
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-09-10 09:49:46 +0200

    r legger til basic test av brukerundersøkelse-komponenten

---
+++ src/komponenter/brukerundersokelse/brukerundersøkelse.test.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import '@testing-library/jest-dom/extend-expect';
+import { render } from '@testing-library/react';
+
+import Brukerundersokelse from './brukerundersokelse';
+import { contextProviders, ProviderProps } from '../../test/test-context-providers';
+import { mockIntersectionObserver } from '../../mocks/intersection-observer-mock';
+
+const providerProps: ProviderProps = {
+    amplitude: {
+        ukerRegistrert: 2,
+        gruppe: 'kss',
+        eksperimenter: ['onboarding14a'],
+    },
+};
+
+describe('tester komponenten for brukerundersøkelser', () => {
+    beforeEach(() => {
+        mockIntersectionObserver();
+    });
+
+    test('komponenten vises IKKE når featuretoggle ikke er satt', () => {
+        const { container } = render(<Brukerundersokelse />, { wrapper: contextProviders(providerProps) });
+        expect(container).toBeEmptyDOMElement();
+    });
+
+    test('komponenten VISES når featureToggle ER satt', () => {
+        const { container } = render(<Brukerundersokelse />, {
+            wrapper: contextProviders({
+                ...providerProps,
+                featureToggle: { 'veientilarbeid.visbrukerundersokelse': true },
+            }),
+        });
+        expect(container).not.toBeEmptyDOMElement();
+    });
+});
