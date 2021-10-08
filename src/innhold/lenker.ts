caffe19a1a8ce9907bbeade30cb919eec493cebf
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-10-08 08:09:40 +0200

    r legger til lenke for registrering

--- src/innhold/lenker.ts
+++ src/innhold/lenker.ts
@@ -24,6 +24,9 @@ export const motestotteLenke = erMikrofrontend() ? `${contextpathDittNav}/start-
 export const reaktiveringLenke = erMikrofrontend()
     ? `${contextpathDittNav}/arbeidssokerregistrering/start`
     : '/arbeidssokerregistrering/start';
+export const registreringsLenke = erMikrofrontend()
+    ? `${contextpathDittNav}/arbeidssokerregistrering/start`
+    : '/arbeidssokerregistrering/start';
 export const sykepengerLenke =
     'https://www.nav.no/no/Person/Arbeid/Sykmeldt%2C+arbeidsavklaringspenger+og+yrkesskade/Sykepenger/Sykepenger+til+arbeidstakere#chapter-7';
 export const meldekortLenke = 'https://www.nav.no/meldekort';
