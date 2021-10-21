f5f19c90dcf0fd52e8de32eef07d2c2d13a0eb47
Sondre Wittek <sondrewittek@nav.no>
2021-10-21 12:44:45 +0200

    R flytter introkort for standard til egen mappe

---
+++ src/komponenter/meldekortintro/standard/Startkort.tsx
@@ -0,0 +1,21 @@
+import PreState from '../../meldekortintro/pre-state';
+
+interface Props {
+    hoppOverIntroCB: () => void;
+    startIntroCB: () => void;
+}
+
+function Startkort(props: Props) {
+    const { startIntroCB, hoppOverIntroCB } = props;
+    return (
+        <PreState
+            tittel={'Det viktigste du trenger å vite om meldekort'}
+            lesetid={'2'}
+            viewportTekst="Viser meldekort pre-state i viewPort"
+            startIntroCB={startIntroCB}
+            hoppOverIntroCB={hoppOverIntroCB}
+        />
+    );
+}
+
+export default Startkort;
---
+++ src/komponenter/meldekortintro/standard/index.tsx
@@ -0,0 +1,5 @@
+import Startkort from './Startkort';
+import Kortliste from './Kortliste';
+import Sluttkort from './Sluttkort';
+
+export { Startkort as StandardStartkort, Kortliste as StandardKortliste, Sluttkort as StandardSluttkort };
