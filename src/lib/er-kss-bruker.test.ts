60ad2944496a15a7ef8e698a00dfcf1d4dcd3f9d
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-10-25 13:01:53 +0200

    F lager funksjon for er-kss-bruker

---
+++ src/lib/er-kss-bruker.test.ts
@@ -0,0 +1,145 @@
+import { DinSituasjonSvar, FremtidigSituasjonSvar } from '../ducks/brukerregistrering';
+import { InnloggingsNiva } from '../ducks/autentisering';
+import { plussDager } from '../utils/date-utils';
+import { POAGruppe } from '../utils/get-poa-group';
+import { EksperimentId } from '../eksperiment/eksperimenter';
+import { erKSSBruker } from './er-kss-bruker';
+import { Formidlingsgruppe, Servicegruppe } from '../ducks/oppfolging';
+
+const eksperiment: EksperimentId = 'onboarding14a';
+const poagruppeKSS: POAGruppe = 'kss';
+const dpVenter: 'nei' = 'nei';
+
+const grunndata = {
+    brukerInfoData: {
+        rettighetsgruppe: 'DAGP',
+        geografiskTilknytning: '110302',
+        alder: 42,
+        erSykmeldtMedArbeidsgiver: false,
+    },
+    oppfolgingData: {
+        formidlingsgruppe: Formidlingsgruppe.ARBS,
+        servicegruppe: Servicegruppe.IKVAL,
+        kanReaktiveres: false,
+        reservasjonKRR: false,
+    },
+    registreringData: {
+        registrering: {
+            opprettetDato: plussDager(new Date(), -78).toISOString(),
+            manueltRegistrertAv: null,
+            besvarelse: {
+                dinSituasjon: DinSituasjonSvar.MISTET_JOBBEN,
+                fremtidigSituasjon: FremtidigSituasjonSvar.INGEN_PASSER,
+                sisteStilling: '',
+                tilbakeIArbeid: '',
+                andreForhold: '',
+                helseHinder: '',
+                utdanning: '',
+                utdanningBestatt: '',
+                utdanningGodkjent: '',
+            },
+            teksterForBesvarelse: [],
+        },
+    },
+    egenvurderingData: {
+        sistOppdatert: plussDager(new Date(), -78).toISOString(),
+    },
+    featuretoggleData: {
+        'veientilarbeid.modal': false,
+        'veientilarbeid.feedback': false,
+        'veientilarbeid.14a-intro': true,
+        'veientilarbeid.registrert-permittert': false,
+        'veientilarbeid.dagpenger-status': false,
+        'veientilarbeid.dpstatus-for-alle': false,
+        'veientilarbeid.egenvurderinguke12': true,
+        'veientilarbeid.rydding.skjulJobbBoks': false,
+        'veientilarbeid.rydding.skjulOkonomiBoks': false,
+        'veientilarbeid.rydding.skjulAAPRad': false,
+        'veientilarbeid.visbrukerundersokelse': false,
+        'veientilarbeid.onboarding14a.situasjonsbestemt': false,
+        'veientilarbeid.meldekort-intro.situasjonsbestemt': false,
+    },
+    amplitudeData: {
+        gruppe: poagruppeKSS,
+        geografiskTilknytning: 'INGEN_VERDI',
+        isKSSX: 'nei',
+        isKSSK: 'nei',
+        erSamarbeidskontor: 'nei',
+        ukerRegistrert: 11,
+        dagerRegistrert: 78,
+        nivaa: InnloggingsNiva.LEVEL_4,
+        kanReaktiveres: 'nei',
+        formidlingsgruppe: 'INGEN_VERDI',
+        servicegruppe: 'IVURD',
+        rettighetsgruppe: 'INGEN_VERDI',
+        meldegruppe: 'INGEN_VERDI',
+        registreringType: 'INGEN_VERDI',
+        underOppfolging: 'nei',
+        antallDagerEtterFastsattMeldingsdag: 'ikke meldekortbruker',
+        antallMeldekortKlareForLevering: 0,
+        gitVersion: 'INGEN_VERDI',
+        buildTimestamp: new Date().toISOString(),
+        antallSynligeInfomeldinger: 0,
+        erSykmeldtMedArbeidsgiver: 'ukjent',
+        dinSituasjon: DinSituasjonSvar.INGEN_VERDI,
+        reservasjonKRR: 'ukjent',
+        eksperimenter: [eksperiment],
+        dagpengerVedleggEttersendes: 0,
+        dagpengerSoknadMellomlagret: 0,
+        dagpengerSoknadVenterPaSvar: dpVenter,
+        dagpengerDagerMellomPaabegyntSoknadOgRegistrering: 0,
+        dagpengerDagerMellomInnsendtSoknadOgRegistrering: 0,
+        dagpengerStatusBeregning: 'INGEN_DATA',
+    },
+    sistVistFraLocalstorage: 0,
+};
+
+describe('Tester funksjonen kanVise14AStatus', () => {
+    test('Nei hvis AAP', () => {
+        const testdata = JSON.parse(JSON.stringify(grunndata));
+        testdata.brukerInfoData.rettighetsgruppe = 'AAP';
+        expect(erKSSBruker(testdata)).toBe(false);
+    });
+
+    test('NEI hvis ikke eksperiment', () => {
+        const testdata = JSON.parse(JSON.stringify(grunndata));
+        testdata.amplitudeData.eksperimenter = [];
+        expect(erKSSBruker(testdata)).toBe(false);
+    });
+
+    test('NEI hvis ikke innefor aldersgruppe', () => {
+        const testdata = JSON.parse(JSON.stringify(grunndata));
+        testdata.brukerInfoData.alder = 56;
+        expect(erKSSBruker(testdata)).toBe(false);
+    });
+
+    test('NEI hvis ikke featureToggle', () => {
+        const testdata = JSON.parse(JSON.stringify(grunndata));
+        testdata.featuretoggleData['veientilarbeid.14a-intro'] = false;
+        expect(erKSSBruker(testdata)).toBe(false);
+    });
+
+    test('NEI hvis ikke bruker kan reaktveres', () => {
+        const testdata = JSON.parse(JSON.stringify(grunndata));
+        testdata.oppfolgingData.kanReaktiveres = true;
+        expect(erKSSBruker(testdata)).toBe(false);
+    });
+
+    test('NEI hvis ikke bruker ikke er standard innsatsgruppe', () => {
+        const testdata = JSON.parse(JSON.stringify(grunndata));
+        testdata.oppfolgingData.servicegruppe = 'BKART';
+        expect(erKSSBruker(testdata)).toBe(false);
+    });
+
+    test('JA hvis ikke bruker skal se eksperiment, er innefor aldersgruppe, har featuretoggle, ikke kan reaktivers og er standard innsatsgruppe', () => {
+        const testdata = JSON.parse(JSON.stringify(grunndata));
+        testdata.amplitudeData.eksperimenter = [eksperiment];
+        testdata.brukerInfoData.alder = 45;
+        testdata.featuretoggleData['veientilarbeid.14a-intro'] = true;
+        testdata.oppfolgingData.kanReaktiveres = false;
+        testdata.oppfolgingData.servicegruppe = 'IKVAL';
+        testdata.oppfolgingData.formidlingsgruppe = 'ARBS';
+
+        expect(erKSSBruker(testdata)).toBe(true);
+    });
+});
---
+++ src/lib/er-kss-bruker.ts
@@ -0,0 +1,36 @@
+import { Data as FeaturetoggleData } from '../ducks/feature-toggles';
+import * as Brukerregistrering from '../ducks/brukerregistrering';
+import * as Oppfolging from '../ducks/oppfolging';
+import * as BrukerInfo from '../ducks/bruker-info';
+import { AmplitudeData } from '../metrics/amplitude-utils';
+import sjekkOmBrukerErStandardInnsatsgruppe from './er-standard-innsatsgruppe';
+
+export function erKSSBruker({
+    brukerInfoData,
+    oppfolgingData,
+    registreringData,
+    amplitudeData,
+    featuretoggleData,
+}: {
+    brukerInfoData: BrukerInfo.Data;
+    oppfolgingData: Oppfolging.Data;
+    registreringData: Brukerregistrering.Data | null;
+    amplitudeData: AmplitudeData;
+    featuretoggleData: FeaturetoggleData;
+}): boolean {
+    const skalSeEksperiment = amplitudeData.eksperimenter.includes('onboarding14a');
+    const erAAP = brukerInfoData.rettighetsgruppe === 'AAP';
+    const brukerregistreringData = registreringData?.registrering ?? null;
+    const featuretoggleAktivert = featuretoggleData && featuretoggleData['veientilarbeid.14a-intro'];
+
+    const aldersgruppeUtenForsterketInnsats = brukerInfoData.alder >= 30 && brukerInfoData.alder <= 55;
+
+    return (
+        featuretoggleAktivert &&
+        aldersgruppeUtenForsterketInnsats &&
+        !erAAP &&
+        skalSeEksperiment &&
+        sjekkOmBrukerErStandardInnsatsgruppe({ brukerregistreringData, oppfolgingData }) &&
+        !oppfolgingData.kanReaktiveres
+    );
+}
