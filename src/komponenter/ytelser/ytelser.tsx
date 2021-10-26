4a7b30a8b975bed9cd5916514b07b441a3a66427
Sondre Wittek <sondrewittek@gmail.com>
2021-10-26 11:22:07 +0200

    f legger til innmat i ytelser-kortet

--- src/komponenter/ytelser/ytelser.tsx
+++ src/komponenter/ytelser/ytelser.tsx
@@ -4,9 +4,13 @@ import * as Oppfolging from '../../ducks/oppfolging';
 import * as BrukerInfo from '../../ducks/bruker-info';
 import { FeaturetoggleContext } from '../../ducks/feature-toggles';
 import { AmplitudeContext } from '../../ducks/amplitude-context';
+import { amplitudeLogger } from '../../metrics/amplitude-utils';
 
 import sjekkOmBrukerErStandardInnsatsgruppe from '../../lib/er-standard-innsatsgruppe';
 import { erKSSBruker } from '../../lib/er-kss-bruker';
+import Panel from 'nav-frontend-paneler';
+import Lenke from 'nav-frontend-lenker';
+import { Element, Normaltekst, Systemtittel } from 'nav-frontend-typografi';
 
 function Ytelser() {
     const { data: registreringData } = useContext(Brukerregistrering.BrukerregistreringContext);
@@ -25,9 +29,63 @@ function Ytelser() {
     });
     const kanViseKomponent = erStandardInnsatsgruppe && !erDetteKSSBruker;
 
+    function loggLenkeKlikk(action: string, url: string) {
+        amplitudeLogger('veientilarbeid.action', {
+            action,
+            ...amplitudeData,
+        });
+        window.location.assign(url);
+    }
+
     if (!kanViseKomponent) return null;
 
-    return <div>Ytelser!</div>;
+    return (
+        <div className={'ytelser-omslutning'}>
+            <Panel className={'ytelser'} border>
+                <Element tag={'h1'}>SPØRSMÅL OM YTELSER</Element>
+                <Systemtittel className={'blokk-xs'}>
+                    Har du spørsmål om å søke om eller motta pengestøtte fra NAV?
+                </Systemtittel>
+                <Normaltekst className={'blokk-xs'}>
+                    Har du spørsmål om å søke eller motta ytelser fra NAV, må du bruke{' '}
+                    <Lenke
+                        href="https://mininnboks.nav.no/sporsmal/skriv/ARBD"
+                        onClick={() =>
+                            loggLenkeKlikk(
+                                'Går til STO fra ytelse kort',
+                                'https://mininnboks.nav.no/sporsmal/skriv/ARBD'
+                            )
+                        }
+                    >
+                        «Skriv til oss»
+                    </Lenke>{' '}
+                    eller{' '}
+                    <Lenke
+                        href="https://www.nav.no/person/kontakt-oss/chat/"
+                        onClick={() =>
+                            loggLenkeKlikk(
+                                'Går til Chat fra ytelse kort',
+                                'https://www.nav.no/person/kontakt-oss/chat/'
+                            )
+                        }
+                    >
+                        «Chat»
+                    </Lenke>
+                    .
+                </Normaltekst>
+                <Normaltekst className={'blokk-m'}>
+                    Du kan lese om livssituasjoner NAV kan hjelpe med på{' '}
+                    <Lenke
+                        href="https://www.nav.no/"
+                        onClick={() => loggLenkeKlikk('Går til forsiden fra ytelse kort', 'https://www.nav.no/')}
+                    >
+                        forsiden{' '}
+                    </Lenke>
+                    av nav.no
+                </Normaltekst>
+            </Panel>
+        </div>
+    );
 }
 
 export default Ytelser;
