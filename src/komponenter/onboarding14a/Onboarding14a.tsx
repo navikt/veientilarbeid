b9852d85353c3775aa5159f92915162f9071fbed
Sondre Wittek <sondrewittek@gmail.com>
2021-10-26 10:19:07 +0200

    r Oppdatert visningslogikk 14a

--- src/komponenter/onboarding14a/Onboarding14a.tsx
+++ src/komponenter/onboarding14a/Onboarding14a.tsx
@@ -2,15 +2,18 @@ import sjekkOmBrukerErStandardInnsatsgruppe from '../../lib/er-standard-innsatsg
 import sjekkOmBrukerErSituasjonsbestemtInnsatsgruppe from '../../lib/er-situasjonsbestemt-innsatsgruppe';
 import * as Brukerregistrering from '../../ducks/brukerregistrering';
 import * as Oppfolging from '../../ducks/oppfolging';
+import * as BrukerInfo from '../../ducks/bruker-info';
 import { FeaturetoggleContext } from '../../ducks/feature-toggles';
 import Kortbunke from './Kortbunke';
 import React from 'react';
 import Enkeltkort from './Enkeltkort';
+import { kanViseOnboarding14A } from '../../lib/kan-vise-onboarding14a';
 
 function Onboarding14a(): JSX.Element | null {
     const { data: registreringData } = React.useContext(Brukerregistrering.BrukerregistreringContext);
     const { data: oppfolgingData } = React.useContext(Oppfolging.OppfolgingContext);
     const { data: featuretoggleData } = React.useContext(FeaturetoggleContext);
+    const { data: brukerInfoData } = React.useContext(BrukerInfo.BrukerInfoContext);
 
     const brukerregistreringData = registreringData?.registrering ?? null;
     const erStandardInnsatsgruppe = sjekkOmBrukerErStandardInnsatsgruppe({ brukerregistreringData, oppfolgingData });
@@ -18,11 +21,15 @@ function Onboarding14a(): JSX.Element | null {
         brukerregistreringData,
         oppfolgingData,
     });
-
     const visOnboardingForSituasjonsbestemtToggle = featuretoggleData['veientilarbeid.onboarding14a.situasjonsbestemt'];
 
     const kanViseSituasjonsbestemt = erSituasjonsbestemtInnsatsgruppe && visOnboardingForSituasjonsbestemtToggle;
-    const kanViseKomponent = erStandardInnsatsgruppe || kanViseSituasjonsbestemt;
+    const kanViseKomponent = kanViseOnboarding14A({
+        featuretoggleData,
+        oppfolgingData,
+        brukerInfoData,
+        registreringData,
+    });
 
     if (!kanViseKomponent) return null;
 
