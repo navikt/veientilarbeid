af664312e50c28d864040c5040d93dc706b5e39a
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-10-27 12:17:07 +0200

    r sletter ubrukt mappe ikke-standard

--- src/komponenter/onboarding14a/ikke-standard/index.tsx
+++
@@ -1,5 +0,0 @@
-import Startkort from './Startkort';
-import Kortliste from './Kortliste';
-import Sluttkort from './Sluttkort';
-
-export { Startkort as IkkeStandardStartkort, Kortliste as IkkeStandardKortliste, Sluttkort as IkkeStandardSluttkort };
