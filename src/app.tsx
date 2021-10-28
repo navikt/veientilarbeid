3fe29e1acbeef725d2b0b006d5e2017638a1686e
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-10-28 13:58:21 +0200

    f legger til støtte for krevStandardInnsatsgruppe på app

--- src/app.tsx
+++ src/app.tsx
@@ -3,11 +3,15 @@ import * as Sentry from '@sentry/react';
 import AutentiseringsInfoFetcher from './komponenter/hent-initial-data/autentiseringsInfoFetcher';
 import { GlobaleInnstillingerProvider } from './context/GlobaleInnstillinger';
 
-class App extends React.Component {
+interface Props {
+    krevStandardInnsatsgruppe?: boolean;
+}
+
+class App extends React.Component<Props> {
     render() {
         return (
             <Sentry.ErrorBoundary>
-                <GlobaleInnstillingerProvider>
+                <GlobaleInnstillingerProvider krevStandardInnsatsgruppe={this.props.krevStandardInnsatsgruppe}>
                     <AutentiseringsInfoFetcher />
                 </GlobaleInnstillingerProvider>
             </Sentry.ErrorBoundary>
