72171ad353639f7a3c8889e812616d164f6e2849
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-10-28 13:08:09 +0200

    r skriver om til å bli funksjon i stedet for klasse

--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,12 @@
-import * as React from 'react';
 import * as Sentry from '@sentry/react';
 import AutentiseringsInfoFetcher from './komponenter/hent-initial-data/autentiseringsInfoFetcher';
 
-class App extends React.Component {
-    render() {
-        return (
-            <Sentry.ErrorBoundary>
-                <AutentiseringsInfoFetcher />
-            </Sentry.ErrorBoundary>
-        );
-    }
+function App() {
+    return (
+        <Sentry.ErrorBoundary>
+            <AutentiseringsInfoFetcher />
+        </Sentry.ErrorBoundary>
+    );
 }
 
 export default App;
