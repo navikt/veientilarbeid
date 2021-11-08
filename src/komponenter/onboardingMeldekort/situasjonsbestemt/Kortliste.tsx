7908254bb16d1ff39299d05dd72ef1accee8e8e6
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-11-08 14:34:26 +0100

    Oppdaterer id på feedback for meldekort situasjonsbestemt

--- src/komponenter/onboardingMeldekort/situasjonsbestemt/Kortliste.tsx
+++ src/komponenter/onboardingMeldekort/situasjonsbestemt/Kortliste.tsx
@@ -16,7 +16,7 @@ function Kort1() {
                     dag.
                 </Normaltekst>
             </div>
-            <Feedback id={'meldekort-kort-01'} />
+            <Feedback id={'meldekort-kort-01-situasjonsbestemt'} />
         </div>
     );
 }
@@ -36,7 +36,7 @@ function Kort2() {
                     Venter du på svar på en innsendt søknad, kan du få avslag dersom du ikke har sendt inn meldekortet.
                 </Normaltekst>
             </div>
-            <Feedback id={'meldekort-kort-02'} />
+            <Feedback id={'meldekort-kort-02-situasjonsbestemt'} />
         </div>
     );
 }
@@ -58,7 +58,7 @@ function Kort3() {
                     sent.
                 </Normaltekst>
             </div>
-            <Feedback id={'meldekort-kort-03'} />
+            <Feedback id={'meldekort-kort-03-situasjonsbestemt'} />
         </div>
     );
 }
