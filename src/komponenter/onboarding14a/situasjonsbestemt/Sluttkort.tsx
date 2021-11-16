6755654a3cf189e7d4cae1ac2fb490b5370db9c0
Sondre Wittek <sondrewittek@gmail.com>
2021-11-16 10:20:05 +0100

    r bruker onboarding-komonent -  14a for situsjonsbestemt

--- src/komponenter/onboarding14a/situasjonsbestemt/Sluttkort.tsx
+++ src/komponenter/onboarding14a/situasjonsbestemt/Sluttkort.tsx
@@ -1,18 +1,15 @@
-import { AmplitudeData, amplitudeLogger } from '../../../metrics/amplitude-utils';
+import { amplitudeLogger } from '../../../metrics/amplitude-utils';
 import { Normaltekst, Systemtittel } from 'nav-frontend-typografi';
-import { OppfolgingContext, Servicegruppe } from '../../../contexts/oppfolging';
+import { Servicegruppe, useOppfolgingData } from '../../../contexts/oppfolging';
 import Lenkepanel14A from '../lenkepanel-14a';
 import Lenke from 'nav-frontend-lenker';
-import { useContext } from 'react';
+import { useAmplitudeData } from '../../../contexts/amplitude-context';
+import { useUlesteDialogerData } from '../../../contexts/ulestedialoger';
 
-interface EndStateProps {
-    amplitudeData: AmplitudeData;
-    antallUlesteDialoger: number;
-}
-
-function Sluttkort(props: EndStateProps) {
-    const { amplitudeData } = props;
-    const { servicegruppe } = useContext(OppfolgingContext).data;
+function Sluttkort() {
+    const amplitudeData = useAmplitudeData();
+    const { antallUleste } = useUlesteDialogerData();
+    const { servicegruppe } = useOppfolgingData();
 
     const handleLesBrev = () => {
         amplitudeLogger('veientilarbeid.intro', {
@@ -22,56 +19,42 @@ function Sluttkort(props: EndStateProps) {
         });
     };
 
-    const VeiledersOppgaver = () => {
-        return (
-            <>
-                <div>
-                    <Systemtittel className={'blokk-xs'}>
-                        {servicegruppe === Servicegruppe.BFORM
-                            ? 'Ta kontakt om du har spørsmål'
-                            : 'Du vil motta et brev som forteller hva NAV kan hjelpe deg med'}
-                    </Systemtittel>
-
-                    <Normaltekst className={'blokk-xs'}>
-                        {servicegruppe === Servicegruppe.BFORM ? (
-                            <>
-                                Du har mottatt brevet{' '}
-                                <Lenke onClick={handleLesBrev} href={'https://mininnboks.nav.no/'}>
-                                    «NAV har vurdert mulighetene dine på arbeidsmarkedet»
-                                </Lenke>
-                                .
-                            </>
-                        ) : (
-                            <>
-                                Brevet får du sendt til{' '}
-                                <Lenke onClick={handleLesBrev} href={'https://mininnboks.nav.no/'}>
-                                    innboksen
-                                </Lenke>{' '}
-                                din innen én uke.
-                            </>
-                        )}
-                    </Normaltekst>
-
-                    <Normaltekst className={'blokk-xs'}>
-                        Dette brevet er ikke et svar på en eventuell søknad om dagpenger.
-                    </Normaltekst>
-                    <Normaltekst className={'blokk-xs'}>
-                        Du kan stille spørsmål til veileder ved å henvende deg via dialogen.
-                    </Normaltekst>
-                    <Lenkepanel14A
-                        amplitudeData={amplitudeData}
-                        href={''}
-                        antallUlesteDialoger={props.antallUlesteDialoger}
-                    />
-                </div>
-            </>
-        );
-    };
-
     return (
-        <div className={'sluttkort'}>
-            <VeiledersOppgaver />
-        </div>
+        <>
+            <Systemtittel className={'blokk-xs'}>
+                {servicegruppe === Servicegruppe.BFORM
+                    ? 'Ta kontakt om du har spørsmål'
+                    : 'Du vil motta et brev som forteller hva NAV kan hjelpe deg med'}
+            </Systemtittel>
+
+            <Normaltekst className={'blokk-xs'}>
+                {servicegruppe === Servicegruppe.BFORM ? (
+                    <>
+                        Du har mottatt brevet{' '}
+                        <Lenke onClick={handleLesBrev} href={'https://mininnboks.nav.no/'}>
+                            «NAV har vurdert mulighetene dine på arbeidsmarkedet»
+                        </Lenke>
+                        .
+                    </>
+                ) : (
+                    <>
+                        Brevet får du sendt til{' '}
+                        <Lenke onClick={handleLesBrev} href={'https://mininnboks.nav.no/'}>
+                            innboksen
+                        </Lenke>{' '}
+                        din innen én uke.
+                    </>
+                )}
+            </Normaltekst>
+
+            <Normaltekst className={'blokk-xs'}>
+                Dette brevet er ikke et svar på en eventuell søknad om dagpenger.
+            </Normaltekst>
+            <Normaltekst className={'blokk-xs'}>
+                Du kan stille spørsmål til veileder ved å henvende deg via dialogen.
+            </Normaltekst>
+            <Lenkepanel14A amplitudeData={amplitudeData} href={''} antallUlesteDialoger={antallUleste} />
+        </>
     );
 }
 
