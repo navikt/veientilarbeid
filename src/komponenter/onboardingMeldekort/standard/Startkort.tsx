8f381508ff3db9c5d516961232fe6b82a1fcd69a
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-11-18 10:20:34 +0100

    f legger til ErRendret og InViewport

--- src/komponenter/onboardingMeldekort/standard/Startkort.tsx
+++ src/komponenter/onboardingMeldekort/standard/Startkort.tsx
@@ -1,14 +1,19 @@
 import { ClockFilled } from '@navikt/ds-icons';
 import { Innholdstittel, Undertekst } from 'nav-frontend-typografi';
 
+import ErRendret from '../../er-rendret/er-rendret';
+import InViewport from '../../in-viewport/in-viewport';
+
 function Startkort() {
     return (
         <>
+            <ErRendret loggTekst="Rendrer meldekort pre-state" />
             <Innholdstittel className={'blokk-xs'}>Det viktigste du trenger å vite om meldekort</Innholdstittel>
             <div className={'lesetid mb-2'}>
                 <ClockFilled className={'mr-05'} />
                 <Undertekst>2 minutter lesetid</Undertekst>
             </div>
+            <InViewport loggTekst="Viser meldekort pre-state i viewport" />
         </>
     );
 }
