674179cb096644031a50d613da7f2ea16eb2dffb
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-11-19 07:07:26 +0100

    r lager sluttkortvelger for dagpenger onboarding

---
+++ src/komponenter/onboarding-dagpenger/sluttkort.tsx
@@ -0,0 +1,23 @@
+import HarIkkeSokt from './sluttkort-har-ikke-sokt';
+import HarPabegyntSoknad from './sluttkort-har-paabegynt-soknad';
+import HarSokt from './sluttkort-har-sokt';
+import MottarDagpenger from './sluttkort-faar-dagpenger';
+
+function hentAktueltSluttkort(situasjon: string) {
+    if (situasjon === 'pabegynt') {
+        return HarPabegyntSoknad;
+    } else if (situasjon === 'sokt') {
+        return HarSokt;
+    } else if (situasjon === 'mottar') {
+        return MottarDagpenger;
+    } else {
+        return HarIkkeSokt;
+    }
+}
+
+function Sluttkort() {
+    const AktueltSluttkort = hentAktueltSluttkort('ikkeSokt');
+    return <AktueltSluttkort />;
+}
+
+export default Sluttkort;
