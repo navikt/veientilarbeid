7d3b4cad5ca25915c6cd7f6062c94103dd8023c8
Sondre Wittek <sondrewittek@gmail.com>
2021-11-19 13:22:09 +0100

    r fikser tester i etter endringer i innhold

--- src/komponenter/onboarding14a/14a-intro.test.tsx
+++ src/komponenter/onboarding14a/14a-intro.test.tsx
@@ -142,7 +142,6 @@ describe('tester onboarding komponenten for 14a-intro', () => {
             }),
         });
         const startKnapp = screen.getByRole('button', { name: /start introduksjonen/i });
-        expect(screen.getByText(/3 minutter lesetid/i)).toBeInTheDocument();
         userEvent.click(startKnapp);
         const forrigeKnapp = screen.getByRole('button', { name: /forrige/i });
         const nesteKnapp = screen.getByRole('button', { name: /neste/i });
--- src/komponenter/onboardingMeldekort/meldekort-intro.test.tsx
+++ src/komponenter/onboardingMeldekort/meldekort-intro.test.tsx
@@ -85,7 +85,6 @@ describe('tester onboarding komponenten for meldekort', () => {
         const props: ProviderProps = { ...providerProps, amplitude: { ukerRegistrert: 0 } };
         render(<MeldekortOnboarding />, { wrapper: contextProviders(props) });
         const startKnapp = screen.getByText(/start introduksjon/i);
-        expect(screen.getByText(/2 minutter lesetid/i)).toBeInTheDocument();
         userEvent.click(startKnapp);
         const nesteknapp = screen.getByText(/neste/i);
         const forrigeknapp = screen.getByText(/forrige/i);
