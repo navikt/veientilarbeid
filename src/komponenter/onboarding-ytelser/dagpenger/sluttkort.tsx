fee95e2df8dac90a14e234cb125fe691def35484
Sondre Wittek <sondrewittek@gmail.com>
2021-11-25 13:10:14 +0100

    r legger til reaktivert på sluttkort

--- src/komponenter/onboarding-ytelser/dagpenger/sluttkort.tsx
+++ src/komponenter/onboarding-ytelser/dagpenger/sluttkort.tsx
@@ -2,6 +2,7 @@ import HarIkkeSokt from './sluttkort-har-ikke-sokt';
 import HarPabegyntSoknad from './sluttkort-har-paabegynt-soknad';
 import HarSokt from './sluttkort-har-sokt';
 import MottarDagpenger from './sluttkort-faar-dagpenger';
+import Reaktivert from './sluttkort-har-blitt-reaktivert';
 
 function hentAktueltSluttkort(situasjon: string) {
     if (situasjon === 'pabegynt') {
@@ -10,6 +11,8 @@ function hentAktueltSluttkort(situasjon: string) {
         return HarSokt;
     } else if (situasjon === 'mottar') {
         return MottarDagpenger;
+    } else if (situasjon === 'reaktivert') {
+        return Reaktivert;
     } else {
         return HarIkkeSokt;
     }
