156284f68c025c431c061bf727582620209541ca
Sondre Wittek <sondrewittek@gmail.com>
2021-11-26 15:28:25 +0100

    r legger til tester på ny lib-funksjon

---
+++ src/lib/er-sannsyligvis-inaktivert-standard-innsatsgruppe.test.ts
@@ -0,0 +1,41 @@
+import { FremtidigSituasjonSvar } from '../contexts/brukerregistrering';
+import { Servicegruppe, Formidlingsgruppe } from '../contexts/oppfolging';
+import erSannsynligvisInaktivertStandardbruker from './er-sannsyligvis-inaktivert-standard-innsatsgruppe';
+
+const grunnData = {
+    brukerregistreringData: {
+        opprettetDato: new Date().toISOString(),
+        manueltRegistrertAv: null,
+        besvarelse: {
+            dinSituasjon: null,
+            fremtidigSituasjon: FremtidigSituasjonSvar.USIKKER,
+            sisteStilling: null,
+            tilbakeIArbeid: null,
+            andreForhold: null,
+            helseHinder: null,
+            utdanning: null,
+            utdanningBestatt: null,
+            utdanningGodkjent: null,
+            teksterForBesvarelse: null,
+        },
+        teksterForBesvarelse: null,
+    },
+    oppfolgingData: {
+        kanReaktiveres: true,
+        reservasjonKRR: false,
+        servicegruppe: Servicegruppe.IKVAL,
+        formidlingsgruppe: Formidlingsgruppe.ARBS,
+    },
+};
+
+describe('tester funksjonaliteten for erStandardInnsatsgruppe', () => {
+    test('returnerer true for ARBS + IKVAL + kan reaktiveres', () => {
+        const testData = { ...grunnData };
+        expect(erSannsynligvisInaktivertStandardbruker(testData)).toBe(true);
+    });
+
+    test('returnerer false for ARBS + IKVAL og ikke kan reaktiveres', () => {
+        const testData = { ...grunnData, oppfolgingData: { ...grunnData.oppfolgingData, kanReaktiveres: false } };
+        expect(erSannsynligvisInaktivertStandardbruker(testData)).toBe(false);
+    });
+});
