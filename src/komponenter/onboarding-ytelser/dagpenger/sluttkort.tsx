ee2c89fce3cc23707539944f497b441727958b11
Jostein Holje <jostein.holje@nav.no>
2021-12-10 10:47:52 +0100

    r Bruk beregnDagpengeStatus i komponent

--- src/komponenter/onboarding-ytelser/dagpenger/sluttkort.tsx
+++ src/komponenter/onboarding-ytelser/dagpenger/sluttkort.tsx
@@ -3,6 +3,13 @@ import HarPabegyntSoknad from './sluttkort-har-paabegynt-soknad';
 import HarSokt from './sluttkort-har-sokt';
 import MottarDagpenger from './sluttkort-faar-dagpenger';
 import Reaktivert from './sluttkort-har-blitt-reaktivert';
+import { useBrukerinfoData } from '../../../contexts/bruker-info';
+import { useBrukerregistreringData } from '../../../contexts/brukerregistrering';
+import { usePaabegynteSoknaderData } from '../../../contexts/paabegynte-soknader';
+import { useDpInnsynSoknadData } from '../../../contexts/dp-innsyn-soknad';
+import { useDpInnsynVedtakData } from '../../../contexts/dp-innsyn-vedtak';
+import { useMeldekortData } from '../../../contexts/meldekort';
+import beregnDagpengeStatus from '../../../lib/beregn-dagpenge-status';
 
 function hentAktueltSluttkort(situasjon: string) {
     if (situasjon === 'pabegynt') {
@@ -19,7 +26,23 @@ function hentAktueltSluttkort(situasjon: string) {
 }
 
 function Sluttkort() {
-    const AktueltSluttkort = hentAktueltSluttkort('');
+    const brukerInfoData = useBrukerinfoData();
+    const registreringData = useBrukerregistreringData();
+    const paabegynteSoknader = usePaabegynteSoknaderData().soknader;
+    const innsendteSoknader = useDpInnsynSoknadData()?.soknad || [];
+    const dagpengeVedtak = useDpInnsynVedtakData().vedtak || [];
+    const meldekort = useMeldekortData()?.meldekort || [];
+
+    const dagpengeStatus = beregnDagpengeStatus({
+        brukerInfoData,
+        registreringData,
+        paabegynteSoknader,
+        innsendteSoknader,
+        dagpengeVedtak,
+        meldekort,
+    });
+
+    const AktueltSluttkort = hentAktueltSluttkort(dagpengeStatus);
     return <AktueltSluttkort />;
 }
 
