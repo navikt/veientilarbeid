496d68e6c6248d70005f15cae33f1d719b1e16e6
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-12-21 07:39:10 +0100

    r legger til diverse Amplitude på Dagpengar og utdanning

--- src/komponenter/meldinger/dagpenger-og-utdanning.tsx
+++ src/komponenter/meldinger/dagpenger-og-utdanning.tsx
@@ -1,15 +1,27 @@
 import { Alert, BodyShort, Link } from '@navikt/ds-react';
 
 import { useBrukerinfoData } from '../../contexts/bruker-info';
+import { useAmplitudeData } from '../../contexts/amplitude-context';
+import ErRendret from '../er-rendret/er-rendret';
+import InViewport from '../in-viewport/in-viewport';
+import { loggAktivitet } from '../../metrics/metrics';
+
+const DAGPENGAR_OG_UTDANNING_URL = 'https://www.nav.no/arbeid/utdanning';
 
 function DagpengerOgUtdanning() {
     const { rettighetsgruppe } = useBrukerinfoData();
+    const amplitudeData = useAmplitudeData();
     const kanViseKomponent = rettighetsgruppe === 'DAGP';
+
+    function onLinkClick() {
+        loggAktivitet({ aktivitet: 'Går til Dagpengar og utdanning fra infoboksen', ...amplitudeData });
+    }
 
     if (!kanViseKomponent) return null;
 
     return (
         <Alert variant="info" className="blokk-xs">
+            <ErRendret loggTekst="Rendrer Dagpengar og utdanning" />
             <BodyShort className="blokk-xs">
                 Mottek du dagpengar og skal ta utdanning eller opplæring i 2022, må du søke om å kombinere dagpengar og
                 utdanning.
@@ -19,8 +31,11 @@ function DagpengerOgUtdanning() {
                 avtalt dato i 2022, eller seinare, treng du ikkje søke.
             </BodyShort>
             <BodyShort>
-                <Link href="https://www.nav.no/arbeid/utdanning">Les meir om dagpengar og utdanning</Link>
+                <Link href={DAGPENGAR_OG_UTDANNING_URL} onClick={onLinkClick}>
+                    Les meir om dagpengar og utdanning
+                </Link>
             </BodyShort>
+            <InViewport loggTekst="Dagpengar og utdanning i viewPort" />
         </Alert>
     );
 }
