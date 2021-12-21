490cf3acb0c09f133c117e7455fc508a3181c41a
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-12-21 07:27:02 +0100

    r fikser typo

--- src/komponenter/tema-panel/tema-panel.tsx
+++ src/komponenter/tema-panel/tema-panel.tsx
@@ -1,13 +1,13 @@
 import MeldekortOnboarding from '../onboardingMeldekort/meldekort-onboarding';
 import Onboarding14A from '../onboarding14a/Onboarding14a';
-import YtelserOnboaridng from '../onboarding-ytelser/ytelser-onboarding';
+import YtelserOnboarding from '../onboarding-ytelser/ytelser-onboarding';
 import './tema-panel.less';
 const Temapanel = () => {
     return (
         <div className="intro-wrapper">
             <Onboarding14A />
             <MeldekortOnboarding />
-            <YtelserOnboaridng />
+            <YtelserOnboarding />
         </div>
     );
 };
