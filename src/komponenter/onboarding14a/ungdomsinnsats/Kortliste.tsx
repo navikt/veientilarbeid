9233962010a5b1ef35da28a7143b37cbf5eb1d7e
Jostein Holje <jostein.holje@nav.no>
2022-01-17 13:28:20 +0100

    r Oppdaterte tekster for ungdomsinnsats-kortene

--- src/komponenter/onboarding14a/ungdomsinnsats/Kortliste.tsx
+++ src/komponenter/onboarding14a/ungdomsinnsats/Kortliste.tsx
@@ -9,67 +9,41 @@ function Kort1() {
     return (
         <div className="kortflate">
             <div>
-                <Heading size="medium">Hva slags hjelp kan jeg få?</Heading>
+                <Heading size="medium">Hva slags hjelp kan jeg som er under 30 få?</Heading>
                 <Detail size="small" className="blokk-xs">
-                    1 av 4
+                    1 av 5
                 </Detail>
 
                 <BodyShort className={'blokk-xs'}>
-                    NAV har gjort en vurdering av svarene dine, og det ser ut til at du har gode muligheter til å skaffe
-                    deg jobb på egenhånd.
+                    På grunn av din alder omfattes du av NAV forsterkede ungdomsinnsats.
                 </BodyShort>
 
-                <BodyShort>Vurderingen baserer seg på:</BodyShort>
-                <ul>
-                    <li>svarene fra registreringen</li>
-                    <li>opplysningene NAV har om din situasjon</li>
-                </ul>
+                <BodyShort className={'blokk-xs'}>
+                    Det betyr at du som hovedregel skal være i arbeidsrettet aktivitet innen 8 uker etter at du
+                    registrerte deg som arbeidssøker.
+                </BodyShort>
             </div>
-            <Feedback id={'Introkort14A-01-standard'} />
+            <Feedback id={'Introkort14A-01-standard-ungdomsinnsats'} />
         </div>
     );
 }
 
 function Kort2() {
-    const { servicegruppe } = useContext(OppfolgingContext).data;
-    const amplitudeData = useAmplitudeData();
-
-    const handleLesBrev = () => {
-        amplitudeLogger('veientilarbeid.intro', {
-            intro: '14a',
-            handling: 'Går til min innboks',
-            ...amplitudeData,
-        });
-    };
-
-    const tittel = servicegruppe === Servicegruppe.IKVAL ? 'Du har mottatt et brev' : 'Du vil motta et brev';
-
     return (
         <div className="kortflate">
             <div>
-                <Heading size="medium">{tittel}</Heading>
+                <Heading size="medium">
+                    I løpet av den første tiden vil du jobbe tett sammen med en veileder fra NAV.
+                </Heading>
                 <Detail size="small" className="blokk-xs">
-                    2 av 4
+                    2 av 5
                 </Detail>
-                <BodyShort className={'blokk-xs'}>
-                    {servicegruppe === Servicegruppe.IKVAL ? (
-                        <>
-                            Du har mottatt brevet{' '}
-                            <Link onClick={handleLesBrev} href={'https://mininnboks.nav.no/'}>
-                                «NAV har vurdert dine muligheter»
-                            </Link>
-                            .
-                        </>
-                    ) : (
-                        'Du vil i løpet av den første uken motta brevet «NAV har vurdert dine muligheter».'
-                    )}
-                </BodyShort>
 
-                <BodyShort className={'blokk-m'}>
-                    Dette brevet er ikke et svar på en eventuell søknad om dagpenger.
+                <BodyShort className={'blokk-xs'}>
+                    Din viktigste oppgave blir å svare på henvendelsene og huske å sende inn meldekort til rett tid.
                 </BodyShort>
             </div>
-            <Feedback id={'Introkort14A-02-standard'} />
+            <Feedback id={'Introkort14A-02-standard-ungdomsinnsats'} />
         </div>
     );
 }
@@ -91,7 +65,7 @@ function Kort3() {
             <div>
                 <Heading size="medium">Hva er en veileder?</Heading>
                 <Detail size="small" className="blokk-xs">
-                    3 av 4
+                    3 av 5
                 </Detail>
                 <BodyShort className={'blokk-xs'}>
                     En veileder sin oppgave er å besvare spørsmål, bistå rundt det å søke stillinger og tilby hjelp på
@@ -131,35 +105,74 @@ function Kort3() {
                     .
                 </BodyShort>
             </div>
-            <Feedback id={'Introkort14A-04-standard'} />
+            <Feedback id={'Introkort14A-03-standard-ungdomsinnsats'} />
         </div>
     );
 }
 
 function Kort4() {
+    const { servicegruppe } = useContext(OppfolgingContext).data;
+    const amplitudeData = useAmplitudeData();
+
+    const handleLesBrev = () => {
+        amplitudeLogger('veientilarbeid.intro', {
+            intro: '14a',
+            handling: 'Går til min innboks',
+            ...amplitudeData,
+        });
+    };
+
+    const tittel = servicegruppe === Servicegruppe.IKVAL ? 'Du har mottatt et brev' : 'Du vil motta et brev';
+
+    return (
+        <div className="kortflate">
+            <div>
+                <Heading size="medium">{tittel}</Heading>
+                <Detail size="small" className="blokk-xs">
+                    4 av 5
+                </Detail>
+                <BodyShort className={'blokk-xs'}>
+                    {servicegruppe === Servicegruppe.IKVAL ? (
+                        <>
+                            Du har mottatt brevet{' '}
+                            <Link onClick={handleLesBrev} href={'https://mininnboks.nav.no/'}>
+                                «NAV har vurdert dine muligheter»
+                            </Link>
+                            .
+                        </>
+                    ) : (
+                        'Du vil i løpet av den første uken motta brevet «NAV har vurdert dine muligheter».'
+                    )}
+                </BodyShort>
+
+                <BodyShort className={'blokk-m'}>
+                    Dette brevet er ikke et svar på en eventuell søknad om dagpenger.
+                </BodyShort>
+            </div>
+            <Feedback id={'Introkort14A-04-standard-ungdomsinnsats'} />
+        </div>
+    );
+}
+
+function Kort5() {
     return (
         <div className="kortflate">
             <div>
                 <Heading size="medium">Ta kontakt om du trenger hjelp</Heading>
                 <Detail size="small" className="blokk-xs">
-                    4 av 4
+                    5 av 5
                 </Detail>
-                <BodyShort className={'blokk-xs'}>Du kan få hjelp fra en veileder.</BodyShort>
 
                 <BodyShort className={'blokk-xs'}>
-                    Da må du selv kontakte veileder ved å bruke dialogen som vises på slutten av denne introduksjonen.
-                </BodyShort>
-
-                <BodyShort className={'blokk-m'}>
-                    Du kan gi oss beskjed om at du ønsker hjelp nå med en gang, eller se litt an hvordan du syns
-                    jobbsøkingen din går før du tar kontakt.
+                    Du kan når som helst kontakte veileder ved å bruke dialogen som vises på slutten av denne
+                    introduksjonen.
                 </BodyShort>
             </div>
-            <Feedback id={'Introkort14A-03-standard'} />
+            <Feedback id={'Introkort14A-05-standard-ungdomsinnsats'} />
         </div>
     );
 }
 
-const kortliste = [<Kort1 />, <Kort2 />, <Kort3 />, <Kort4 />];
+const kortliste = [<Kort1 />, <Kort2 />, <Kort3 />, <Kort4 />, <Kort5 />];
 
 export default kortliste;
