774eb9a63a2b6f257526516e2dfe2f44f0172f4f
Jostein Holje <jostein.holje@nav.no>
2022-01-20 13:41:56 +0100

    r Ikke exporter anonym funksjon som default
    - bygget knekker

--- src/komponenter/onboarding14a/standardinnsats/index.tsx
+++ src/komponenter/onboarding14a/standardinnsats/index.tsx
@@ -4,10 +4,12 @@ import EngelskKortliste from './en/Kortliste';
 import Sluttkort from './Sluttkort';
 import { Sprak } from '../../../contexts/sprak';
 
-export default (sprak: Sprak): [() => JSX.Element, JSX.Element[], () => JSX.Element] => {
+const lagStandardinnsatsKort = (sprak: Sprak): [() => JSX.Element, JSX.Element[], () => JSX.Element] => {
     if (sprak === 'en') {
         return [Startkort, EngelskKortliste, Sluttkort];
     }
 
     return [Startkort, Kortliste, Sluttkort];
 };
+
+export default lagStandardinnsatsKort;
