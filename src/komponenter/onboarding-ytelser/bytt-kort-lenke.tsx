7012d007071a4aedccf32ef045d7b4adbd83ae9b
Jostein Holje <jostein.holje@nav.no>
2022-01-27 13:06:22 +0100

    f Språkstøtte på bytt-kort-lenke

--- src/komponenter/onboarding-ytelser/bytt-kort-lenke.tsx
+++ src/komponenter/onboarding-ytelser/bytt-kort-lenke.tsx
@@ -2,10 +2,24 @@ import { useFeatureToggleData } from '../../contexts/feature-toggles';
 import { ReactComponent as BytteIkon } from './bytte-ikon.svg';
 import { Link } from '@navikt/ds-react';
 import './bytt-kort-lenke.less';
+import lagHentTekstForSprak, { Tekster } from '../../lib/lag-hent-tekst-for-sprak';
+import { useSprakValg } from '../../contexts/sprak';
+
+const TEKSTER: Tekster<string> = {
+    nb: {
+        aktuelt: 'Dagpenger er mest aktuelt',
+        ikkeAktuelt: 'Dagpenger er ikke aktuelt',
+    },
+    en: {
+        aktuelt: 'Unemployment benefits are most relevant',
+        ikkeAktuelt: 'Unemployment benefits are not applicable',
+    },
+};
 
 const ByttKortLenke = (props: { handleByttKortKlikk: (e: React.MouseEvent) => void; valgtYtelserVisning: string }) => {
     const featuretoggleData = useFeatureToggleData();
     const kanViseKomponent = featuretoggleData['veientilarbeid.onboardingDagpenger.toggle'];
+    const tekst = lagHentTekstForSprak(TEKSTER, useSprakValg().sprak);
 
     if (!kanViseKomponent) return null;
 
@@ -13,7 +27,7 @@ const ByttKortLenke = (props: { handleByttKortKlikk: (e: React.MouseEvent) => vo
         <div className="bytt-tema-container">
             <BytteIkon />
             <Link href="" onClick={props.handleByttKortKlikk}>
-                {props.valgtYtelserVisning === 'dagpenger' ? 'Dagpenger er ikke aktuelt' : 'Dagpenger er mest aktuelt'}
+                {props.valgtYtelserVisning === 'dagpenger' ? tekst('ikkeAktuelt') : tekst('aktuelt')}
             </Link>
         </div>
     );
