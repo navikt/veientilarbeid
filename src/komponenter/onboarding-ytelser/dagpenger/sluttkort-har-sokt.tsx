1243e46ccfbe7a8a8d7a9d2607a41fff98551fa5
Jostein Holje <jostein.holje@nav.no>
2022-01-28 10:53:10 +0100

    f Trekker ut ettersend-dokumentasjon i egen komponent med språkstøtte

--- src/komponenter/onboarding-ytelser/dagpenger/sluttkort-har-sokt.tsx
+++ src/komponenter/onboarding-ytelser/dagpenger/sluttkort-har-sokt.tsx
@@ -1,18 +1,14 @@
-import { BodyShort, Heading, Link } from '@navikt/ds-react';
-import { useAmplitudeData } from '../../../contexts/amplitude-context';
+import { Heading } from '@navikt/ds-react';
 import { useDpInnsynSoknadData } from '../../../contexts/dp-innsyn-soknad';
 import { sorterEtterNyesteDatoInnsendt } from '../../../lib/beregn-dagpenge-status';
-import { loggAktivitet } from '../../../metrics/metrics';
-import { mine_dagpenger_url } from '../../../url';
 import SistInnsendtSoknad from './sist-innsendt-soknad';
 import { usePaabegynteSoknaderData } from '../../../contexts/paabegynte-soknader';
 import PaabegynteSoknader from './paabegynte-soknader';
 import SkrivTilOssOgChat from './skriv-til-oss-og-chat';
 import LesOmYtelser from './les-om-ytelser';
+import EttersendDokumentasjon from './ettersend-dokumentasjon';
 
 const Sluttkort = () => {
-    const amplitudeData = useAmplitudeData();
-
     const soknader = useDpInnsynSoknadData();
     const sisteInnsendteSoknad = soknader?.sort(sorterEtterNyesteDatoInnsendt)[0];
     const paabegynteSoknader = usePaabegynteSoknaderData().soknader;
@@ -23,10 +19,7 @@ const Sluttkort = () => {
         sistePaabegyntSoknad &&
         new Date(sistePaabegyntSoknad.dato).getTime() > new Date(sisteInnsendteSoknad?.datoInnsendt).getTime();
 
-    function loggLenkeKlikk(action: string, url: string) {
-        loggAktivitet({ aktivitet: action, ...amplitudeData });
-        window.location.assign(url);
-    }
+    const amplitudeTemaNavn = '"dagpenger-tema - har søkt dagpenger"';
 
     return (
         <>
@@ -34,27 +27,10 @@ const Sluttkort = () => {
                 {harPaabegyntEtterInnsendt ? 'Søknad om dagpenger og påbegynte søknader' : 'Søknad om dagpenger'}
             </Heading>
             <SistInnsendtSoknad dato={sisteInnsendteSoknad?.datoInnsendt} komponent="sokt" />
-
-            <BodyShort className={'blokk-xs'}>
-                Du kan ettersende dokumentasjon og se mer informasjon via {' '}
-                <Link
-                    className={'tracking-wide'}
-                    href={mine_dagpenger_url}
-                    onClick={() =>
-                        loggLenkeKlikk(
-                            'Går til Mine dagpenger fra "dagpenger-tema - har søkt dagpenger"',
-                            mine_dagpenger_url
-                        )
-                    }
-                >
-                    Mine dagpenger
-                </Link>
-            </BodyShort>
-
+            <EttersendDokumentasjon amplitudeTemaNavn={amplitudeTemaNavn} />
             <PaabegynteSoknader dato={sisteInnsendteSoknad?.datoInnsendt} komponent="sokt" />
-
-            <SkrivTilOssOgChat amplitudeTemaNavn='"dagpenger-tema - har søkt dagpenger"' />
-            <LesOmYtelser amplitudeTemaNavn={'dagpenger-tema - har søkt dagpenger'} />
+            <SkrivTilOssOgChat amplitudeTemaNavn={amplitudeTemaNavn} />
+            <LesOmYtelser amplitudeTemaNavn={amplitudeTemaNavn} />
         </>
     );
 };
