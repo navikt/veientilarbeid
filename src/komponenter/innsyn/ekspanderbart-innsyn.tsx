cdae96831eebca3bf332d31e3f6d8d81439df7c8
Geir Gåsodden <geir.gasodden@pythonia.no>
2022-01-28 11:56:14 +0100

    f legger til språkstøtte på ekspanderbart innsyn

--- src/komponenter/innsyn/ekspanderbart-innsyn.tsx
+++ src/komponenter/innsyn/ekspanderbart-innsyn.tsx
@@ -1,14 +1,27 @@
 import { useState } from 'react';
+import { Accordion } from '@navikt/ds-react';
+
 import { useAmplitudeData } from '../../contexts/amplitude-context';
 import { useBrukerregistreringData } from '../../contexts/brukerregistrering';
 import Opplysninger from './registreringsopplysninger';
-import { Accordion } from '@navikt/ds-react';
 import { loggAktivitet } from '../../metrics/metrics';
 import useErInnloggetArbeidssoker from '../../hooks/useErInnloggetArbeidssoker';
+import lagHentTekstForSprak from '../../lib/lag-hent-tekst-for-sprak';
+import { useSprakValg } from '../../contexts/sprak';
+
+const TEKSTER = {
+    nb: {
+        header: 'Se svarene dine fra registreringen',
+    },
+    en: {
+        header: 'See your answers from the registration as job seeker',
+    },
+};
 
 const EkspanderbartInnsyn = () => {
     const [clickedInnsyn, setClickedInnsyn] = useState(false);
 
+    const tekst = lagHentTekstForSprak(TEKSTER, useSprakValg().sprak);
     const brukerregistreringData = useBrukerregistreringData();
     const amplitudeData = useAmplitudeData();
     const { opprettetDato, manueltRegistrertAv, besvarelse, teksterForBesvarelse } =
@@ -28,7 +41,7 @@ const EkspanderbartInnsyn = () => {
         <div style={{ margin: '1rem 0' }}>
             <Accordion style={{ background: 'white', borderRadius: '5px' }}>
                 <Accordion.Item>
-                    <Accordion.Header onClick={handleClickOpen}>Se svarene dine fra registreringen</Accordion.Header>
+                    <Accordion.Header onClick={handleClickOpen}>{tekst('header')}</Accordion.Header>
                     <Accordion.Content>
                         <Opplysninger
                             opprettetDato={opprettetDato}
