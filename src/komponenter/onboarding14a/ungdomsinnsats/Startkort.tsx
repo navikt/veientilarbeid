4df6337fdeaa30de008860b7267c38d29babbdbe
Geir Gåsodden <geir.gasodden@pythonia.no>
2022-02-02 07:20:38 +0100

    f innfører språkstøtte på startkortet

--- src/komponenter/onboarding14a/ungdomsinnsats/Startkort.tsx
+++ src/komponenter/onboarding14a/ungdomsinnsats/Startkort.tsx
@@ -2,13 +2,25 @@ import { Heading } from '@navikt/ds-react';
 
 import ErRendret from '../../er-rendret/er-rendret';
 import InViewport from '../../in-viewport/in-viewport';
+import lagHentTekstForSprak from '../../../lib/lag-hent-tekst-for-sprak';
+import { useSprakValg } from '../../../contexts/sprak';
+
+const TEKSTER = {
+    nb: {
+        heading: 'Hva slags hjelp kan jeg som er under 30 få?',
+    },
+    en: {
+        heading: "I'm under 30, what kind of help can I get?",
+    },
+};
 
 function Startkort() {
+    const tekst = lagHentTekstForSprak(TEKSTER, useSprakValg().sprak);
     return (
         <>
             <ErRendret loggTekst="Rendrer 14a pre-state" />
             <Heading size="large" className={'blokk-xs'}>
-                Hva slags hjelp kan jeg som er under 30 få?
+                {tekst('heading')}
             </Heading>
             <InViewport loggTekst="Viser 14a pre-state i viewport" />
         </>
