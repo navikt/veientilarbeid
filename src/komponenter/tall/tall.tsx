a9ac8beb1548bb1c539a6870fd822153062ce428
Geir Gåsodden <geir.gasodden@pythonia.no>
2022-05-12 14:15:56 +0200

    f legger til tall-komponent for tall i sirkler

---
+++ src/komponenter/tall/tall.tsx
@@ -0,0 +1,9 @@
+function TallSirkel(tall: Number) {
+    return (
+        <div className="tall-sirkel">
+            <span className="tall-innhold">{tall}</span>
+        </div>
+    );
+}
+
+export default TallSirkel;
