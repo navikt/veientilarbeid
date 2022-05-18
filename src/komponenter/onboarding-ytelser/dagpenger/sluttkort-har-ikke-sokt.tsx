55dadaf16adc26c1d6d997de7633b8a38d0feaca
Geir Gåsodden <geir.gasodden@pythonia.no>
2022-05-18 08:33:35 +0200

    r sletter tekst, link og sto/chat-komponent fra sluttkort har ikke søkt

--- src/komponenter/onboarding-ytelser/dagpenger/sluttkort-har-ikke-sokt.tsx
+++ src/komponenter/onboarding-ytelser/dagpenger/sluttkort-har-ikke-sokt.tsx
@@ -1,11 +1,9 @@
-import { BodyShort, Button, Heading, Link } from '@navikt/ds-react';
+import { Button, Heading } from '@navikt/ds-react';
 import { Next } from '@navikt/ds-icons';
 
 import { useAmplitudeData } from '../../../contexts/amplitude-context';
 import { loggAktivitet } from '../../../metrics/metrics';
 import { dagpengerSoknadLenke } from '../../../innhold/lenker';
-import { mine_dagpenger_url } from '../../../url';
-import SkrivTilOssOgChat from './skriv-til-oss-og-chat';
 import lagHentTekstForSprak, { Tekster } from '../../../lib/lag-hent-tekst-for-sprak';
 import { useSprakValg } from '../../../contexts/sprak';
 import OnboardingStandardStegnummer from '../../onboarding-standard/onboarding-standard-stegnummer';
@@ -13,28 +11,17 @@ import OnboardingStandardStegnummer from '../../onboarding-standard/onboarding-s
 const TEKSTER: Tekster<string> = {
     nb: {
         heading: 'Du har ikke sendt inn søknad om dagpenger',
-        ingress: 'Du kan tidligst få dagpenger fra den dagen du sender inn søknaden.',
         sok: 'Søk om dagpenger',
-        feil: 'Mener du dette er feil, kan du sjekke',
-        mineDagpenger: 'Mine dagpenger',
     },
     en: {
         heading: 'You have not submitted an application for unemployment benefits',
-        ingress: 'You can receive unemployment benefits at the earliest from the day you submit the application.',
         sok: 'Apply for unemployment benefits',
-        feil: 'If you think this is wrong, you can check',
-        mineDagpenger: 'My unemployment benefits',
     },
 };
 
 const Sluttkort = () => {
     const amplitudeData = useAmplitudeData();
     const tekst = lagHentTekstForSprak(TEKSTER, useSprakValg().sprak);
-
-    function loggLenkeKlikk(action: string, url: string) {
-        loggAktivitet({ aktivitet: action, ...amplitudeData });
-        window.location.assign(url);
-    }
 
     const handleButtonClick = () => {
         loggAktivitet({
@@ -51,30 +38,10 @@ const Sluttkort = () => {
                 {tekst('heading')}
             </Heading>
 
-            <BodyShort className={'blokk-xs'}>{tekst('ingress')}</BodyShort>
-
             <Button variant="secondary" onClick={handleButtonClick} className="blokk-xs">
                 {tekst('sok')}
                 <Next />
             </Button>
-
-            <BodyShort className={'blokk-xs'}>
-                {`${tekst('feil')} `}
-                <Link
-                    className={'tracking-wide'}
-                    href={mine_dagpenger_url}
-                    onClick={() =>
-                        loggLenkeKlikk(
-                            'Går til Mine dagpenger fra "dagpenger-tema - ikke søkt dagpenger"',
-                            mine_dagpenger_url
-                        )
-                    }
-                >
-                    {tekst('mineDagpenger')}
-                </Link>
-            </BodyShort>
-
-            <SkrivTilOssOgChat amplitudeTemaNavn='"dagpenger-tema - ikke søkt dagpenger"' />
         </>
     );
 };
