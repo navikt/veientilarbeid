a21099a244551e691be5a4992e78c08ef1976211
Jostein Holje <jostein.holje@nav.no>
2022-06-09 12:43:55 +0200

    f Enkel modal ui for <ArbeidsledigDato />

--- src/komponenter/arbeidsledig-dato/ArbeidsledigDato.tsx
+++ src/komponenter/arbeidsledig-dato/ArbeidsledigDato.tsx
@@ -1,17 +1,50 @@
-import { hentQueryParam } from '../../utils/query-param-utils';
+import { fjernQueryParam, hentQueryParam } from '../../utils/query-param-utils';
 import { DemoData } from '../../demo/demo-state';
+import { BodyShort, Button, Heading, Link, Modal } from '@navikt/ds-react';
+import { useCallback, useState } from 'react';
 
 function ArbeidsledigDato(): JSX.Element | null {
     const visKomponent = hentQueryParam(DemoData.VIS_ARBEIDSLEDIG_DATO) === 'true';
+    const [visModal, settVisModal] = useState<boolean>(visKomponent);
+
+    const onClose = useCallback(() => {
+        fjernQueryParam(DemoData.VIS_ARBEIDSLEDIG_DATO);
+        settVisModal(false);
+    }, [settVisModal]);
 
     if (!visKomponent) {
         return null;
     }
 
     return (
-        <div style={{ backgroundColor: 'hotpink', padding: '1em' }}>
-            <h1>Når mistet du eller kommer du til å miste jobben?</h1>
-        </div>
+        <Modal open={visModal} onClose={onClose} shouldCloseOnOverlayClick={false}>
+            <Modal.Content>
+                <Heading spacing={true} size={'medium'} style={{ marginRight: '2em' }}>
+                    Når mistet du eller kommer du til å miste jobben?
+                </Heading>
+                <BodyShort spacing={true}>
+                    <input
+                        type="date"
+                        className="navds-text-field__input"
+                        onChange={(e) => console.log('onChange', e.target.value)}
+                    />
+                </BodyShort>
+                <BodyShort spacing={true}>
+                    <Link
+                        href={'#'}
+                        onClick={(e) => {
+                            e.preventDefault();
+                            onClose();
+                        }}
+                    >
+                        Ønsker ikke å oppgi/vet ikke
+                    </Link>
+                </BodyShort>
+                <Button variant={'secondary'} onClick={onClose}>
+                    Lukk
+                </Button>
+            </Modal.Content>
+        </Modal>
     );
 }
 
