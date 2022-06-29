651b58b2a6702b201c6fb5049b2acf07f6fb7d5b
Geir Gåsodden <geir.gasodden@pythonia.no>
2022-06-29 10:42:02 +0200

    r legger til egen innhold-view for andre enn standard

---
+++ src/innhold/innhold-andre.tsx
@@ -0,0 +1,36 @@
+import './innhold.css';
+import Rad from './rad';
+import ReaktiveringKort from '../komponenter/reaktivering/reaktivering-kort';
+import Egenvurdering from '../komponenter/egenvurdering/egenvurdering';
+import Motestotte from '../komponenter/motestotte/motestotte';
+import KrrMelding from '../komponenter/krr-melding/krr-melding';
+import Registrert from '../komponenter/registrert/registrert';
+import AktivitetDialogMeldekort from './aktivitet-dialog-meldekort';
+import InnholdMetrics from './innhold-metrics';
+import InViewport from '../komponenter/in-viewport/in-viewport';
+import StatusTittel from '../komponenter/registrert/status-tittel';
+import EkspanderbartInnsyn from '../komponenter/innsyn/ekspanderbart-innsyn';
+import ReaktiveringKvittering from '../komponenter/reaktivering/reaktivering-kvittering';
+
+const InnholdView = () => {
+    return (
+        <>
+            <InnholdMetrics />
+            <InViewport loggTekst="Veien til arbeid i viewport" />
+            <Rad>
+                <ReaktiveringKort />
+                <KrrMelding />
+                <StatusTittel />
+                <ReaktiveringKvittering />
+                <Registrert />
+                <EkspanderbartInnsyn />
+                <Egenvurdering />
+                <Motestotte />
+                <AktivitetDialogMeldekort />
+            </Rad>
+            <InViewport loggTekst="Bunnen av veien til arbeid - andre i viewport" />
+        </>
+    );
+};
+
+export default InnholdView;
