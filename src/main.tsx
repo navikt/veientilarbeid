ff4607f426c8f6a00214d546166727b9a8037ea7
sasoria <sergio.alberto.arevalo.soria@nav.no>
2022-06-29 13:40:40 +0200

    Bygger appen til esm slik at den kan importeres fra min-side

---
+++ src/main.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import DataProvider from './komponenter/hent-initial-data/data-provider';
+import OppfolgingBrukerregistreringProvider from './komponenter/hent-initial-data/oppfolging-brukerregistrering-provider';
+import Innholdslaster from './komponenter/innholdslaster/innholdslaster';
+import * as Autentisering from './contexts/autentisering';
+import Feilmelding from './komponenter/feilmeldinger/feilmelding';
+import { fetchData } from './ducks/api-utils';
+import { AUTH_API } from './ducks/api';
+import App from './app';
+import './index.css';
+
+const Mikrofrontend = () => {
+    const [state, setState] = React.useState<Autentisering.State>(Autentisering.initialState);
+
+    React.useEffect(() => {
+        fetchData<Autentisering.State, Autentisering.Data>(state, setState, AUTH_API);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    return (
+        <Innholdslaster feilmeldingKomponent={<Feilmelding />} storrelse="XXL" avhengigheter={[state]}>
+            <Autentisering.AutentiseringContext.Provider value={state}>
+                <OppfolgingBrukerregistreringProvider>
+                    <DataProvider>
+                        <App />
+                    </DataProvider>
+                </OppfolgingBrukerregistreringProvider>
+            </Autentisering.AutentiseringContext.Provider>
+        </Innholdslaster>
+    );
+};
+
+export default Mikrofrontend;
