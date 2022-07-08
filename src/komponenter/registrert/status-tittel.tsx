4545f83d8b64270bb37aaf7f8cf2a64e2390c052
Jostein Holje <jostein.holje@nav.no>
2022-07-08 13:31:53 +0200

    f Marker ny-registrert i statusTittel

--- src/komponenter/registrert/status-tittel.tsx
+++ src/komponenter/registrert/status-tittel.tsx
@@ -1,16 +1,19 @@
-import { Heading } from '@navikt/ds-react';
+import { Heading, Panel } from '@navikt/ds-react';
 
 import useErInnloggetArbeidssoker from '../../hooks/useErInnloggetArbeidssoker';
 import lagHentTekstForSprak from '../../lib/lag-hent-tekst-for-sprak';
 import { useSprakValg } from '../../contexts/sprak';
-import { createRef, useCallback, useEffect } from 'react';
+import { createRef, useCallback, useEffect, useState } from 'react';
+import { Success, SuccessColored } from '@navikt/ds-icons';
 
 const TEKSTER = {
     nb: {
         registrert: 'Du er registrert som arbeidssøker',
+        registrertNy: 'Du er nå registrert som arbeidssøker',
     },
     en: {
         registrert: 'You are registered as job seeker',
+        registrertNy: 'You are now registered as job seeker',
     },
 };
 
@@ -18,11 +21,13 @@ const StatusTittel = () => {
     const kanViseKomponent = useErInnloggetArbeidssoker();
     const tekst = lagHentTekstForSprak(TEKSTER, useSprakValg().sprak);
     const containerRef = createRef<HTMLDivElement>();
+    const [erNyRegistrert, settErNyRegistrert] = useState<boolean>(false);
 
     const scrollToRegistrering = useCallback(() => {
         const goto = new URLSearchParams(window.location.search).get('goTo');
         if (goto === 'registrering' && containerRef.current) {
             containerRef.current.scrollIntoView();
+            settErNyRegistrert(true);
         }
     }, [containerRef]);
 
@@ -36,9 +41,14 @@ const StatusTittel = () => {
 
     return (
         <div ref={containerRef}>
-            <Heading style={{ margin: 'var(--navds-font-size-medium) 0' }} size="medium">
-                {tekst('registrert')}
-            </Heading>
+            <Panel className="blokk-xs">
+                <Heading size="medium">
+                    <span style={{ marginRight: '0.5em', position: 'relative', top: '4px' }}>
+                        {erNyRegistrert ? <SuccessColored /> : <Success />}
+                    </span>
+                    {tekst(erNyRegistrert ? 'registrertNy' : 'registrert')}
+                </Heading>
+            </Panel>
         </div>
     );
 };
