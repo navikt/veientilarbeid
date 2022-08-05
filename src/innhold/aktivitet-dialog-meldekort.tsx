541554aeafd83de225c9e4dc3d120558ed43ea97
Ane Halse Sporastøyl <47738086+anehs@users.noreply.github.com>
2022-08-05 12:11:51 +0200

    r Gi aktivitetsplanboksen full bredde
    Denne var tidligere i en grid sammen med ditt sykefravær/meldekort, og fikk alltid
    50% bredde uansett om sykefravær/meldekortboksen var der eller ikke (det er ikke alltid denne skal vises).
    Endrer sånn at begge boksene får full bredde og havner under hverandre.

--- src/innhold/aktivitet-dialog-meldekort.tsx
+++ src/innhold/aktivitet-dialog-meldekort.tsx
@@ -2,19 +2,14 @@ import { useBrukerinfoData } from '../contexts/bruker-info';
 import Aktivitetsplan from '../komponenter/aktivitetsplan/aktivitetsplan';
 import DittSykefravaer from '../komponenter/ditt-sykefravaer/ditt-sykefravaer';
 import Meldekort from '../komponenter/meldekort/meldekort';
-import { Cell, Grid } from '@navikt/ds-react';
 
 const AktivitetDialog = () => {
     const { erSykmeldtMedArbeidsgiver } = useBrukerinfoData();
     return (
-        <Grid className="blokk-xs">
-            <Cell xs={12} md={6}>
-                <Aktivitetsplan />
-            </Cell>
-            <Cell xs={12} md={6}>
-                {erSykmeldtMedArbeidsgiver ? <DittSykefravaer /> : <Meldekort />}
-            </Cell>
-        </Grid>
+        <>
+            <Aktivitetsplan />
+            {erSykmeldtMedArbeidsgiver ? <DittSykefravaer /> : <Meldekort />}
+        </>
     );
 };
 
