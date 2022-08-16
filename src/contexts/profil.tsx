9364e51661b0451380cd6542b5ed67f11e287a4c
Ane Halse Sporastøyl <47738086+anehs@users.noreply.github.com>
2022-08-16 12:08:54 +0200

    f Begynn på profil-context. Profilen skal brukes til å lagre brukervalg
    Denne vil bli brukt til å lagre om bruker har gjennomført egenvurdering, gitt feedback osv., sånn at det alltid vises riktig neste gang brukeren besøker siden.

---
+++ src/contexts/profil.tsx
@@ -0,0 +1,64 @@
+import { createContext, ReactNode, useContext, useEffect, useState } from 'react';
+import { fetchToJson } from '../ducks/api-utils';
+import { PROFIL_URL, requestConfig } from '../ducks/api';
+
+interface ProfilProviderType {
+    profil: string | null;
+    lagreProfil: (profil: string | null) => Promise<void>;
+}
+
+const ProfilContext = createContext<ProfilProviderType>({
+    profil: null,
+    lagreProfil: () => Promise.resolve(),
+});
+
+function ProfilProvider(props: { children: ReactNode }) {
+    const [profil, settProfil] = useState<string | null>(null);
+
+    const hentProfil = async () => {
+        try {
+            const { profil } = await fetchToJson(PROFIL_URL, requestConfig());
+            settProfil(profil);
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
+    const lagreProfil = async (profil: string | null) => {
+        try {
+            await fetchToJson(PROFIL_URL, {
+                ...requestConfig(),
+                method: 'POST',
+                body: JSON.stringify({ profil }),
+            });
+        } catch (error) {
+            console.error(error);
+            throw error;
+        } finally {
+            await hentProfil();
+        }
+    };
+
+    useEffect(() => {
+        hentProfil();
+    }, []);
+
+    const contextValue = {
+        profil,
+        lagreProfil,
+    };
+
+    return <ProfilContext.Provider value={contextValue}>{props.children}</ProfilContext.Provider>;
+}
+
+function useProfil() {
+    const context = useContext(ProfilContext);
+
+    if (context === undefined) {
+        throw new Error('useProfil må brukes under en ProfilProvider');
+    }
+
+    return context;
+}
+
+export { ProfilProvider, useProfil };
