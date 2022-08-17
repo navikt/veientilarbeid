952fca7a73a764e0736fa47b2ae6d25f9d9da3a8
Geir Gåsodden <geir.gasodden@pythonia.no>
2022-08-17 12:54:52 +0200

    b fikser typo i Content-type

--- src/ducks/api-utils.ts
+++ src/ducks/api-utils.ts
@@ -26,7 +26,7 @@ function sjekkStatuskode(response: Response): Response {
 function toJson(response: Response) {
     if (
         response.status === 204 ||
-        (response.status === 201 && response.headers.get('Content-Type') !== 'application/jspn')
+        (response.status === 201 && response.headers.get('Content-Type') !== 'application/json')
     ) {
         // 204 No content
         // 201 Created med tom respons tilbake
