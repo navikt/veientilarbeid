1b037df77d740904bd732e8a37aeed69996b672c
Geir Gåsodden <geir.gasodden@pythonia.no>
2022-08-18 10:51:48 +0200

    f legger til ny feedback komponent

--- src/komponenter/meldekort/meldekort-forklaring.tsx
+++ src/komponenter/meldekort/meldekort-forklaring.tsx
@@ -1,7 +1,9 @@
-import lagHentTekstForSprak from '../../lib/lag-hent-tekst-for-sprak';
-import { useSprakValg } from '../../contexts/sprak';
 import { BodyLong, Heading } from '@navikt/ds-react';
-import Feedback from '../feedback/feedback-legacy';
+
+import { useSprakValg } from '../../contexts/sprak';
+
+import lagHentTekstForSprak from '../../lib/lag-hent-tekst-for-sprak';
+import Feedback from '../feedback/feedback';
 
 const TEKSTER = {
     nb: {
