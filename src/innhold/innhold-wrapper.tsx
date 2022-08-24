336e54e1dd8ca21b941eb727fc2c09684440d3b8
Ane Halse Sporastøyl <47738086+anehs@users.noreply.github.com>
2022-08-24 12:42:20 +0200

    f Alltid splitt standard/ikke standard. Fjern sjekk av featuretoggle
    Sjekker ikke lenger featuretoggle for om vi skal splitte opp i egne visninger for standard og ikke standard. 
    Vi har gått permanent over til dette nå. 
    
    Slettet innhold-view siden denne inneholder den utdaterte felles-visningen som vi ikke skal bruke lenger.
    La innholdet i innhold-velger inn i innhold-wrapper

--- src/innhold/innhold-wrapper.tsx
+++ src/innhold/innhold-wrapper.tsx
@@ -1,24 +1,31 @@
-import { useFeatureToggleData } from '../contexts/feature-toggles';
-import InnholdView from './innhold-view';
-import InnholdVelger from './innhold-velger';
+import { useContext } from 'react';
+
 import GenerelleFliser from '../komponenter/dittnav/generelle-fliser';
+import { useBrukerregistreringData } from '../contexts/brukerregistrering';
+import { useOppfolgingData } from '../contexts/oppfolging';
+import sjekkOmBrukerErStandardInnsatsgruppe from '../lib/er-standard-innsatsgruppe';
+import InnholdStandard from './innhold-standard';
+import InnholdIkkeStandard from './innhold-ikke-standard';
+import { UnderOppfolgingContext } from '../contexts/under-oppfolging';
+
+function InnholdVelger() {
+    const oppfolgingData = useOppfolgingData();
+    const registreringData = useBrukerregistreringData();
+    const { underOppfolging } = useContext(UnderOppfolgingContext).data;
+    const brukerregistreringData = registreringData?.registrering ?? null;
+    const erStandardInnsatsgruppe = sjekkOmBrukerErStandardInnsatsgruppe({
+        brukerregistreringData,
+        oppfolgingData,
+    });
+
+    if (!underOppfolging) return null;
+    return erStandardInnsatsgruppe ? <InnholdStandard /> : <InnholdIkkeStandard />;
+}
 
 function InnholdWrapper() {
-    const featureToggles = useFeatureToggleData();
-    const skalBrukeSplitt = featureToggles['veientilarbeid.splitt-standard-visning'];
-
-    if (skalBrukeSplitt) {
-        return (
-            <>
-                <InnholdVelger />
-                <GenerelleFliser />
-            </>
-        );
-    }
-
     return (
         <>
-            <InnholdView />
+            <InnholdVelger />
             <GenerelleFliser />
         </>
     );
