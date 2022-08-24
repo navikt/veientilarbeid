d73e59c91331fe24f25a7d4672a32d098949febc
Ane Halse Sporastøyl <47738086+anehs@users.noreply.github.com>
2022-08-24 15:05:35 +0200

    c Endre url fra dittnav til minside

--- src/utils/app-state-utils.ts
+++ src/utils/app-state-utils.ts
@@ -10,7 +10,7 @@ export const erMock = () => process.env.REACT_APP_MOCK;
 const url = window && window.location && window.location.href ? window.location.href : '';
 
 export const erProduksjon = () => {
-    return url.indexOf('www.nav.no/person/dittnav') > -1;
+    return url.indexOf('www.nav.no/minside') > -1;
 };
 
 export const erStandalone = () => /[&?]standalone=true/.test(url);
