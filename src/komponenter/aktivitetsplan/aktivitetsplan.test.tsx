4aad47f76ac3482144a76aa07bdc407eaafbf291
Geir Gåsodden <geir.gasodden@pythonia.no>
2022-08-26 07:58:43 +0200

    r legger til test for aktivitetsplanen

---
+++ src/komponenter/aktivitetsplan/aktivitetsplan.test.tsx
@@ -0,0 +1,27 @@
+import '@testing-library/jest-dom/extend-expect';
+import { render, screen } from '@testing-library/react';
+
+import Aktivitetsplan from './aktivitetsplan';
+import { contextProviders, ProviderProps } from '../../test/test-context-providers';
+
+describe('Tester komponenten Aktivitetsplan', () => {
+    test('Komponenten VISES om featuretoggle er aktiv', async () => {
+        const props: ProviderProps = {
+            featureToggle: {
+                'veientilarbeid.ny-standardvisning': true,
+            },
+        };
+        render(<Aktivitetsplan />, { wrapper: contextProviders(props) });
+        expect(screen.getByText(/aktivitetsplanen din/i)).toBeTruthy();
+    });
+
+    test('Komponenten vises IKKE featuretoggle IKKE er aktiv', async () => {
+        const props: ProviderProps = {
+            featureToggle: {
+                'veientilarbeid.ny-standardvisning': false,
+            },
+        };
+        const { container } = render(<Aktivitetsplan />, { wrapper: contextProviders(props) });
+        expect(container).toBeEmptyDOMElement();
+    });
+});
