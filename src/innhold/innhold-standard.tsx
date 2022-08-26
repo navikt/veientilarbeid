60971b57c15612c2f6ba756365d45a6f0b310441
Geir Gåsodden <geir.gasodden@pythonia.no>
2022-08-26 10:06:49 +0200

    r fjerner ubrukte komponenter fra innhold-standard

--- src/innhold/innhold-standard.tsx
+++ src/innhold/innhold-standard.tsx
@@ -1,11 +1,9 @@
 import './innhold.css';
 import KrrMelding from '../komponenter/krr-melding/krr-melding';
-import Registrert from '../komponenter/registrert/registrert';
 import InnholdMetrics from './innhold-metrics';
 import InViewport from '../komponenter/in-viewport/in-viewport';
 import StatusTittel from '../komponenter/registrert/status-tittel';
 import ReaktiveringKvittering from '../komponenter/reaktivering/reaktivering-kvittering';
-import OnboardingStandard from '../komponenter/onboarding-standard/onboarding-standard';
 import GjelderFraDato from '../komponenter/gjelder-fra-dato/GjelderFraDato';
 import DagpengerOgYtelser from '../komponenter/dagpenger/dagpenger-og-ytelser';
 import Meldekort from '../komponenter/meldekort/meldekort';
@@ -30,8 +28,6 @@ const InnholdStandard = () => {
                 <HjelpOgStotte />
                 <Aktivitetsplan />
                 <GjelderFraDato />
-                <OnboardingStandard />
-                <Registrert />
             </div>
             <InViewport loggTekst="Bunnen av veien til arbeid i viewport" />
         </>
