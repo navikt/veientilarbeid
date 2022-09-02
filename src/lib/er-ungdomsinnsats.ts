646f7a4c510f0bdfb0a44063a4482d16ce55f756
Ane Halse Sporastøyl <47738086+anehs@users.noreply.github.com>
2022-09-02 13:51:31 +0200

    r Fjern flere ubrukte lib-filer

--- src/lib/er-ungdomsinnsats.ts
+++
@@ -1,9 +0,0 @@
-import * as BrukerInfo from '../contexts/bruker-info';
-
-function sjekkOmBrukerErUngdomsinnsats({ brukerInfoData }: { brukerInfoData: BrukerInfo.Data }) {
-    const { alder } = brukerInfoData;
-
-    return alder < 30;
-}
-
-export default sjekkOmBrukerErUngdomsinnsats;
