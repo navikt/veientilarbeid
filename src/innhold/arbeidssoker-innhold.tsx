f34db591e774dcb5f32ca871833a12b19ffff8dc
Jostein Holje <jostein.holje@nav.no>
2022-09-12 12:14:04 +0200

    f Bruk swr til å hente om bruker er standard

--- src/innhold/arbeidssoker-innhold.tsx
+++ src/innhold/arbeidssoker-innhold.tsx
@@ -1,19 +1,19 @@
 import { useUnderOppfolging } from '../contexts/arbeidssoker';
 import { InnloggingsNiva, useAutentiseringData } from '../contexts/autentisering';
-import React from 'react';
 import InnholdStandard from './innhold-standard';
 import InnholdIkkeStandard from './innhold-ikke-standard';
+import useSwr from 'swr';
+import { ER_STANDARD_INNSATSGRUPPE_URL } from '../ducks/api';
 
 function ArbeidssokerInnhold() {
     const underOppfolging = useUnderOppfolging()?.underoppfolging;
     const innloggingsnivaa = useAutentiseringData().securityLevel;
-    const [erStandard, setErStandard] = React.useState(false);
-    //TODO: Erstatte med erStandard-kall:
-    setErStandard(false);
+    const { data: erStandard } = useSwr(ER_STANDARD_INNSATSGRUPPE_URL);
 
     if (underOppfolging && innloggingsnivaa === InnloggingsNiva.LEVEL_4 && erStandard) {
         return <InnholdStandard />;
     }
+
     return <InnholdIkkeStandard />;
 }
 
