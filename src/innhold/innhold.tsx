a8f75b09e9ab7911c3bbd7896fe5a7a084878fb5
Jostein Holje <jostein.holje@nav.no>
2022-09-13 10:22:08 +0200

    f <ArbeidssokerDataProvider>
    - populer provider data for arbeidssoker views
    - hent feature toggle i main

--- src/innhold/innhold.tsx
+++ src/innhold/innhold.tsx
@@ -2,12 +2,20 @@ import { useArbeidssokerPerioder } from '../contexts/arbeidssoker';
 import beregnArbeidssokerperioder from '../lib/beregn-arbeidssokerperioder';
 import ArbeidssokerInnhold from './arbeidssoker-innhold';
 import IkkeArbeidssokerInnhold from './ikke-arbeidssoker-innhold';
+import ArbeidssokerDataProvider from './arbeidssoker-data-provider';
 
 function Innhold() {
     const arbeidssokerperioderData = useArbeidssokerPerioder();
     const arbeidssokerperioder = beregnArbeidssokerperioder(arbeidssokerperioderData);
     const harAktivArbeidssokerperiode = arbeidssokerperioder.harAktivArbeidssokerperiode === 'Ja';
-    return harAktivArbeidssokerperiode ? <ArbeidssokerInnhold /> : <IkkeArbeidssokerInnhold />;
+
+    return harAktivArbeidssokerperiode ? (
+        <ArbeidssokerDataProvider>
+            <ArbeidssokerInnhold />
+        </ArbeidssokerDataProvider>
+    ) : (
+        <IkkeArbeidssokerInnhold />
+    );
 }
 
 export default Innhold;
--- src/main.tsx
+++ src/main.tsx
@@ -4,35 +4,49 @@ import Innholdslaster from './komponenter/innholdslaster/innholdslaster';
 import * as Autentisering from './contexts/autentisering';
 import Feilmelding from './komponenter/feilmeldinger/feilmelding';
 import { fetchData } from './ducks/api-utils';
-import { ARBEIDSSOKER_NIVA3_URL, AUTH_API } from './ducks/api';
+import { ARBEIDSSOKER_NIVA3_URL, AUTH_API, FEATURE_URL } from './ducks/api';
 import Innhold from './innhold/innhold';
 
 import './index.css';
 import * as Arbeidssoker from './contexts/arbeidssoker';
+import * as FeatureToggle from './contexts/feature-toggles';
 
 const Mikrofrontend = () => {
     const [state, setState] = React.useState<Autentisering.State>(Autentisering.initialState);
     const [arbeidssokerState, setArbeidssokerState] = React.useState<Arbeidssoker.State>(Arbeidssoker.initialState);
+    const [featureToggleState, setFeatureToggleState] = React.useState<FeatureToggle.State>(FeatureToggle.initialState);
+
+    const parameters = Object.values(FeatureToggle.FeatureToggles)
+        .map((element) => 'feature=' + element)
+        .join('&');
+    const featureTogglesUrl = `${FEATURE_URL}?${parameters}`;
 
     React.useEffect(() => {
         fetchData<Autentisering.State, Autentisering.Data>(state, setState, AUTH_API);
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
-
-    React.useEffect(() => {
         fetchData<Arbeidssoker.State, Arbeidssoker.Data>(
             arbeidssokerState,
             setArbeidssokerState,
             ARBEIDSSOKER_NIVA3_URL
         );
+        fetchData<FeatureToggle.State, FeatureToggle.Data>(
+            featureToggleState,
+            setFeatureToggleState,
+            featureTogglesUrl
+        );
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     return (
-        <Innholdslaster feilmeldingKomponent={<Feilmelding />} storrelse="XXL" avhengigheter={[state]}>
+        <Innholdslaster
+            feilmeldingKomponent={<Feilmelding />}
+            storrelse="XXL"
+            avhengigheter={[state, arbeidssokerState, featureToggleState]}
+        >
             <Autentisering.AutentiseringContext.Provider value={state}>
                 <Arbeidssoker.ArbeidssokerContext.Provider value={arbeidssokerState}>
-                    <Innhold />
+                    <FeatureToggle.FeaturetoggleContext.Provider value={featureToggleState}>
+                        <Innhold />
+                    </FeatureToggle.FeaturetoggleContext.Provider>
                 </Arbeidssoker.ArbeidssokerContext.Provider>
             </Autentisering.AutentiseringContext.Provider>
         </Innholdslaster>
