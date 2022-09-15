b47833eb7d392670631cb4f130c55c13483a3785
Jostein Holje <jostein.holje@nav.no>
2022-09-15 10:45:15 +0200

    b Ikke inkluder <Reaktivering> for arbeidssøkere

--- src/innhold/ikke-arbeidssoker-innhold.tsx
+++ src/innhold/ikke-arbeidssoker-innhold.tsx
@@ -1,14 +1,14 @@
 import { useArbeidssokerPerioder } from '../contexts/arbeidssoker';
 import beregnArbeidssokerperioder from '../lib/beregn-arbeidssokerperioder';
 import IkkeRegistrert from '../komponenter/ikke-registrert/ikke-registrert';
-import Reaktivering from '../komponenter/reaktivering/reaktivering';
+import ReaktiveringWrapper from '../komponenter/reaktivering/reaktivering-wrapper';
 
 function IkkeArbeidssokerInnhold() {
     const arbeidssokerperioderData = useArbeidssokerPerioder();
     const arbeidssokerperioder = beregnArbeidssokerperioder(arbeidssokerperioderData);
 
     if (arbeidssokerperioder.antallDagerSidenSisteArbeidssokerperiode < 28) {
-        return <Reaktivering />;
+        return <ReaktiveringWrapper />;
     }
 
     return <IkkeRegistrert />;
