02a4bc9ffa3e0624019aff1acb981ff062b3122a
Jostein Holje <jostein.holje@nav.no>
2022-10-17 14:37:42 +0200

    Bruk klarer seg selv feature toggle

--- src/contexts/feature-toggles.ts
+++ src/contexts/feature-toggles.ts
@@ -4,6 +4,7 @@ import { DataElement, STATUS } from '../ducks/api';
 export enum FeatureToggles {
     EGENVURDERING_UKE12 = 'veientilarbeid.egenvurderinguke12',
     VIS_GJELDER_FRA_DATO = 'veientilarbeid.vis-gjelder-fra-dato',
+    BRUK_KLARER_SEG_SELV = 'veientilarbeid.bruk-klarer-seg-selv',
 }
 
 export function prettyPrintFeatureToggle(toggle: FeatureToggles) {
@@ -12,12 +13,15 @@ export function prettyPrintFeatureToggle(toggle: FeatureToggles) {
             return 'Egenvurdering uke 12';
         case FeatureToggles.VIS_GJELDER_FRA_DATO:
             return 'Gjelder fra dato';
+        case FeatureToggles.BRUK_KLARER_SEG_SELV:
+            return 'Bruk klarer seg selv';
     }
 }
 
 export interface Data {
     'veientilarbeid.egenvurderinguke12': boolean;
     'veientilarbeid.vis-gjelder-fra-dato'?: boolean;
+    'veientilarbeid.bruk-klarer-seg-selv'?: boolean;
 }
 
 export interface State extends DataElement {
@@ -28,6 +32,7 @@ export const initialState: State = {
     data: {
         'veientilarbeid.egenvurderinguke12': false,
         'veientilarbeid.vis-gjelder-fra-dato': false,
+        'veientilarbeid.bruk-klarer-seg-selv': false,
     },
     status: STATUS.NOT_STARTED,
 };
