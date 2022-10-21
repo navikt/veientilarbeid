8a30038c078e937f900993d67e78c4cf9230f514
Geir Gåsodden <geir.gasodden@gmail.com>
2022-10-21 14:18:07 +0200

    f legger til basen for komponenten behovsavklaring

---
+++ src/komponenter/behovsavklaring-oppfolging/behovsavklaring-oppfolging.tsx
@@ -0,0 +1,52 @@
+import { Dialog } from '@navikt/ds-icons';
+import { Detail, Panel } from '@navikt/ds-react';
+
+import { useSprakValg } from '../../contexts/sprak';
+
+import lagHentTekstForSprak from '../../lib/lag-hent-tekst-for-sprak';
+import ErRendret from '../er-rendret/er-rendret';
+import InViewport from '../in-viewport/in-viewport';
+
+import spacingStyles from '../../spacing.module.css';
+import flexStyles from '../../flex.module.css';
+
+const TEKSTER = {
+    nb: {
+        overskrift: 'Hjelp og støtte',
+        heading: 'Om du ønsker hjelp fra oss må du gi beskjed',
+        readMoreHeading: 'Hva slags hjelp kan jeg få?',
+    },
+    en: {
+        heading: 'Get in touch if you need help',
+        readMoreHeading: 'What kind of help can I get?',
+    },
+};
+
+function Behovsavklaring() {
+    const sprak = useSprakValg().sprak;
+    const tekst = lagHentTekstForSprak(TEKSTER, sprak);
+
+    return (
+        <Panel className={`${flexStyles.flex} ${spacingStyles.px1_5}`}>
+            <ErRendret loggTekst="Rendrer behovsavklaring komponent" />
+            <span
+                style={{
+                    marginRight: '0.5em',
+                    position: 'relative',
+                    top: '6px',
+                    fontSize: 'var(--navds-font-size-heading-medium)',
+                }}
+            >
+                <Dialog />
+            </span>
+            <div className={spacingStyles.fullWidth}>
+                <Detail uppercase style={{ marginTop: '-1rem' }}>
+                    {tekst('overskrift')}
+                </Detail>
+            </div>
+            <InViewport loggTekst="Viser behovsavklaring viewport" />
+        </Panel>
+    );
+}
+
+export default Behovsavklaring;
