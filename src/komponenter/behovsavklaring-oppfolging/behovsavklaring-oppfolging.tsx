233391456712deee235c3a7524d0a90225131777
Geir Gåsodden <geir.gasodden@gmail.com>
2022-10-24 12:17:17 +0200

    r splitter svart og ikke svart i egne komponenter på behovsavklaring

--- src/komponenter/behovsavklaring-oppfolging/behovsavklaring-oppfolging.tsx
+++ src/komponenter/behovsavklaring-oppfolging/behovsavklaring-oppfolging.tsx
@@ -1,68 +1,12 @@
-import { Dialog } from '@navikt/ds-icons';
-import { BodyLong, BodyShort, Button, Detail, Heading, Panel } from '@navikt/ds-react';
+import { useBehovForVeiledning } from '../../contexts/behov-for-veiledning';
 
-import { useSprakValg } from '../../contexts/sprak';
-
-import lagHentTekstForSprak from '../../lib/lag-hent-tekst-for-sprak';
-import ErRendret from '../er-rendret/er-rendret';
-import InViewport from '../in-viewport/in-viewport';
-
-import spacingStyles from '../../spacing.module.css';
-import flexStyles from '../../flex.module.css';
-
-const TEKSTER = {
-    nb: {
-        overskrift: 'Hjelp og støtte',
-        heading: 'Hva slags veiledning ønsker du?',
-        beskrivelse:
-            'Vi tror du har gode muligheter til å søke og skaffe seg jobb på egenhånd - uten hjelp fra veileder.',
-        hvaTenkerDu: 'Hva tenker du?',
-        klareDegSelv: 'Ønsker du å klare deg selv?',
-        readMoreHeading: 'Hva slags hjelp kan jeg få?',
-    },
-    en: {
-        heading: 'Get in touch if you need help',
-        readMoreHeading: 'What kind of help can I get?',
-    },
-};
+import IkkeSvartPaaBehovsavklaring from './behovsavklaring-ikke-svart';
+import SvartPaaBehovsavklaring from './behovsavklaring-svart';
 
 function Behovsavklaring() {
-    const sprak = useSprakValg().sprak;
-    const tekst = lagHentTekstForSprak(TEKSTER, sprak);
+    const { behovForVeiledning } = useBehovForVeiledning();
 
-    return (
-        <Panel className={`${flexStyles.flex} ${spacingStyles.px1_5}`}>
-            <ErRendret loggTekst="Rendrer behovsavklaringkomponent" />
-            <span
-                style={{
-                    marginRight: '0.5em',
-                    position: 'relative',
-                    top: '6px',
-                    fontSize: 'var(--navds-font-size-heading-medium)',
-                }}
-            >
-                <Dialog />
-            </span>
-            <div className={spacingStyles.fullWidth}>
-                <Detail uppercase style={{ marginTop: '-1rem' }}>
-                    {tekst('overskrift')}
-                </Detail>
-                <Heading className={spacingStyles.blokkXs} size="medium">
-                    {tekst('heading')}
-                </Heading>
-                <BodyLong className={`${spacingStyles.mb1}`}>{tekst('beskrivelse')}</BodyLong>
-                <BodyShort className={`${spacingStyles.mb1}`}>{tekst('hvaTenkerDu')}</BodyShort>
-                <BodyShort className={`${spacingStyles.mb1}`}>{tekst('klareDegSelv')}</BodyShort>
-                <div className={`${flexStyles.flex} ${flexStyles.flexColumn}`}>
-                    <Button>Ja, jeg ønsker å klare meg selv</Button>
-                    <Button variant="secondary" className={`${spacingStyles.mt1}`}>
-                        Nei, jeg har behov for veiledning
-                    </Button>
-                </div>
-            </div>
-            <InViewport loggTekst="Viser behovsavklaringkomponent i viewport" />
-        </Panel>
-    );
+    return behovForVeiledning ? <SvartPaaBehovsavklaring /> : <IkkeSvartPaaBehovsavklaring />;
 }
 
 export default Behovsavklaring;
---
+++ src/komponenter/behovsavklaring-oppfolging/behovsavklaring-svart.tsx
@@ -0,0 +1,61 @@
+import { Dialog } from '@navikt/ds-icons';
+import { Detail, Heading, Panel } from '@navikt/ds-react';
+
+import { useSprakValg } from '../../contexts/sprak';
+import { useBehovForVeiledning } from '../../contexts/behov-for-veiledning';
+
+import lagHentTekstForSprak from '../../lib/lag-hent-tekst-for-sprak';
+import ErRendret from '../er-rendret/er-rendret';
+import InViewport from '../in-viewport/in-viewport';
+
+import spacingStyles from '../../spacing.module.css';
+import flexStyles from '../../flex.module.css';
+
+const TEKSTER = {
+    nb: {
+        overskrift: 'Hjelp og støtte',
+        heading: 'Hva slags veiledning ønsker du?',
+        beskrivelse:
+            'Vi tror du har gode muligheter til å søke og skaffe seg jobb på egenhånd - uten hjelp fra veileder.',
+        hvaTenkerDu: 'Hva tenker du?',
+        klareDegSelv: 'Ønsker du å klare deg selv?',
+        readMoreHeading: 'Hva slags hjelp kan jeg få?',
+    },
+    en: {
+        heading: 'Get in touch if you need help',
+        readMoreHeading: 'What kind of help can I get?',
+    },
+};
+
+function SvartPaaBehovsavklaring() {
+    const { behovForVeiledning } = useBehovForVeiledning();
+    const sprak = useSprakValg().sprak;
+    const tekst = lagHentTekstForSprak(TEKSTER, sprak);
+
+    return (
+        <Panel className={`${flexStyles.flex} ${spacingStyles.px1_5}`}>
+            <ErRendret loggTekst="Rendrer behovsavklaringkomponent" />
+            <span
+                style={{
+                    marginRight: '0.5em',
+                    position: 'relative',
+                    top: '6px',
+                    fontSize: 'var(--navds-font-size-heading-medium)',
+                }}
+            >
+                <Dialog />
+            </span>
+            <div className={spacingStyles.fullWidth}>
+                <Detail uppercase style={{ marginTop: '-1rem' }}>
+                    {tekst('overskrift')}
+                </Detail>
+                <Heading className={spacingStyles.blokkXs} size="medium">
+                    {tekst('heading')} {behovForVeiledning?.oppfolging}
+                </Heading>
+            </div>
+            <InViewport loggTekst="Viser behovsavklaringkomponent i viewport" />
+        </Panel>
+    );
+}
+
+export default SvartPaaBehovsavklaring;
