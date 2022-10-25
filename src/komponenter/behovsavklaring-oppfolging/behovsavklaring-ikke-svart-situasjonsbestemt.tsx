92b14fc304d5a4e27aea9b47a2087743ab38bec5
Geir Gåsodden <geir.gasodden@gmail.com>
2022-10-25 12:02:31 +0200

    r splitter ikke svart i egne komponenter for standard og situasjonsbestemt

---
+++ src/komponenter/behovsavklaring-oppfolging/behovsavklaring-ikke-svart-situasjonsbestemt.tsx
@@ -0,0 +1,85 @@
+import { Dialog } from '@navikt/ds-icons';
+import { BodyLong, BodyShort, Button, Detail, Heading, Panel } from '@navikt/ds-react';
+
+import { useSprakValg } from '../../contexts/sprak';
+import { BehovForVeiledningValg, useBehovForVeiledning } from '../../contexts/behov-for-veiledning';
+import { useAmplitudeData } from '../../contexts/amplitude-context';
+
+import lagHentTekstForSprak from '../../lib/lag-hent-tekst-for-sprak';
+import { loggAktivitet } from '../../metrics/metrics';
+import ReadMoreVeileder from './readmore-veileder';
+import ErRendret from '../er-rendret/er-rendret';
+import InViewport from '../in-viewport/in-viewport';
+
+import spacingStyles from '../../spacing.module.css';
+import flexStyles from '../../flex.module.css';
+import ReadMoreVurdering from './readmore-vurdering';
+
+const TEKSTER = {
+    nb: {
+        overskrift: 'Hjelp og støtte',
+        heading: 'Ønsker du hjelp fra en veileder?',
+        beskrivelse: 'Vi tror du vil trenge hjelp fra en veileder for å nå ditt mål om arbeid.',
+        hvaTenkerDu: 'Hva tenker du?',
+        klareDegSelv: 'Ønsker du å hjelp fra en veileder?',
+        readMoreHeading: 'Hva slags hjelp kan jeg få?',
+    },
+    en: {
+        heading: 'Get in touch if you need help',
+        readMoreHeading: 'What kind of help can I get?',
+    },
+};
+
+function IkkeSvartPaaBehovsavklaringSituasjonsbestemt() {
+    const { lagreBehovForVeiledning } = useBehovForVeiledning();
+    const { amplitudeData } = useAmplitudeData();
+    const sprak = useSprakValg().sprak;
+    const tekst = lagHentTekstForSprak(TEKSTER, sprak);
+
+    function handleBehovForVeiledning(behov: BehovForVeiledningValg) {
+        lagreBehovForVeiledning(behov);
+        loggAktivitet({ ...amplitudeData, aktivitet: `Velger ${behov} fra behovsavklaringkomponent` });
+    }
+
+    return (
+        <Panel className={`${flexStyles.flex} ${spacingStyles.px1_5}`}>
+            <ErRendret loggTekst="Rendrer behovsavklaringkomponent ikke besvart" />
+            <span
+                style={{
+                    marginRight: '0.5em',
+                    position: 'relative',
+                    top: '6px',
+                    fontSize: 'var(--navds-font-size-heading-medium)',
+                }}
+            >
+                <Dialog />
+            </span>
+            <div className={spacingStyles.fullWidth}>
+                <Detail uppercase style={{ marginTop: '-1rem' }}>
+                    {tekst('overskrift')}
+                </Detail>
+                <Heading className={spacingStyles.blokkXs} size="medium">
+                    {tekst('heading')}
+                </Heading>
+                <BodyLong className={`${spacingStyles.mb1}`}>{tekst('beskrivelse')}</BodyLong>
+                <BodyShort className={`${spacingStyles.mb1}`}>{tekst('hvaTenkerDu')}</BodyShort>
+                <BodyShort className={`${spacingStyles.mb1}`}>{tekst('klareDegSelv')}</BodyShort>
+                <div className={`${flexStyles.flex} ${flexStyles.flexColumn} ${spacingStyles.mb1}`}>
+                    <Button onClick={() => handleBehovForVeiledning('ONSKER_OPPFOLGING')}>Ja, jeg ønsker hjelp</Button>
+                    <Button
+                        onClick={() => handleBehovForVeiledning('KLARE_SEG_SELV')}
+                        variant="secondary"
+                        className={`${spacingStyles.mt1}`}
+                    >
+                        Nei, jeg vil gjerne klare meg selv
+                    </Button>
+                </div>
+                <ReadMoreVurdering />
+                <ReadMoreVeileder />
+            </div>
+            <InViewport loggTekst="Viser behovsavklaringkomponent ikke besvart i viewport" />
+        </Panel>
+    );
+}
+
+export default IkkeSvartPaaBehovsavklaringSituasjonsbestemt;
--- src/komponenter/behovsavklaring-oppfolging/behovsavklaring-ikke-svart.tsx
+++ src/komponenter/behovsavklaring-oppfolging/behovsavklaring-ikke-svart.tsx
@@ -1,87 +1,18 @@
-import { Dialog } from '@navikt/ds-icons';
-import { BodyLong, BodyShort, Button, Detail, Heading, Panel } from '@navikt/ds-react';
+import { useBrukerregistreringData, ForeslattInnsatsgruppe } from '../../contexts/brukerregistrering';
 
-import { useSprakValg } from '../../contexts/sprak';
-import { BehovForVeiledningValg, useBehovForVeiledning } from '../../contexts/behov-for-veiledning';
-import { useAmplitudeData } from '../../contexts/amplitude-context';
-
-import lagHentTekstForSprak from '../../lib/lag-hent-tekst-for-sprak';
-import { loggAktivitet } from '../../metrics/metrics';
-import ReadMoreVeileder from './readmore-veileder';
-import ErRendret from '../er-rendret/er-rendret';
-import InViewport from '../in-viewport/in-viewport';
-
-import spacingStyles from '../../spacing.module.css';
-import flexStyles from '../../flex.module.css';
-import ReadMoreVurdering from './readmore-vurdering';
-
-const TEKSTER = {
-    nb: {
-        overskrift: 'Hjelp og støtte',
-        heading: 'Hva slags veiledning ønsker du?',
-        beskrivelse:
-            'Vi tror du har gode muligheter til å søke og skaffe seg jobb på egenhånd - uten hjelp fra veileder.',
-        hvaTenkerDu: 'Hva tenker du?',
-        klareDegSelv: 'Ønsker du å klare deg selv?',
-        readMoreHeading: 'Hva slags hjelp kan jeg få?',
-    },
-    en: {
-        heading: 'Get in touch if you need help',
-        readMoreHeading: 'What kind of help can I get?',
-    },
-};
+import IkkeSvartPaaBehovsavklaringStandardInnsats from './behovsavklaring-ikke-svart-standard';
+import IkkeSvartPaaBehovsavklaringSituasjonsbestemt from './behovsavklaring-ikke-svart-situasjonsbestemt';
 
 function IkkeSvartPaaBehovsavklaring() {
-    const { lagreBehovForVeiledning } = useBehovForVeiledning();
-    const { amplitudeData } = useAmplitudeData();
-    const sprak = useSprakValg().sprak;
-    const tekst = lagHentTekstForSprak(TEKSTER, sprak);
+    const brukerregistreringsData = useBrukerregistreringData();
+    const foreslattInnsatsgruppe = brukerregistreringsData?.registrering?.profilering?.innsatsgruppe;
 
-    function handleBehovForVeiledning(behov: BehovForVeiledningValg) {
-        lagreBehovForVeiledning(behov);
-        loggAktivitet({ ...amplitudeData, aktivitet: `Velger ${behov} fra behovsavklaringkomponent` });
-    }
+    if (!foreslattInnsatsgruppe) return null;
 
-    return (
-        <Panel className={`${flexStyles.flex} ${spacingStyles.px1_5}`}>
-            <ErRendret loggTekst="Rendrer behovsavklaringkomponent ikke besvart" />
-            <span
-                style={{
-                    marginRight: '0.5em',
-                    position: 'relative',
-                    top: '6px',
-                    fontSize: 'var(--navds-font-size-heading-medium)',
-                }}
-            >
-                <Dialog />
-            </span>
-            <div className={spacingStyles.fullWidth}>
-                <Detail uppercase style={{ marginTop: '-1rem' }}>
-                    {tekst('overskrift')}
-                </Detail>
-                <Heading className={spacingStyles.blokkXs} size="medium">
-                    {tekst('heading')}
-                </Heading>
-                <BodyLong className={`${spacingStyles.mb1}`}>{tekst('beskrivelse')}</BodyLong>
-                <BodyShort className={`${spacingStyles.mb1}`}>{tekst('hvaTenkerDu')}</BodyShort>
-                <BodyShort className={`${spacingStyles.mb1}`}>{tekst('klareDegSelv')}</BodyShort>
-                <div className={`${flexStyles.flex} ${flexStyles.flexColumn} ${spacingStyles.mb1}`}>
-                    <Button onClick={() => handleBehovForVeiledning('KLARE_SEG_SELV')}>
-                        Ja, jeg ønsker å klare meg selv
-                    </Button>
-                    <Button
-                        onClick={() => handleBehovForVeiledning('ONSKER_OPPFOLGING')}
-                        variant="secondary"
-                        className={`${spacingStyles.mt1}`}
-                    >
-                        Nei, jeg har behov for veiledning
-                    </Button>
-                </div>
-                <ReadMoreVurdering />
-                <ReadMoreVeileder />
-            </div>
-            <InViewport loggTekst="Viser behovsavklaringkomponent ikke besvart i viewport" />
-        </Panel>
+    return foreslattInnsatsgruppe === ForeslattInnsatsgruppe.STANDARD_INNSATS ? (
+        <IkkeSvartPaaBehovsavklaringStandardInnsats />
+    ) : (
+        <IkkeSvartPaaBehovsavklaringSituasjonsbestemt />
     );
 }
 
