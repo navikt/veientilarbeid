fdc56501e566a8a4d308aba341b9f7040d12ee01
Geir Gåsodden <geir.gasodden@gmail.com>
2022-10-25 07:16:37 +0200

    r utvider klare seg selv kortet med tittel og ReadMore for dialog

--- src/komponenter/behovsavklaring-oppfolging/behovsavklaring-svart-klare-seg-selv.tsx
+++ src/komponenter/behovsavklaring-oppfolging/behovsavklaring-svart-klare-seg-selv.tsx
@@ -1,8 +1,7 @@
 import { Dialog } from '@navikt/ds-icons';
-import { Detail, Heading, Panel } from '@navikt/ds-react';
+import { Detail, Heading, Panel, ReadMore } from '@navikt/ds-react';
 
 import { useSprakValg } from '../../contexts/sprak';
-import { useBehovForVeiledning } from '../../contexts/behov-for-veiledning';
 
 import lagHentTekstForSprak from '../../lib/lag-hent-tekst-for-sprak';
 import ErRendret from '../er-rendret/er-rendret';
@@ -14,12 +13,8 @@ import flexStyles from '../../flex.module.css';
 const TEKSTER = {
     nb: {
         overskrift: 'Hjelp og støtte',
-        heading: 'Hva slags veiledning ønsker du?',
-        beskrivelse:
-            'Vi tror du har gode muligheter til å søke og skaffe seg jobb på egenhånd - uten hjelp fra veileder.',
-        hvaTenkerDu: 'Hva tenker du?',
-        klareDegSelv: 'Ønsker du å klare deg selv?',
-        readMoreHeading: 'Hva slags hjelp kan jeg få?',
+        heading: 'Du ønsker å klare deg selv',
+        readMoreHeading: 'Si fra dersom du likevel ønsker veiledning',
     },
     en: {
         heading: 'Get in touch if you need help',
@@ -28,7 +23,6 @@ const TEKSTER = {
 };
 
 function SvartKlareSegSelvPaaBehovsavklaring() {
-    const { behovForVeiledning } = useBehovForVeiledning();
     const sprak = useSprakValg().sprak;
     const tekst = lagHentTekstForSprak(TEKSTER, sprak);
 
@@ -50,8 +44,11 @@ function SvartKlareSegSelvPaaBehovsavklaring() {
                     {tekst('overskrift')}
                 </Detail>
                 <Heading className={spacingStyles.blokkXs} size="medium">
-                    {tekst('heading')} {behovForVeiledning?.oppfolging}
+                    {tekst('heading')}
                 </Heading>
+                <ReadMore size="medium" header={tekst('readMoreHeading')}>
+                    Her kommer kanskje en dialog?
+                </ReadMore>
             </div>
             <InViewport loggTekst="Viser behovsavklaringkomponent i viewport" />
         </Panel>
