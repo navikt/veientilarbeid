4e09a5969ecd9a947a16aeb3a1f5882b5e2a0396
Geir Gåsodden <geir.gasodden@gmail.com>
2022-11-02 07:03:06 +0100

    r legger til reell switch for standard på behovsavklaring etter vedtak

--- src/komponenter/behovsavklaring-oppfolging/behovsavklaring-avklart.tsx
+++ src/komponenter/behovsavklaring-oppfolging/behovsavklaring-avklart.tsx
@@ -1,8 +1,11 @@
+import { useOppfolgingData, Servicegruppe } from '../../contexts/oppfolging';
+
 import BehovsavklaringAvklartSituasjonsbestemt from './behovsavklaring-avklart-situasjonsbestemt';
 import BehovsavklaringAvklartStandard from './behovsavklaring-avklart-standard';
 
 function BehovsavklaringAvklart() {
-    const erStandard = true;
+    const { servicegruppe } = useOppfolgingData();
+    const erStandard = servicegruppe === Servicegruppe.IKVAL;
 
     return erStandard ? <BehovsavklaringAvklartStandard /> : <BehovsavklaringAvklartSituasjonsbestemt />;
 }
