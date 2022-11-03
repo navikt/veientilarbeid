eeedbb8e7f0de6c662f771ac07f0c3a4f8e29677
Geir Gåsodden <geir.gasodden@gmail.com>
2022-11-03 07:50:16 +0100

    r legger til sjekk for hvorvidt eventuelt svar på behovsavklaring fortsatt er gyldig

--- src/komponenter/behovsavklaring-oppfolging/behovsavklaring-oppfolging.tsx
+++ src/komponenter/behovsavklaring-oppfolging/behovsavklaring-oppfolging.tsx
@@ -1,21 +1,27 @@
 import { useBehovForVeiledning } from '../../contexts/behov-for-veiledning';
 import { useOppfolgingData, Servicegruppe } from '../../contexts/oppfolging';
+import { useArbeidssokerPerioder } from '../../contexts/arbeidssoker';
 
 import IkkeSvartPaaBehovsavklaring from './behovsavklaring-ikke-svart';
 import SvartPaaBehovsavklaring from './behovsavklaring-svart';
 import BehovsavklaringAvklart from './behovsavklaring-avklart';
+import beregnArbeidssokerperioder from '../../lib/beregn-arbeidssokerperioder';
 
 function Behovsavklaring() {
     const { behovForVeiledning } = useBehovForVeiledning();
     const { servicegruppe } = useOppfolgingData();
+    const arbeidssokerperioderData = useArbeidssokerPerioder();
+    const { harAktivArbeidssokerperiode, aktivPeriodeStart } = beregnArbeidssokerperioder(arbeidssokerperioderData);
+    const harSistSvartDato = behovForVeiledning && behovForVeiledning.dato ? new Date(behovForVeiledning.dato) : null;
+    const harPeriodeStart = harAktivArbeidssokerperiode === 'Ja' ? new Date(aktivPeriodeStart) : null;
 
     if (servicegruppe !== Servicegruppe.IVURD) {
         return <BehovsavklaringAvklart servicegruppe={servicegruppe} />;
     }
 
-    //TODO: sjekke at dato for svar er senere enn start på løpende arbeidssøkerperiode
+    const harGyldigBehovsvurdering = harSistSvartDato && harPeriodeStart && harSistSvartDato > harPeriodeStart;
 
-    return !behovForVeiledning ? <IkkeSvartPaaBehovsavklaring /> : <SvartPaaBehovsavklaring />;
+    return !harGyldigBehovsvurdering ? <IkkeSvartPaaBehovsavklaring /> : <SvartPaaBehovsavklaring />;
 }
 
 export default Behovsavklaring;
