349a9a3277eb7fcd03872ec90f218fba65b84572
Geir Gåsodden <geir.gasodden@gmail.com>
2022-11-16 09:03:18 +0100

    r tar i bruk MeldepliktProvider på arbeidssoker-innhold for å kunne berike amplitudedata med svar på spm 5

--- src/innhold/arbeidssoker-innhold-wrapper.tsx
+++ src/innhold/arbeidssoker-innhold-wrapper.tsx
@@ -1,10 +1,13 @@
 import ArbeidssokerDataProvider from './arbeidssoker-data-provider';
 import ArbeidssokerInnhold from './arbeidssoker-innhold';
+import { MeldepliktProvider } from '../contexts/meldeplikt';
 
 function ArbeidssokerInnholdWrapper() {
     return (
         <ArbeidssokerDataProvider>
-            <ArbeidssokerInnhold />
+            <MeldepliktProvider>
+                <ArbeidssokerInnhold />
+            </MeldepliktProvider>
         </ArbeidssokerDataProvider>
     );
 }
