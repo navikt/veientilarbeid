7e106db6f81305e6e6a66c00340b70622935f70d
Geir Gåsodden <geir.gasodden@gmail.com>
2022-11-17 07:40:12 +0100

    f lager komponent for å vise videre registrert valg fra sist innsendte meldekort

---
+++ src/komponenter/reaktivering/siste-meldekort-videre-registrert-valg.tsx
@@ -0,0 +1,19 @@
+import { BodyLong } from '@navikt/ds-react';
+
+import { Meldeplikt } from '../../contexts/meldeplikt';
+import prettyPrintDato from '../../utils/pretty-print-dato';
+
+import spacingStyles from '../../spacing.module.css';
+
+function SisteMeldekortVidereRegistrertValg({ meldeplikt }: { meldeplikt: Meldeplikt | null }) {
+    if (!meldeplikt) return null;
+
+    return (
+        <BodyLong className={spacingStyles.mb1}>
+            Du sa at du ønsket å{meldeplikt.erArbeidssokerNestePeriode ? ' fortsatt ' : ' ikke '}være registrert som
+            arbeidssøker på meldekortet som ble levert {prettyPrintDato(meldeplikt.eventOpprettet)}
+        </BodyLong>
+    );
+}
+
+export default SisteMeldekortVidereRegistrertValg;
