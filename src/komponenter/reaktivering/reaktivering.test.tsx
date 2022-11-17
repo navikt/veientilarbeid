600aa67c29cc90a106df0f5c6a9d77579e61cec1
Geir Gåsodden <geir.gasodden@gmail.com>
2022-11-17 14:00:08 +0100

    r legger inn flere tester for reaktiveringskomponenten

--- src/komponenter/reaktivering/reaktivering.test.tsx
+++ src/komponenter/reaktivering/reaktivering.test.tsx
@@ -3,6 +3,8 @@ import { render, screen } from '@testing-library/react';
 import { contextProviders, ProviderProps } from '../../test/test-context-providers';
 import Reaktivering from './reaktivering';
 import { mockIntersectionObserver } from '../../mocks/intersection-observer-mock';
+import levertMeldekortMock from '../../mocks/meldeplikt-hendelser.mock';
+import { Meldeplikt } from '../../contexts/meldeplikt';
 
 describe('Tester at komponenten rendres slik den skal', () => {
     beforeEach(() => {
@@ -16,12 +18,40 @@ describe('Tester at komponenten rendres slik den skal', () => {
         expect(screen.getByText(/Har du mottatt dagpenger vil utbetalingene nå være stoppet/i)).toBeInTheDocument();
     });
 
+    test('Viser reaktivering-kanskje-aktuelt dersom brukeren ikke har svart nei på spørsmål 5', async () => {
+        const meldeplikt: Meldeplikt = levertMeldekortMock(false);
+        const providerProps: ProviderProps = { profil: {}, meldeplikt };
+        render(<Reaktivering />, { wrapper: contextProviders(providerProps) });
+        expect(screen.getByText(/du sa at du ønsket/i)).toBeInTheDocument();
+    });
+
     test('Riktig tekst vises dersom brukeren ikke har behov for å være arbeidssøker', async () => {
         const providerProps: ProviderProps = {
             profil: { aiaReaktiveringVisning: { valg: 'nei', oppdatert: new Date().toISOString() } },
         };
         render(<Reaktivering />, { wrapper: contextProviders(providerProps) });
         expect(screen.getByText(/registrer deg som arbeidssøker/i)).toBeInTheDocument();
+        expect(await screen.queryByText(/mottatt dagpenger vil utbetalingene nå være stoppet/)).not.toBeInTheDocument();
+    });
+
+    test('Riktig tekst vises dersom brukeren ikke har behov for å være arbeidssøker selv om meldekortet sier noe annet', async () => {
+        const meldeplikt: Meldeplikt = levertMeldekortMock(true);
+        const providerProps: ProviderProps = {
+            profil: { aiaReaktiveringVisning: { valg: 'nei', oppdatert: new Date('2022-11-01').toISOString() } },
+            meldeplikt: { ...meldeplikt, eventOpprettet: new Date('2022-10-01').toISOString() },
+        };
+        render(<Reaktivering />, { wrapper: contextProviders(providerProps) });
+        expect(screen.getByText(/registrer deg som arbeidssøker/i)).toBeInTheDocument();
+        expect(await screen.queryByText(/mottatt dagpenger vil utbetalingene nå være stoppet/)).not.toBeInTheDocument();
+    });
+
+    test('Riktig tekst vises meldekortet er nyere enn profilsvar', async () => {
+        const meldeplikt: Meldeplikt = levertMeldekortMock(false);
+        const providerProps: ProviderProps = {
+            profil: { aiaReaktiveringVisning: { valg: 'ja', oppdatert: new Date('2022-10-01').toISOString() } },
+            meldeplikt: { ...meldeplikt, eventOpprettet: new Date('2022-11-01').toISOString() },
+        };
+        render(<Reaktivering />, { wrapper: contextProviders(providerProps) });
         expect(await screen.queryByText(/mottatt dagpenger vil utbetalingene nå være stoppet/)).not.toBeInTheDocument();
     });
 });
--- src/mocks/meldeplikt-hendelser.mock.ts
+++ src/mocks/meldeplikt-hendelser.mock.ts
@@ -1,8 +1,11 @@
+import { MeldeKortType } from '../contexts/meldeplikt';
+
 const levertMeldekortMock = (erArbeidssokerNestePeriode: boolean) => ({
     erArbeidssokerNestePeriode,
     periodeFra: '2022-10-24',
     periodeTil: '2022-11-06',
-    meldekorttype: 'MANUELL_ARENA',
+    meldekorttype: 'MANUELL_ARENA' as MeldeKortType,
     eventOpprettet: '2022-11-09T12:30:52.107',
 });
+
 export default levertMeldekortMock;
