3de9ebeae69279bd4ba1ad1cf6dfe7a447027d31
Geir Gåsodden <geir.gasodden@gmail.com>
2022-12-20 08:06:20 +0100

    r skjuler reasten av AiA når reaktiveringskortet er aktivt for ikke-standard

--- src/innhold/innhold-ikke-standard.tsx
+++ src/innhold/innhold-ikke-standard.tsx
@@ -1,3 +1,6 @@
+import { useFeatureToggleData } from '../contexts/feature-toggles';
+import { useReaktivering } from '../contexts/reaktivering';
+
 import InnholdMetrics from './innhold-metrics';
 import InViewport from '../komponenter/in-viewport/in-viewport';
 import RegistrertTittel from '../komponenter/registrert-tittel/registrert-tittel';
@@ -5,20 +8,31 @@ import ReaktiveringKvittering from '../komponenter/reaktivering/reaktivering-kvi
 import IkkeStandard from '../komponenter/ikke-standard/ikke-standard';
 import Motestotte from '../komponenter/ikke-standard/motestotte';
 import { AutomatiskReaktivert } from '../komponenter/reaktivering/automatisk-reaktivert';
+import { visAutomatiskReaktiveringsKort } from '../lib/vis-automatisk-reaktiverings-kort';
 
 import styles from './innhold.module.css';
 
 const InnholdView = () => {
+    const featureToggleData = useFeatureToggleData();
+    const { reaktivering } = useReaktivering();
+
+    const skalViseReaktiveringsKort = visAutomatiskReaktiveringsKort(featureToggleData, reaktivering);
+
     return (
         <>
             <InnholdMetrics />
             <InViewport loggTekst="AiA i viewport" />
             <div className={styles.limit}>
-                <AutomatiskReaktivert />
-                <ReaktiveringKvittering />
-                <RegistrertTittel />
-                <IkkeStandard />
-                <Motestotte />
+                {skalViseReaktiveringsKort ? (
+                    <AutomatiskReaktivert />
+                ) : (
+                    <>
+                        <ReaktiveringKvittering />
+                        <RegistrertTittel />
+                        <IkkeStandard />
+                        <Motestotte />
+                    </>
+                )}
             </div>
             <InViewport loggTekst="AiA i viewport - bunnen" />
         </>
