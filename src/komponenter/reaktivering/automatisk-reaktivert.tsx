c740924cc5711eb7bb782a655b3f78f3443443ff
Geir Gåsodden <geir.gasodden@gmail.com>
2023-01-09 12:05:44 +0100

    r legger til ErRendret på automatisk-reaktivert

--- src/komponenter/reaktivering/automatisk-reaktivert.tsx
+++ src/komponenter/reaktivering/automatisk-reaktivert.tsx
@@ -10,6 +10,7 @@ import { useAmplitudeData } from '../hent-initial-data/amplitude-provider';
 import { ReadMoreInaktivering } from './readmore-derfor-ble-du-inaktivert';
 import { ReadMoreViktigRegistrert } from './readmore-viktig-registrert';
 import InViewport from '../in-viewport/in-viewport';
+import ErRendret from '../er-rendret/er-rendret';
 import beregnArbeidssokerperioder from '../../lib/beregn-arbeidssokerperioder';
 import prettyPrintDato from '../../utils/pretty-print-dato';
 import { loggAktivitet } from '../../metrics/metrics';
@@ -71,6 +72,7 @@ function AutomatiskReaktivert() {
 
     return (
         <Panel className={spacingStyles.px1_5}>
+            <ErRendret loggTekst="Rendrer automatisk reaktivert" />
             {visSprakvelger && (
                 <Button variant="tertiary" size="xsmall" className={spacingStyles.ml1_75} onClick={toggleByttSprak}>
                     {tekst('sprak')}
