7fca00da2ba62ece91cb96c8227cd13c532a29f6
Geir Gåsodden <geir.gasodden@gmail.com>
2023-04-20 11:11:26 +0200

    r fjerner heading for Min situasjon

--- src/komponenter/min-situasjon/min-situasjon.tsx
+++ src/komponenter/min-situasjon/min-situasjon.tsx
@@ -1,31 +1,17 @@
-import { Calender } from '@navikt/ds-icons';
-import { Detail, Heading, Panel } from '@navikt/ds-react';
+import { Panel } from '@navikt/ds-react';
 
-import { useSprakValg } from '../../contexts/sprak';
 import { useAmplitudeData } from '../hent-initial-data/amplitude-provider';
 import { useBrukerregistreringData } from '../../contexts/brukerregistrering';
 import { useArbeidssokerPerioder } from '../../contexts/arbeidssoker';
 import { InnloggingsNiva, useAutentiseringData } from '../../contexts/autentisering';
 
-import lagHentTekstForSprak from '../../lib/lag-hent-tekst-for-sprak';
 import Sammendrag from './sammendrag';
 import InnsynLesMer from '../innsyn/innsyn-les-mer';
 import beregnArbeidssokerperioder from '../../lib/beregn-arbeidssokerperioder';
 
 import spacingStyles from '../../spacing.module.css';
-import flexStyles from '../../flex.module.css';
 
-const TEKSTER = {
-    nb: {
-        detail: 'Min situasjon',
-        heading: 'Min arbeidssøkersituasjon',
-    },
-    en: {
-        detail: 'My situation',
-        heading: 'My situation as job seeker',
-    },
-};
-
+/*
 const situasjon = {
     MISTET_JOBBEN: 'Har mistet eller kommer til å miste jobben',
     ALDRI_HATT_JOBB: 'Har aldri vært i jobb',
@@ -39,6 +25,7 @@ const situasjon = {
     DELTIDSJOBB_VIL_MER: 'Har deltidsjobb, men vil jobbe mer',
     DEFAULT: 'Min arbeidssøkersituasjon',
 };
+*/
 
 function MinSituasjon(props: any) {
     const brukerregistreringData = useBrukerregistreringData();
@@ -46,41 +33,20 @@ function MinSituasjon(props: any) {
     const { amplitudeData } = useAmplitudeData();
     const autentiseringData = useAutentiseringData();
 
-    const sprak = useSprakValg().sprak;
-    const tekst = lagHentTekstForSprak(TEKSTER, sprak);
     const { aktivPeriodeStart } = beregnArbeidssokerperioder(arbeidssokerperiodeData);
-    const { opprettetDato, manueltRegistrertAv, besvarelse } = brukerregistreringData?.registrering || {};
-    const minSituasjon = (besvarelse && besvarelse['dinSituasjon']) || 'DEFAULT';
+    const { opprettetDato, manueltRegistrertAv } = brukerregistreringData?.registrering || {};
     const kanViseKomponent = autentiseringData.securityLevel === InnloggingsNiva.LEVEL_4;
 
     if (!kanViseKomponent) return null;
 
     return (
-        <Panel className={`${flexStyles.flex} ${spacingStyles.px1_5}`}>
-            <span
-                style={{
-                    marginRight: '0.5em',
-                    position: 'relative',
-                    top: '6px',
-                    fontSize: 'var(--a-font-size-heading-medium)',
-                }}
-            >
-                <Calender aria-hidden="true" />
-            </span>
-            <div className={spacingStyles.fullWidth}>
-                <Detail uppercase style={{ marginTop: '-1rem' }}>
-                    {tekst('detail')}
-                </Detail>
-                <Heading className={spacingStyles.blokkXs} size="medium">
-                    {besvarelse ? situasjon[minSituasjon] : tekst('heading')}
-                </Heading>
-                <Sammendrag
-                    startDato={opprettetDato || aktivPeriodeStart}
-                    manueltRegistrertAv={manueltRegistrertAv}
-                    amplitudeData={amplitudeData}
-                />
-                <InnsynLesMer />
-            </div>
+        <Panel className={spacingStyles.ml2_39}>
+            <Sammendrag
+                startDato={opprettetDato || aktivPeriodeStart}
+                manueltRegistrertAv={manueltRegistrertAv}
+                amplitudeData={amplitudeData}
+            />
+            <InnsynLesMer />
         </Panel>
     );
 }
