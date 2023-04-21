a63bdc29d230298d1668044fbf88cd94b2ecf1f0
Geir Gåsodden <geir.gasodden@gmail.com>
2023-04-21 07:19:39 +0200

    r flytter tabs demo inn i arbeidssoker-innhold-wrapper

--- src/innhold/arbeidssoker-innhold-wrapper.tsx
+++ src/innhold/arbeidssoker-innhold-wrapper.tsx
@@ -1,15 +1,19 @@
+import { useFeatureToggleData } from '../contexts/feature-toggles';
+
 import ArbeidssokerDataProvider from './arbeidssoker-data-provider';
 import ArbeidssokerInnhold from './arbeidssoker-innhold';
+import TabsDemo from '../tabs-demo/tabs-demo';
 import { MeldepliktProvider } from '../contexts/meldeplikt';
 import { ReaktiveringProvider } from '../contexts/reaktivering';
 
 function ArbeidssokerInnholdWrapper() {
+    const featureToggles = useFeatureToggleData();
+    const brukTabsDemo = featureToggles['aia.bruk-tabs-demo'];
+
     return (
         <ArbeidssokerDataProvider>
             <MeldepliktProvider>
-                <ReaktiveringProvider>
-                    <ArbeidssokerInnhold />
-                </ReaktiveringProvider>
+                <ReaktiveringProvider>{brukTabsDemo ? <TabsDemo /> : <ArbeidssokerInnhold />}</ReaktiveringProvider>
             </MeldepliktProvider>
         </ArbeidssokerDataProvider>
     );
--- src/tabs-demo/tabs-demo.tsx
+++ src/tabs-demo/tabs-demo.tsx
@@ -1,32 +1,74 @@
-import { Tabs } from '@navikt/ds-react';
+import { BodyShort, Heading, Panel, Tabs } from '@navikt/ds-react';
+
+import RegistrertTittel from '../komponenter/registrert-tittel/registrert-tittel';
+import MinSituasjon from '../komponenter/min-situasjon/min-situasjon';
+import HjelpOgStotte from '../komponenter/hjelp-og-stotte/hjelp-og-stotte';
 
 import styles from '../innhold/innhold.module.css';
+import tabStyles from './tabs.module.css';
 
-const Example = () => {
+const Varselboks = () => {
+    return (
+        <Panel border>
+            <Heading size="medium">Utkast/påbegynt søknad om dagpenger</Heading>
+            <BodyShort>Fullfør og send inn søknaden du startet på 21. april</BodyShort>
+        </Panel>
+    );
+};
+
+const MinSituasjonTab = () => {
+    return (
+        <Tabs.Panel value="situasjon" className="h-24 w-full bg-gray-50 p-4">
+            <div className={`${tabStyles.mln2_4} ${tabStyles.mt1}`}>
+                <MinSituasjon />
+            </div>
+        </Tabs.Panel>
+    );
+};
+
+const HjelpOgStotteTab = () => {
+    return (
+        <Tabs.Panel value="hjelp" className="h-24 w-full bg-gray-50 p-4">
+            <HjelpOgStotte />
+        </Tabs.Panel>
+    );
+};
+
+const YtelseTab = () => {
+    return (
+        <Tabs.Panel value="ytelse" className="h-24  w-full bg-gray-50 p-4">
+            Pengestøtte-tab
+        </Tabs.Panel>
+    );
+};
+
+const MeldekortTab = () => {
+    return (
+        <Tabs.Panel value="meldekort" className="h-24  w-full bg-gray-50 p-4">
+            Meldekort-tab
+        </Tabs.Panel>
+    );
+};
+
+const TabsDemo = () => {
     return (
         <div className={styles.limit}>
-            <Tabs defaultValue="situasjon">
+            <RegistrertTittel />
+            <Varselboks />
+            <Tabs defaultValue="situasjon" className={tabStyles.mb2}>
                 <Tabs.List>
                     <Tabs.Tab value="situasjon" label="Min situasjon" />
                     <Tabs.Tab value="hjelp" label="Hjelp og støtte" />
                     <Tabs.Tab value="ytelse" label="Pengestøtte" />
                     <Tabs.Tab value="meldekort" label="Meldekort" />
                 </Tabs.List>
-                <Tabs.Panel value="situasjon" className="h-24 w-full bg-gray-50 p-4">
-                    Min situasjon-tab
-                </Tabs.Panel>
-                <Tabs.Panel value="hjelp" className="h-24 w-full bg-gray-50 p-4">
-                    Hjelp og støtte-tab
-                </Tabs.Panel>
-                <Tabs.Panel value="ytelse" className="h-24  w-full bg-gray-50 p-4">
-                    Pengestøtte-tab
-                </Tabs.Panel>
-                <Tabs.Panel value="meldekort" className="h-24  w-full bg-gray-50 p-4">
-                    Meldekort-tab
-                </Tabs.Panel>
+                <MinSituasjonTab />
+                <HjelpOgStotteTab />
+                <YtelseTab />
+                <MeldekortTab />
             </Tabs>
         </div>
     );
 };
 
-export default Example;
+export default TabsDemo;
