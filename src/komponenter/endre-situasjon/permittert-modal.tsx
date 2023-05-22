997d97989ced5ea799946e1a0ce7b422bbc9b30c
Jostein Holje <jostein.holje@nav.no>
2023-05-22 15:37:42 +0200

    Begynn på fler-stegs gui i modal

--- src/komponenter/endre-situasjon/permittert-modal.tsx
+++ src/komponenter/endre-situasjon/permittert-modal.tsx
@@ -2,16 +2,15 @@ import { BodyShort, Button, Heading, Modal, Select, UNSAFE_useDatepicker } from 
 import spacing from '../../spacing.module.css';
 import flex from '../../flex.module.css';
 import React, { useEffect, useState } from 'react';
-import { DinSituasjonSvar } from '../../contexts/brukerregistrering';
 
 enum PermittertSvar {
-    OPPSIGELSE,
-    ENDRET,
-    TILBAKE_I_JOBB,
-    NY_JOBB,
-    MIDLERTIDIG_JOBB,
-    UAVKLART,
-    ANNET,
+    OPPSIGELSE = 'OPPSIGELSE',
+    ENDRET = 'ENDRET',
+    TILBAKE_I_JOBB = 'TILBAKE_I_JOBB',
+    NY_JOBB = 'NY_JOBB',
+    MIDLERTIDIG_JOBB = 'MIDLERTIDIG_JOBB',
+    UAVKLART = 'UAVKLART',
+    ANNET = 'ANNET',
 }
 const permittertTekster = {
     [PermittertSvar.OPPSIGELSE]: 'Jeg har fått oppsigelse',
@@ -29,14 +28,70 @@ interface PermittertModalProps {
     besvarelse: any | null; //BesvarelseResponse['besvarelse'] | null;
 }
 
+interface Steg1Props {
+    valgtSituasjon: string;
+    settValgtSituasjon: React.Dispatch<React.SetStateAction<string>>;
+    onClick: () => void;
+}
+const Steg1 = (props: Steg1Props) => {
+    const { valgtSituasjon, settValgtSituasjon, onClick } = props;
+    return (
+        <>
+            <Select
+                className={spacing.mb1}
+                label={'Velg den nye situasjonen som passer deg best'}
+                onChange={(e) => settValgtSituasjon(e.target.value)}
+                value={valgtSituasjon}
+            >
+                {Object.keys(permittertTekster).map((situasjon) => (
+                    <option key={situasjon} value={situasjon}>
+                        {permittertTekster[situasjon]}
+                    </option>
+                ))}
+            </Select>
+            <div className={`${flex.flex} ${flex.flexEnd}`}>
+                <Button variant={'primary'} onClick={onClick}>
+                    Neste
+                </Button>
+            </div>
+        </>
+    );
+};
+
+const Steg2 = () => {
+    return (
+        <>
+            <h1>Steg 2</h1>
+            {/*<UNSAFE_DatePicker {...datepickerProps} strategy="fixed">*/}
+            {/*    <UNSAFE_DatePicker.Input*/}
+            {/*        {...inputProps}*/}
+            {/*        className={spacing.mb1}*/}
+            {/*        label="Når mottok du oppsigelsen?"*/}
+            {/*        defaultValue={new Date().toLocaleDateString()}*/}
+            {/*    />*/}
+            {/*</UNSAFE_DatePicker>*/}
+            {/*<BodyShort className={spacing.mb1}>*/}
+            {/*    NAV bruker opplysningene til å vurdere hvor mye veiledning du trenger.*/}
+            {/*</BodyShort>*/}
+            {/*<div className={`${flex.flex} ${flex.flexEnd}`}>*/}
+            {/*    <Button variant={'primary'} onClick={handleLagreEndringer}>*/}
+            {/*        Lagre endring i situasjon*/}
+            {/*    </Button>*/}
+            {/*</div>*/}
+        </>
+    );
+};
 const PermittertModal = (props: PermittertModalProps) => {
     const { openModal, setOpenModal, /*amplitudeData,*/ besvarelse } = props;
     const { datepickerProps /*inputProps, selectedDay*/ } = UNSAFE_useDatepicker({
         fromDate: new Date('Jan 01 2022'),
         defaultSelected: new Date(),
     });
+    const [aktivSide, settAktivSide] = React.useState<number>(1);
+    const [valgtSituasjon, settValgtSituasjon] = useState<string>(PermittertSvar.OPPSIGELSE);
+
     // const { lagreBesvarelse } = useBesvarelse();
-    const [valgtSituasjon, settValgtSituasjon] = useState<string>(DinSituasjonSvar.ER_PERMITTERT);
+
     // const handleLagreEndringer = () => {
     //     loggAktivitet({
     //         aktivitet: 'Lagrer endring i jobbsituasjonen',
@@ -66,6 +121,28 @@ const PermittertModal = (props: PermittertModalProps) => {
         }
     }, [besvarelse]);
 
+    useEffect(() => {
+        settAktivSide(1);
+    }, [openModal]);
+
+    const Innhold = () => {
+        if (aktivSide == 1) {
+            return (
+                <Steg1
+                    valgtSituasjon={valgtSituasjon}
+                    settValgtSituasjon={settValgtSituasjon}
+                    onClick={() => settAktivSide(2)}
+                />
+            );
+        }
+
+        if (aktivSide == 2) {
+            return <Steg2 />;
+        }
+
+        return null;
+    };
+
     return (
         <Modal
             open={openModal}
@@ -78,36 +155,8 @@ const PermittertModal = (props: PermittertModalProps) => {
                 <Heading spacing level="1" size="large" id="modal-heading">
                     Min jobbsituasjonen har endret seg
                 </Heading>
-                <BodyShort className={spacing.mb1}>Steg 1 / 2</BodyShort>
-                <Select
-                    className={spacing.mb1}
-                    label={'Velg den nye situasjonen som passer deg best'}
-                    onChange={(e) => settValgtSituasjon(e.target.value)}
-                    value={valgtSituasjon}
-                >
-                    {Object.keys(permittertTekster).map((situasjon) => (
-                        <option key={situasjon} value={situasjon}>
-                            {permittertTekster[situasjon]}
-                        </option>
-                    ))}
-                </Select>
-                {/*<UNSAFE_DatePicker {...datepickerProps} strategy="fixed">*/}
-                {/*    <UNSAFE_DatePicker.Input*/}
-                {/*        {...inputProps}*/}
-                {/*        className={spacing.mb1}*/}
-                {/*        label="Når mottok du oppsigelsen?"*/}
-                {/*        defaultValue={new Date().toLocaleDateString()}*/}
-                {/*    />*/}
-                {/*</UNSAFE_DatePicker>*/}
-                {/*<BodyShort className={spacing.mb1}>*/}
-                {/*    NAV bruker opplysningene til å vurdere hvor mye veiledning du trenger.*/}
-                {/*</BodyShort>*/}
-                {/*<Button variant={'primary'} onClick={handleLagreEndringer}>*/}
-                {/*    Lagre endring i situasjon*/}
-                {/*</Button>*/}
-                <div className={`${flex.flex} ${flex.flexEnd}`}>
-                    <Button variant={'primary'}>Neste</Button>
-                </div>
+                <BodyShort className={spacing.mb1}>Steg {aktivSide} / 2</BodyShort>
+                <Innhold />
             </Modal.Content>
         </Modal>
     );
