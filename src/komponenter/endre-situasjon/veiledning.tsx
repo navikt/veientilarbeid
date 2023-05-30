d5fea65263c2746fbb07b59fd8ac80d0df4f42c9
Jostein Holje <jostein.holje@nav.no>
2023-05-30 13:29:01 +0200

    Veiledning for OPPSIGELSE

--- src/komponenter/endre-situasjon/veiledning.tsx
+++ src/komponenter/endre-situasjon/veiledning.tsx
@@ -1,8 +1,11 @@
-import { BodyShort } from '@navikt/ds-react';
+import { BodyShort, Link } from '@navikt/ds-react';
 
 import { DinSituasjonSvar, PermittertSvar } from './permittert-modal';
 
 import spacing from '../../spacing.module.css';
+import { mine_dagpenger_url } from '../../url';
+import prettyPrintDato from '../../utils/pretty-print-dato';
+import { plussDager } from '../../utils/date-utils';
 
 export interface VeiledningsProps {
     valgtSituasjon: PermittertSvar | DinSituasjonSvar;
@@ -10,9 +13,44 @@ export interface VeiledningsProps {
 }
 
 const OPPSIGELSE = (props: VeiledningsProps) => {
+    const { tilleggsData } = props;
+
+    if (!tilleggsData) return null;
+
+    const { oppsigelseDato, sisteArbeidsdagDato } = tilleggsData;
+
     return (
         <>
-            <BodyShort className={spacing.mb1}>Veiledning for {props.valgtSituasjon}</BodyShort>
+            <p>Basert på det du har oppgitt:</p>
+            <p>
+                Du må dokumentere oppsigelsen. Måten du gjør det på er å laste opp dokumentet via{' '}
+                <Link href={mine_dagpenger_url}>mine dagpenger</Link>.
+            </p>
+            <p>
+                Du må sende inn meldekort frem til og med meldekortet som ‘dekker’ {prettyPrintDato(oppsigelseDato!)}{' '}
+                (dagen du mottok oppsigelsen).
+                <br />
+                Siste dagen du får dagpenger som permittert er frem til og med dagen du fikk beskjeden – altså{' '}
+                {prettyPrintDato(oppsigelseDato!)}.
+                <br />
+                Frem til {prettyPrintDato(oppsigelseDato!)} får du utbetalt dagpenger som permittert.
+                <br />
+                Arbeidsgiveren din har ansvaret for å betal lønn fra{' '}
+                {prettyPrintDato(plussDager(new Date(oppsigelseDato!), 1).toISOString())} frem til{' '}
+                {prettyPrintDato(sisteArbeidsdagDato!)}. Lenke til permitteringsregelverk?
+            </p>
+            <p>
+                Du bør sende inn ny søknad om ordinære dagpenger mellom{' '}
+                {prettyPrintDato(plussDager(new Date(oppsigelseDato!), -7).toISOString())} og{' '}
+                {prettyPrintDato(sisteArbeidsdagDato!)} – da du tidligst kan få dagpenger fra den dagen du sender inn
+                søknaden.
+            </p>
+            <p>
+                Du kan svare ‘nei’ på det femtespørsmålet på ‘det siste aktuelle meldekortet’ du skal sende inn (for
+                perioden X til Y/lenke til?).
+                <br />
+                Da er det viktig at du registrere deg på nytt før du evnt. sender inn ny søknad om ordinære dagpenger.
+            </p>
         </>
     );
 };
