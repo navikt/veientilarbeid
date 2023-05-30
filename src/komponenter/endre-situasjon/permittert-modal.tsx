dcfc3148023aa906f04eeaa8e791ed0d8cac8bbf
Geir Gåsodden <geir.gasodden@gmail.com>
2023-05-30 08:53:00 +0200

    c legger til svar og tekster for dinSituasjon

--- src/komponenter/endre-situasjon/permittert-modal.tsx
+++ src/komponenter/endre-situasjon/permittert-modal.tsx
@@ -17,6 +17,32 @@ export enum PermittertSvar {
     ANNET = 'ANNET',
 }
 
+export enum DinSituasjonSvar {
+    MISTET_JOBBEN = 'MISTET_JOBBEN',
+    HAR_SAGT_OPP = 'HAR_SAGT_OPP',
+    DELTIDSJOBB_VIL_MER = 'DELTIDSJOBB_VIL_MER',
+    ALDRI_HATT_JOBB = 'ALDRI_HATT_JOBB',
+    VIL_BYTTE_JOBB = 'VIL_BYTTE_JOBB',
+    JOBB_OVER_2_AAR = 'JOBB_OVER_2_AAR',
+    ER_PERMITTERT = 'ER_PERMITTERT',
+    USIKKER_JOBBSITUASJON = 'USIKKER_JOBBSITUASJON',
+    AKKURAT_FULLFORT_UTDANNING = 'AKKURAT_FULLFORT_UTDANNING',
+    VIL_FORTSETTE_I_JOBB = 'VIL_FORTSETTE_I_JOBB',
+}
+
+export const dinSituasjonSvarTekster = {
+    [DinSituasjonSvar.MISTET_JOBBEN]: 'Har mistet eller kommer til å miste jobben',
+    [DinSituasjonSvar.HAR_SAGT_OPP]: 'Har sagt opp eller kommer til å si opp',
+    [DinSituasjonSvar.DELTIDSJOBB_VIL_MER]: 'Har deltidsjobb, men vil jobbe mer',
+    [DinSituasjonSvar.ALDRI_HATT_JOBB]: 'Har aldri vært i jobb',
+    [DinSituasjonSvar.VIL_BYTTE_JOBB]: 'Har jobb, men vil bytte',
+    [DinSituasjonSvar.JOBB_OVER_2_AAR]: 'Har ikke vært i jobb de siste 2 årene',
+    [DinSituasjonSvar.ER_PERMITTERT]: 'Er permittert eller kommer til å bli permittert',
+    [DinSituasjonSvar.USIKKER_JOBBSITUASJON]: 'Er usikker på jobbsituasjonen min',
+    [DinSituasjonSvar.AKKURAT_FULLFORT_UTDANNING]: 'Har akkurat fullført utdanning, militærtjeneste eller annet',
+    [DinSituasjonSvar.VIL_FORTSETTE_I_JOBB]: 'Har jobb og ønsker å fortsette i den jobben jeg er i',
+};
+
 export const permittertTekster = {
     [PermittertSvar.OPPSIGELSE]: 'Jeg har fått oppsigelse',
     [PermittertSvar.ENDRET_PERMITTERINGSPROSENT]: 'Permitteringsprosenten har endret seg',
