6c3ce1c8c42df53d70c3187b555675a6d4ca77a7
Geir Gåsodden <geir.gasodden@gmail.com>
2023-05-31 07:31:48 +0200

    r legger mer innhold i veiledning på steg 3

--- src/komponenter/endre-situasjon/veiledning.tsx
+++ src/komponenter/endre-situasjon/veiledning.tsx
@@ -56,42 +56,111 @@ const OPPSIGELSE = (props: VeiledningsProps) => {
     );
 };
 
-const ENDRET = (props: VeiledningsProps) => {
+const ENDRET_PERMITTERINGSPROSENT = (props: VeiledningsProps) => {
+    const { tilleggsData } = props;
+
+    if (!tilleggsData) return null;
+
     return (
         <>
-            <BodyShort className={spacing.mb1}>Veiledning for {props.valgtSituasjon}</BodyShort>
+            <p>Basert på det du har oppgitt:</p>
+            <p>
+                Du må sende oss det nye permitteringsvarselet. Måten du gjør det på er å laste opp dokumentet via{' '}
+                <Link href={mine_dagpenger_url}>mine dagpenger</Link>.
+            </p>
+            <p>Du må fortsette å sende meldekort i permitteringsperioden.</p>
         </>
     );
 };
 
 const TILBAKE_TIL_JOBB = (props: VeiledningsProps) => {
+    const { tilleggsData } = props;
+
+    if (!tilleggsData) return null;
+
+    const { forsteArbeidsdagDato } = tilleggsData;
+
     return (
         <>
-            <BodyShort className={spacing.mb1}>Veiledning for {props.valgtSituasjon}</BodyShort>
+            <p>Basert på det du har oppgitt:</p>
+            <p>
+                Du må sende inn meldekort frem til og med meldekortet som ‘dekker’{' '}
+                {prettyPrintDato(plussDager(new Date(forsteArbeidsdagDato!), -1).toISOString())} (siste dag som
+                permittert).
+            </p>
+            <p>
+                Om du ikke lenger ønsker å være registrert arbeidssøker kan du svare "nei" til spm 5 på det siste
+                aktuelle meldekortet.
+            </p>
         </>
     );
 };
 
 const NY_JOBB = (props: VeiledningsProps) => {
+    const { tilleggsData } = props;
+
+    if (!tilleggsData) return null;
+
+    const { sisteArbeidsdagDato } = tilleggsData;
+
     return (
         <>
-            <BodyShort className={spacing.mb1}>Veiledning for {props.valgtSituasjon}</BodyShort>
+            <p>Basert på det du har oppgitt:</p>
+            <p>
+                Du må sende inn meldekort frem til og med meldekortet som ‘dekker’{' '}
+                {prettyPrintDato(sisteArbeidsdagDato!)} (siste arbeidsdag i nåværende jobb).
+            </p>
+            <p>Oppsigelsestid når du er permittert er 14 dager.</p>
+            <p>
+                Om du ikke lenger ønsker å være registrert arbeidssøker kan du svare "nei" til spm 5 på det siste
+                aktuelle meldekortet.
+            </p>
         </>
     );
 };
 
 const MIDLERTIDIG_JOBB = (props: VeiledningsProps) => {
+    const { tilleggsData } = props;
+
+    if (!tilleggsData) return null;
+
     return (
         <>
-            <BodyShort className={spacing.mb1}>Veiledning for {props.valgtSituasjon}</BodyShort>
+            <p>Basert på det du har oppgitt:</p>
+            <p>Du må fortsette å sende inn meldekort i perioden du er midlertidig i arbeid.</p>
+            <p>Gi oss beskjed om noe endrer seg.</p>
         </>
     );
 };
 
 const KONKURS = (props: VeiledningsProps) => {
+    const { tilleggsData } = props;
+
+    if (!tilleggsData) return null;
+
+    const { sisteArbeidsdagDato } = tilleggsData;
+
     return (
         <>
-            <BodyShort className={spacing.mb1}>Veiledning for {props.valgtSituasjon}</BodyShort>
+            <p>Basert på det du har oppgitt:</p>
+            <p>
+                Du må dokumentere konkursen. Måten du gjør det på er å laste opp dokumentet via{' '}
+                <Link href={mine_dagpenger_url}>mine dagpenger</Link>.
+            </p>
+            <p>
+                Du må sende inn meldekort frem til og med meldekortet som ‘dekker’{' '}
+                {prettyPrintDato(sisteArbeidsdagDato!)} (siste arbeidsdag).
+            </p>
+            <p>
+                Dersom du fortsatt ønsker å stå registrert som arbeidssøker må du fortsette å sende inn meldekort også
+                etter dette.
+            </p>
+            <p>
+                Dersom du vil <Link href={dagpengerSoknadLenke}>søke om dagpenger som forskudd på lønnsgaranti</Link>,
+                bør du sende den mellom {prettyPrintDato(plussDager(new Date(sisteArbeidsdagDato!), -7).toISOString())}{' '}
+                og {prettyPrintDato(sisteArbeidsdagDato!)}
+            </p>
+            <p>Burde det også stå noe her om å søker ordinære dagpenger?</p>
         </>
     );
 };
@@ -125,7 +194,7 @@ const Veiledning = (props: VeiledningsProps) => {
     } else if (valgtSituasjon === PermittertSvar.NY_JOBB) {
         return <NY_JOBB {...props} />;
     } else if (valgtSituasjon === PermittertSvar.ENDRET_PERMITTERINGSPROSENT) {
-        return <ENDRET {...props} />;
+        return <ENDRET_PERMITTERINGSPROSENT {...props} />;
     } else if (valgtSituasjon === PermittertSvar.ANNET) {
         return <ANNET {...props} />;
     } else if (valgtSituasjon === PermittertSvar.UAVKLART) {
