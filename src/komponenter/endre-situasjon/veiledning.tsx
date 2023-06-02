1b5951b65885241c96d60a273231dda83ebbc0a8
Geir Gåsodden <geir.gasodden@gmail.com>
2023-06-02 07:51:24 +0200

    r legger inn skal for DinSituasjonSvar på steg 3

--- src/komponenter/endre-situasjon/veiledning.tsx
+++ src/komponenter/endre-situasjon/veiledning.tsx
@@ -2,11 +2,12 @@ import { BodyShort, Link, ReadMore } from '@navikt/ds-react';
 
 import { DinSituasjonSvar, PermittertSvar } from './permittert-modal';
 
-import spacing from '../../spacing.module.css';
 import { dokumentasjon_url } from '../../url';
 import prettyPrintDato from '../../utils/pretty-print-dato';
 import { plussDager } from '../../utils/date-utils';
 import { dagpengerSoknadLenke } from '../../innhold/lenker';
+
+import spacing from '../../spacing.module.css';
 
 export interface VeiledningsProps {
     valgtSituasjon: PermittertSvar | DinSituasjonSvar;
@@ -232,6 +233,86 @@ const ANNET = (props: VeiledningsProps) => {
     );
 };
 
+const MISTET_JOBBEN = (props: VeiledningsProps) => {
+    return (
+        <>
+            <BodyShort className={spacing.mb1}>Veiledning for {props.valgtSituasjon}</BodyShort>
+        </>
+    );
+};
+
+const HAR_SAGT_OPP = (props: VeiledningsProps) => {
+    return (
+        <>
+            <BodyShort className={spacing.mb1}>Veiledning for {props.valgtSituasjon}</BodyShort>
+        </>
+    );
+};
+
+const DELTIDSJOBB_VIL_MER = (props: VeiledningsProps) => {
+    return (
+        <>
+            <BodyShort className={spacing.mb1}>Veiledning for {props.valgtSituasjon}</BodyShort>
+        </>
+    );
+};
+
+const ALDRI_HATT_JOBB = (props: VeiledningsProps) => {
+    return (
+        <>
+            <BodyShort className={spacing.mb1}>Veiledning for {props.valgtSituasjon}</BodyShort>
+        </>
+    );
+};
+
+const VIL_BYTTE_JOBB = (props: VeiledningsProps) => {
+    return (
+        <>
+            <BodyShort className={spacing.mb1}>Veiledning for {props.valgtSituasjon}</BodyShort>
+        </>
+    );
+};
+
+const JOBB_OVER_2_AAR = (props: VeiledningsProps) => {
+    return (
+        <>
+            <BodyShort className={spacing.mb1}>Veiledning for {props.valgtSituasjon}</BodyShort>
+        </>
+    );
+};
+
+const ER_PERMITTERT = (props: VeiledningsProps) => {
+    return (
+        <>
+            <BodyShort className={spacing.mb1}>Veiledning for {props.valgtSituasjon}</BodyShort>
+        </>
+    );
+};
+
+const USIKKER_JOBBSITUASJON = (props: VeiledningsProps) => {
+    return (
+        <>
+            <BodyShort className={spacing.mb1}>Veiledning for {props.valgtSituasjon}</BodyShort>
+        </>
+    );
+};
+
+const AKKURAT_FULLFORT_UTDANNING = (props: VeiledningsProps) => {
+    return (
+        <>
+            <BodyShort className={spacing.mb1}>Veiledning for {props.valgtSituasjon}</BodyShort>
+        </>
+    );
+};
+
+const VIL_FORTSETTE_I_JOBB = (props: VeiledningsProps) => {
+    return (
+        <>
+            <BodyShort className={spacing.mb1}>Veiledning for {props.valgtSituasjon}</BodyShort>
+        </>
+    );
+};
+
 const Veiledning = (props: VeiledningsProps) => {
     const { valgtSituasjon } = props;
     if (valgtSituasjon === PermittertSvar.OPPSIGELSE) {
@@ -250,6 +331,26 @@ const Veiledning = (props: VeiledningsProps) => {
         return <ANNET {...props} />;
     } else if (valgtSituasjon === PermittertSvar.UAVKLART) {
         return <UAVKLART {...props} />;
+    } else if (valgtSituasjon === DinSituasjonSvar.MISTET_JOBBEN) {
+        return <MISTET_JOBBEN {...props} />;
+    } else if (valgtSituasjon === DinSituasjonSvar.HAR_SAGT_OPP) {
+        return <HAR_SAGT_OPP {...props} />;
+    } else if (valgtSituasjon === DinSituasjonSvar.DELTIDSJOBB_VIL_MER) {
+        return <DELTIDSJOBB_VIL_MER {...props} />;
+    } else if (valgtSituasjon === DinSituasjonSvar.ALDRI_HATT_JOBB) {
+        return <ALDRI_HATT_JOBB {...props} />;
+    } else if (valgtSituasjon === DinSituasjonSvar.VIL_BYTTE_JOBB) {
+        return <VIL_BYTTE_JOBB {...props} />;
+    } else if (valgtSituasjon === DinSituasjonSvar.JOBB_OVER_2_AAR) {
+        return <JOBB_OVER_2_AAR {...props} />;
+    } else if (valgtSituasjon === DinSituasjonSvar.ER_PERMITTERT) {
+        return <ER_PERMITTERT {...props} />;
+    } else if (valgtSituasjon === DinSituasjonSvar.USIKKER_JOBBSITUASJON) {
+        return <USIKKER_JOBBSITUASJON {...props} />;
+    } else if (valgtSituasjon === DinSituasjonSvar.AKKURAT_FULLFORT_UTDANNING) {
+        return <AKKURAT_FULLFORT_UTDANNING {...props} />;
+    } else if (valgtSituasjon === DinSituasjonSvar.VIL_FORTSETTE_I_JOBB) {
+        return <VIL_FORTSETTE_I_JOBB {...props} />;
     } else {
         return <ANNET {...props} />;
     }
