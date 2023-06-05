a32b9bd4882c8a8b1f14d37bf45536e440c61bc3
Geir Gåsodden <geir.gasodden@gmail.com>
2023-06-05 07:13:42 +0200

    r legger til registreringsdato i registreringstittel

--- src/komponenter/registrert-tittel/registrert-tittel.tsx
+++ src/komponenter/registrert-tittel/registrert-tittel.tsx
@@ -1,5 +1,5 @@
 import { createRef, useCallback, useEffect, useState } from 'react';
-import { Heading, Panel } from '@navikt/ds-react';
+import { BodyShort, Heading, Panel } from '@navikt/ds-react';
 import { Success, SuccessColored } from '@navikt/ds-icons';
 
 import { useSprakValg } from '../../contexts/sprak';
@@ -7,21 +7,24 @@ import { useArbeidssokerPerioder } from '../../contexts/arbeidssoker';
 
 import lagHentTekstForSprak from '../../lib/lag-hent-tekst-for-sprak';
 import beregnArbeidssokerperioder from '../../lib/beregn-arbeidssokerperioder';
+import { DinSituasjonSvar, useBrukerregistreringData } from '../../contexts/brukerregistrering';
+import prettyPrintDato from '../../utils/pretty-print-dato';
 
 import spacingStyles from '../../spacing.module.css';
 import flexStyles from '../../flex.module.css';
-import { DinSituasjonSvar, useBrukerregistreringData } from '../../contexts/brukerregistrering';
 
 const TEKSTER = {
     nb: {
         registrert: 'Du er registrert som arbeidssøker',
         registrertNy: 'Du er nå registrert som arbeidssøker',
         registrertPermittert: 'Du er registrert som permittert arbeidssøker',
+        registreringsDato: 'Registreringsdato',
     },
     en: {
         registrert: 'You are registered as job seeker',
         registrertNy: 'You are now registered as job seeker',
         registrertPermittert: 'You are registered as a temporarily layed off job seeker',
+        registreringsDato: 'Date of registration',
     },
 };
 
@@ -44,6 +47,7 @@ const RegistrertTittel = () => {
     const harAktivArbeidssokerperiode = arbeidssokerperioder.harAktivArbeidssokerperiode === 'Ja';
     const erPermittert =
         brukerregistreringData?.registrering?.besvarelse.dinSituasjon === DinSituasjonSvar.ER_PERMITTERT;
+    const registrertDato = brukerregistreringData?.registrering.opprettetDato || false;
 
     const scrollToRegistrering = useCallback(() => {
         const goto = new URLSearchParams(window.location.search).get('goTo');
@@ -75,6 +79,11 @@ const RegistrertTittel = () => {
                     </span>
                     <div>
                         <Heading size="medium">{tekst(hentTekstNokkel(erNyRegistrert, erPermittert))}</Heading>
+                        {registrertDato && (
+                            <BodyShort>
+                                {tekst('registreringsDato')}: {prettyPrintDato(registrertDato)}
+                            </BodyShort>
+                        )}
                     </div>
                 </div>
             </Panel>
