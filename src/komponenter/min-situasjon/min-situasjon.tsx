8dfdc8b2bd4f94519e6783c1520c9d31ea626d96
Geir Gåsodden <geir.gasodden@gmail.com>
2023-06-07 07:05:46 +0200

    c fjernet ubrukt kode

--- src/komponenter/min-situasjon/min-situasjon.tsx
+++ src/komponenter/min-situasjon/min-situasjon.tsx
@@ -1,5 +1,6 @@
 import { Panel } from '@navikt/ds-react';
 
+import { useFeatureToggleData } from '../../contexts/feature-toggles';
 import { useAmplitudeData } from '../hent-initial-data/amplitude-provider';
 import { DinSituasjonSvar, useBrukerregistreringData } from '../../contexts/brukerregistrering';
 import { useArbeidssokerPerioder } from '../../contexts/arbeidssoker';
@@ -8,26 +9,9 @@ import { InnloggingsNiva, useAutentiseringData } from '../../contexts/autentiser
 import Sammendrag from './sammendrag';
 import InnsynLesMer from '../innsyn/innsyn-les-mer';
 import beregnArbeidssokerperioder from '../../lib/beregn-arbeidssokerperioder';
-
-import spacingStyles from '../../spacing.module.css';
-import { useFeatureToggleData } from '../../contexts/feature-toggles';
 import EndreSituasjon from '../endre-situasjon/min-situasjon';
 
-/*
-const situasjon = {
-    MISTET_JOBBEN: 'Har mistet eller kommer til å miste jobben',
-    ALDRI_HATT_JOBB: 'Har aldri vært i jobb',
-    HAR_SAGT_OPP: 'Har sagt opp eller kommer til å si opp',
-    VIL_BYTTE_JOBB: 'Har jobb, men vil bytte',
-    ER_PERMITTERT: 'Er permittert eller kommer til å bli permittert',
-    USIKKER_JOBBSITUASJON: 'Er usikker på jobbsituasjonen min',
-    JOBB_OVER_2_AAR: 'Har ikke vært i jobb de siste 2 årene',
-    VIL_FORTSETTE_I_JOBB: 'Har jobb og ønsker å fortsette i den jobben jeg er i',
-    AKKURAT_FULLFORT_UTDANNING: 'Har akkurat fullført utdanning, militærtjeneste eller annet',
-    DELTIDSJOBB_VIL_MER: 'Har deltidsjobb, men vil jobbe mer',
-    DEFAULT: 'Min arbeidssøkersituasjon',
-};
-*/
+import spacingStyles from '../../spacing.module.css';
 
 function MinSituasjon(props: any) {
     const brukerregistreringData = useBrukerregistreringData();
