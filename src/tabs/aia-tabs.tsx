669f16e2c846f52360f1a5807947d62b14af18df
Geir Gåsodden <geir.gasodden@gmail.com>
2023-06-12 08:54:55 +0200

    r flytter logg til amplitude fra onClick til onChange for tabs

--- src/tabs/aia-tabs.tsx
+++ src/tabs/aia-tabs.tsx
@@ -1,4 +1,5 @@
 import { Tabs } from '@navikt/ds-react';
+import { useEffect, useState } from 'react';
 
 import { useAmplitudeData } from '../komponenter/hent-initial-data/amplitude-provider';
 
@@ -8,11 +9,26 @@ import Behovsavklaring from '../komponenter/behovsavklaring-oppfolging/behovsavk
 import DagpengerOgYtelser from '../komponenter/dagpenger/dagpenger-og-ytelser';
 import Meldekort from '../komponenter/meldekort/meldekort';
 import { loggAktivitet } from '../metrics/metrics';
-
-import styles from '../innhold/innhold.module.css';
-import tabStyles from './tabs.module.css';
-import { useEffect, useState } from 'react';
 import { hentQueryParam, settQueryParam } from '../utils/query-param-utils';
+
+import tabStyles from './tabs.module.css';
+import styles from '../innhold/innhold.module.css';
+
+const QUERY_PARAM = 'aia.aktivTab';
+
+enum TabValue {
+    MIN_SITUASJON = 'situasjon',
+    HJELP_OG_STOTTE = 'hjelp',
+    PENGESTOTTE = 'ytelse',
+    MELDEKORT = 'meldekort',
+}
+
+const tabValueMap = {
+    situasjon: 'Min situasjon',
+    hjelp: 'Hjelp og støtte',
+    ytelse: 'Pengestøtte',
+    meldekort: 'Meldekort',
+};
 
 const MinSituasjonTab = () => {
     return (
@@ -45,13 +61,7 @@ const MeldekortTab = () => {
         </Tabs.Panel>
     );
 };
-const QUERY_PARAM = 'aia.aktivTab';
-enum TabValue {
-    MIN_SITUASJON = 'situasjon',
-    HJELP_OG_STOTTE = 'hjelp',
-    PENGESTOTTE = 'ytelse',
-    MELDEKORT = 'meldekort',
-}
+
 const AiaTabs = () => {
     const { amplitudeData } = useAmplitudeData();
 
@@ -60,13 +70,16 @@ const AiaTabs = () => {
     };
 
     const [aktivTab, settAktivTab] = useState<TabValue>(TabValue.MIN_SITUASJON);
+
     const onChangeTab = (tab: string) => {
         settQueryParam(QUERY_PARAM, tab);
         settAktivTab(tab as TabValue);
+        loggTabSkifte(tabValueMap[tab]);
     };
 
     useEffect(() => {
         const tabFraQuery = hentQueryParam(QUERY_PARAM);
+
         if (
             [TabValue.HJELP_OG_STOTTE, TabValue.MIN_SITUASJON, TabValue.MELDEKORT, TabValue.PENGESTOTTE].includes(
                 tabFraQuery as any
@@ -81,10 +94,10 @@ const AiaTabs = () => {
             <RegistrertTittel />
             <Tabs value={aktivTab} onChange={onChangeTab} className={`${tabStyles.mb2} ${tabStyles.mt1}`}>
                 <Tabs.List>
-                    <Tabs.Tab value="situasjon" label="Min situasjon" onClick={() => loggTabSkifte('Min situasjon')} />
-                    <Tabs.Tab value="hjelp" label="Hjelp og støtte" onClick={() => loggTabSkifte('Hjelp og støtte')} />
-                    <Tabs.Tab value="ytelse" label="Pengestøtte" onClick={() => loggTabSkifte('Pengestøtte')} />
-                    <Tabs.Tab value="meldekort" label="Meldekort" onClick={() => loggTabSkifte('Meldekort')} />
+                    <Tabs.Tab value="situasjon" label="Min situasjon" />
+                    <Tabs.Tab value="hjelp" label="Hjelp og støtte" />
+                    <Tabs.Tab value="ytelse" label="Pengestøtte" />
+                    <Tabs.Tab value="meldekort" label="Meldekort" />
                 </Tabs.List>
                 <MinSituasjonTab />
                 <HjelpOgStotteTab />
