004b46694006dae6990afe382939939160b8bf65
Jostein Holje <jostein.holje@nav.no>
2023-06-21 10:32:09 +0200

    Slett url.ts
    - flytt til ducks/urls.ts

--- src/ducks/urls.ts
+++ src/ducks/urls.ts
@@ -24,6 +24,20 @@ const INNLOGGINGSSTATUS_URL = {
     development: 'https://www.ekstern.dev.nav.no/person/nav-dekoratoren-api/auth',
     production: 'https://www.nav.no/person/nav-dekoratoren-api/auth',
 };
+const MINE_DAGPENGER_URL = {
+    local: 'https://arbeid.intern.dev.nav.no/arbeid/dagpenger/mine-dagpenger',
+    development: 'https://arbeid.intern.dev.nav.no/arbeid/dagpenger/mine-dagpenger',
+    production: 'https://www.nav.no/arbeid/dagpenger/mine-dagpenger',
+};
+
+const DOKUMENTASJON_URL = {
+    local: 'https://arbeid.intern.dev.nav.no/dagpenger/dialog/generell-innsending',
+    development: 'https://arbeid.intern.dev.nav.no/dagpenger/dialog/generell-innsending',
+    production: 'https://www.nav.no/dagpenger/dialog/generell-innsending',
+};
+
+export const mine_dagpenger_url = MINE_DAGPENGER_URL[getEnvironment()];
+export const dokumentasjon_url = DOKUMENTASJON_URL[getEnvironment()];
 
 export const innloggingsStatusUrl = INNLOGGINGSSTATUS_URL[getEnvironment()];
 export const aiaBackendUrl = AIA_BACKEND_URL[getEnvironment()];
