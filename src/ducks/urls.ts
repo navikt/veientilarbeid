eae35c1ec74f59c15b6ade66fcf80d0f1d9316a4
Jostein Holje <jostein.holje@nav.no>
2023-08-18 13:06:16 +0200

    Gå rett til aia-backend i prod

--- src/ducks/urls.ts
+++ src/ducks/urls.ts
@@ -16,7 +16,7 @@ export const getEnvironment = () => {
 const AIA_BACKEND_URL = {
     local: 'http://localhost:3000/aia-backend',
     development: 'https://www.intern.dev.nav.no/aia-backend',
-    production: 'https://www.nav.no/tms-min-side-proxy/aia/aia-backend',
+    production: 'https://www.nav.no/aia-backend',
 };
 
 const INNLOGGINGSSTATUS_URL = {
