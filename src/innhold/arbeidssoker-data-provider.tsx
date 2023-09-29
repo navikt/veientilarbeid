3dcece0509f6c8b2a84ecf0239411f685bfe2770
robertkittilsen <robert.graarup.kittilsen@nav.no>
2023-09-29 09:55:50 +0200

    Lagt til brukerregistrering, brukerInfo og ulesteDialoger i state fra nytt kall.

--- src/innhold/arbeidssoker-data-provider.tsx
+++ src/innhold/arbeidssoker-data-provider.tsx
@@ -1,25 +1,19 @@
 import * as React from 'react';
+import { useEffect } from 'react';
 import * as Oppfolging from '../contexts/oppfolging';
 import * as Brukerregistrering from '../contexts/brukerregistrering';
+import { ForeslattInnsatsgruppe, selectForeslattInnsatsgruppe } from '../contexts/brukerregistrering';
 import { fetchData } from '../ducks/api-utils';
-import {
-    ARBEIDSOKER_INNHOLD,
-    BRUKERINFO_URL,
-    BRUKERREGISTRERING_URL,
-    MOTESTOTTE_URL,
-    ULESTEDIALOGER_URL,
-} from '../ducks/api';
+import { ARBEIDSOKER_INNHOLD, MOTESTOTTE_URL, STATUS } from '../ducks/api';
 import Innholdslaster from '../komponenter/innholdslaster/innholdslaster';
 import Feilmelding from '../komponenter/feilmeldinger/feilmelding';
 import { ProfilProvider } from '../contexts/profil';
 import * as BrukerInfo from '../contexts/bruker-info';
-import { useEffect } from 'react';
 import { GjelderFraDatoModalProvider } from '../contexts/gjelder-fra-dato-modal';
 import { GjelderFraDatoProvider } from '../contexts/gjelder-fra-dato';
 import { AmplitudeProvider } from '../komponenter/hent-initial-data/amplitude-provider';
 import * as Motestotte from '../contexts/motestotte';
 import * as UlesteDialoger from '../contexts/ulestedialoger';
-import { ForeslattInnsatsgruppe, selectForeslattInnsatsgruppe } from '../contexts/brukerregistrering';
 import { BehovForVeiledningProvider } from '../contexts/behov-for-veiledning';
 import { DagpengerStatusProvider } from '../contexts/dagpenger-status';
 import { useSWRImmutable } from '../hooks/useSWR';
@@ -34,32 +28,37 @@ const ArbeidssokerDataProvider = (props: Props) => {
     );
     const [oppfolgingState, setOppfolgingState] = React.useState<Oppfolging.State>(Oppfolging.initialState);
     const [brukerInfoState, setBrukerInfoState] = React.useState<BrukerInfo.State>(BrukerInfo.initialState);
+
     const [motestotteState, setMotestotteState] = React.useState<Motestotte.State>(Motestotte.initialState);
     const [ulesteDialogerState, setUlesteDialogerState] = React.useState<UlesteDialoger.State>(
         UlesteDialoger.initialState,
     );
 
-    const { data } = useSWRImmutable<{ oppfolging: any }>(ARBEIDSOKER_INNHOLD);
+    const { data } = useSWRImmutable<{
+        oppfolging: any;
+        brukerregistrering: any;
+        brukerInfo: any;
+        ulesteDialoger: any;
+    }>(ARBEIDSOKER_INNHOLD);
 
     useEffect(() => {
-        /*fetchData<Oppfolging.State, Oppfolging.Data>(oppfolgingState, setOppfolgingState, VEILARBOPPFOLGING_URL);*/
-        fetchData<Brukerregistrering.State, Brukerregistrering.Data>(
-            brukerregistreringState,
-            setBrukerregistreringState,
-            BRUKERREGISTRERING_URL,
-        );
-        fetchData<BrukerInfo.State, BrukerInfo.Data>(brukerInfoState, setBrukerInfoState, BRUKERINFO_URL);
-        fetchData<UlesteDialoger.State, UlesteDialoger.Data>(
-            ulesteDialogerState,
-            setUlesteDialogerState,
-            ULESTEDIALOGER_URL,
-        );
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
-
-    useEffect(() => {
-        if (data && data?.oppfolging?.status === 200) {
-            setOppfolgingState(data?.oppfolging?.data as Oppfolging.State);
+        if (data) {
+            setOppfolgingState({
+                data: data.oppfolging.data,
+                status: data.oppfolging.status === 200 ? STATUS.OK : STATUS.ERROR,
+            });
+            setBrukerInfoState({
+                data: data.brukerInfo.data,
+                status: data.brukerInfo.status === 200 ? STATUS.OK : STATUS.ERROR,
+            });
+            setBrukerregistreringState({
+                data: data.brukerregistrering.data,
+                status: data.brukerregistrering.status === 200 ? STATUS.OK : STATUS.ERROR,
+            });
+            setUlesteDialogerState({
+                data: data.ulesteDialoger.data,
+                status: data.ulesteDialoger.status === 200 ? STATUS.OK : STATUS.ERROR,
+            });
         }
     }, [data]);
 
@@ -72,7 +71,7 @@ const ArbeidssokerDataProvider = (props: Props) => {
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [brukerregistreringState]);
 
-    const avhengigheter = [/*oppfolgingState, */ brukerregistreringState, brukerInfoState];
+    const avhengigheter = [oppfolgingState, brukerregistreringState, brukerInfoState];
 
     return (
         <Innholdslaster feilmeldingKomponent={<Feilmelding />} storrelse="XXL" avhengigheter={avhengigheter}>
