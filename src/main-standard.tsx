8976e002a197126115823cf06843f94c73542b4a
robertkittilsen <robert.graarup.kittilsen@nav.no>
2023-10-12 14:41:50 +0200

    Fjerner eksplisitt erStandard = true for innholdslaster og arbeidssokerDataProvider

--- src/main-standard.tsx
+++ src/main-standard.tsx
@@ -57,13 +57,13 @@ const Mikrofrontend = () => {
             feilmeldingKomponent={<Feilmelding />}
             storrelse="XXL"
             avhengigheter={[state, arbeidssokerState, featureToggleState]}
-            erStandard={true}
+            erStandard
         >
             <Autentisering.AutentiseringContext.Provider value={state}>
                 <Arbeidssoker.ArbeidssokerContext.Provider value={arbeidssokerState}>
                     <FeatureToggle.FeaturetoggleContext.Provider value={featureToggleState}>
                         <SprakValg.SprakContext.Provider value={valgtSprak}>
-                            <ArbeidssokerDataProvider erStandard={true}>
+                            <ArbeidssokerDataProvider erStandard>
                                 <MeldepliktProvider>
                                     <ReaktiveringProvider>
                                         <BesvarelseProvider>
