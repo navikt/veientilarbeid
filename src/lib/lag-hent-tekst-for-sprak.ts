f8dbe5fdef57c0aabbdb1bb9eed3712ca8e93f13
Jostein Holje <jostein.holje@nav.no>
2024-02-05 13:24:03 +0100

    Bruk lagHentTekstForSprak fra utils

--- src/lib/lag-hent-tekst-for-sprak.ts
+++ src/lib/lag-hent-tekst-for-sprak.ts
@@ -1,15 +1,4 @@
-import { Sprak } from '../contexts/sprak';
-
-export type Tekster<T> = { nb: Record<string, T> } & Partial<{ [P in Exclude<Sprak, 'nb'>]: Record<string, T> }>;
-
-const lagHentTekstForSprak = (tekster: Tekster<any>, sprak: Sprak) => (key: string) => {
-    const tekst = tekster[sprak];
-
-    if (tekst) {
-        return tekst[key] || tekster.nb[key];
-    }
-
-    return tekster.nb[key];
-};
+import { lagHentTekstForSprak, Tekster } from '@navikt/arbeidssokerregisteret-utils';
+export type { Tekster };
 
 export default lagHentTekstForSprak;
