105f52a16eced5a2a79484cef88382c4da9e85a1
Jostein Holje <jostein.holje@nav.no>
2024-02-05 13:23:09 +0100

    Bruk typer fra arbeidssokerregisteret-utils i use-brukerregistrering-data

--- src/hooks/use-brukerregistrering-data.ts
+++ src/hooks/use-brukerregistrering-data.ts
@@ -2,20 +2,27 @@ import { useArbeidssokerData } from './use-arbeidssoker-data';
 
 import { DataElement, STATUS } from '../ducks/api';
 
-export enum DinSituasjonSvar {
-    MISTET_JOBBEN = 'MISTET_JOBBEN',
-    ALDRI_HATT_JOBB = 'ALDRI_HATT_JOBB',
-    HAR_SAGT_OPP = 'HAR_SAGT_OPP',
-    VIL_BYTTE_JOBB = 'VIL_BYTTE_JOBB',
-    ER_PERMITTERT = 'ER_PERMITTERT',
-    USIKKER_JOBBSITUASJON = 'USIKKER_JOBBSITUASJON',
-    JOBB_OVER_2_AAR = 'JOBB_OVER_2_AAR',
-    VIL_FORTSETTE_I_JOBB = 'VIL_FORTSETTE_I_JOBB',
-    AKKURAT_FULLFORT_UTDANNING = 'AKKURAT_FULLFORT_UTDANNING',
-    DELTIDSJOBB_VIL_MER = 'DELTIDSJOBB_VIL_MER',
-    INGEN_SVAR = 'INGEN_SVAR',
-    INGEN_VERDI = 'INGEN_VERDI',
-}
+import {
+    DinSituasjon as DinSituasjonSvar,
+    Utdanningsnivaa as UtdanningSvar,
+    JaEllerNei as UtdanningBestattSvar,
+    JaEllerNei as HelseHinderSvar,
+    JaEllerNei as AndreForholdSvar,
+    UtdanningGodkjentValg as UtdanningGodkjentSvar,
+    TilbakeIArbeid as TilbakeIArbeidSvar,
+    SisteStillingValg as SisteStillingSvar,
+} from '@navikt/arbeidssokerregisteret-utils';
+
+export {
+    DinSituasjonSvar,
+    UtdanningSvar,
+    UtdanningGodkjentSvar,
+    UtdanningBestattSvar,
+    HelseHinderSvar,
+    AndreForholdSvar,
+    TilbakeIArbeidSvar,
+    SisteStillingSvar,
+};
 
 export enum FremtidigSituasjonSvar {
     SAMME_ARBEIDSGIVER = 'SAMME_ARBEIDSGIVER',
@@ -23,54 +30,6 @@ export enum FremtidigSituasjonSvar {
     NY_ARBEIDSGIVER = 'NY_ARBEIDSGIVER',
     USIKKER = 'USIKKER',
     INGEN_PASSER = 'INGEN_PASSER',
-}
-
-export enum UtdanningSvar {
-    INGEN_UTDANNING = 'INGEN_UTDANNING',
-    GRUNNSKOLE = 'GRUNNSKOLE',
-    VIDEREGAENDE_GRUNNUTDANNING = 'VIDEREGAENDE_GRUNNUTDANNING',
-    VIDEREGAENDE_FAGBREV_SVENNEBREV = 'VIDEREGAENDE_FAGBREV_SVENNEBREV',
-    HOYERE_UTDANNING_1_TIL_4 = 'HOYERE_UTDANNING_1_TIL_4',
-    HOYERE_UTDANNING_5_ELLER_MER = 'HOYERE_UTDANNING_5_ELLER_MER',
-    INGEN_SVAR = 'INGEN_SVAR',
-}
-
-export enum UtdanningBestattSvar {
-    JA = 'JA',
-    NEI = 'NEI',
-    INGEN_SVAR = 'INGEN_SVAR',
-}
-
-export enum UtdanningGodkjentSvar {
-    JA = 'JA',
-    NEI = 'NEI',
-    VET_IKKE = 'VET_IKKE',
-    INGEN_SVAR = 'INGEN_SVAR',
-}
-
-export enum HelseHinderSvar {
-    JA = 'JA',
-    NEI = 'NEI',
-    INGEN_SVAR = 'INGEN_SVAR',
-}
-
-export enum AndreForholdSvar {
-    JA = 'JA',
-    NEI = 'NEI',
-    INGEN_SVAR = 'INGEN_SVAR',
-}
-
-export enum SisteStillingSvar {
-    HAR_HATT_JOBB = 'HAR_HATT_JOBB',
-    HAR_IKKE_HATT_JOBB = 'HAR_IKKE_HATT_JOBB',
-    INGEN_SVAR = 'INGEN_SVAR',
-}
-
-export enum TilbakeIArbeidSvar {
-    JA_FULL_STILLING = 'JA_FULL_STILLING',
-    JA_REDUSERT_STILLING = 'JA_REDUSERT_STILLING',
-    USIKKER = 'USIKKER',
-    NEI = 'NEI',
 }
 
 export interface Besvarelse {
